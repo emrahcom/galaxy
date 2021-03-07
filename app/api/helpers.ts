d3743cd53edb2a7635eea7cbbf342009f6a907fd
emrah <emrah.com@gmail.com>
2021-03-07 18:50:58 +0300

    catch broken req

--- app/api/helpers.ts
+++ app/api/helpers.ts
@@ -13,49 +13,77 @@ class BadRequest extends Error {
 export const about = (req: ServerRequest) =>
   req.respond({
     body: JSON.stringify({ message: "galaxy" }),
-  });
+  })
+    .catch((e) => {
+      req.conn.close();
+    })
+    .catch(() => {});
 
 // ----------------------------------------------------------------------------
 export const forbidden = (req: ServerRequest) =>
   req.respond({
     status: Status.Forbidden,
     body: JSON.stringify({ message: "Forbidden" }),
-  });
+  })
+    .catch((e) => {
+      req.conn.close();
+    })
+    .catch(() => {});
 
 // ----------------------------------------------------------------------------
 export const internalServerError = (req: ServerRequest) =>
   req.respond({
     status: Status.InternalServerError,
     body: JSON.stringify({ message: "InternalServerError" }),
-  });
+  })
+    .catch((e) => {
+      req.conn.close();
+    })
+    .catch(() => {});
 
 // ----------------------------------------------------------------------------
 export const ok = (req: ServerRequest, resBody: string) =>
   req.respond({
     status: Status.OK,
     body: resBody,
-  });
+  })
+    .catch((e) => {
+      req.conn.close();
+    })
+    .catch(() => {});
 
 // ----------------------------------------------------------------------------
 export const methodNotAllowed = (req: ServerRequest) =>
   req.respond({
     status: Status.MethodNotAllowed,
     body: JSON.stringify({ message: "MethodNotAllowed" }),
-  });
+  })
+    .catch((e) => {
+      req.conn.close();
+    })
+    .catch(() => {});
 
 // ----------------------------------------------------------------------------
 export const notFound = (req: ServerRequest) =>
   req.respond({
     status: Status.NotFound,
     body: JSON.stringify({ message: "NotFound" }),
-  });
+  })
+    .catch((e) => {
+      req.conn.close();
+    })
+    .catch(() => {});
 
 // ----------------------------------------------------------------------------
 export const unauthorized = (req: ServerRequest) =>
   req.respond({
     status: Status.Unauthorized,
     body: JSON.stringify({ message: "Unauthorized" }),
-  });
+  })
+    .catch((e) => {
+      req.conn.close();
+    })
+    .catch(() => {});
 
 // ----------------------------------------------------------------------------
 export function parseQueryString(req: ServerRequest): URLSearchParams {
