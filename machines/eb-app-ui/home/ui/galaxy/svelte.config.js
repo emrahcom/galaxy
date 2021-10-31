f1d44000c436924a561f9fe8d03c57b8d1e7332b
emrah <emrah.com@gmail.com>
2021-10-31 19:44:35 +0300

    refactor: format according to prettier

--- machines/eb-app-ui/home/ui/galaxy/svelte.config.js
+++ machines/eb-app-ui/home/ui/galaxy/svelte.config.js
@@ -1,4 +1,4 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
