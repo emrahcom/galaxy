7b508609e1bea36930edace4265e4f8cc379d3ad
emrah <emrah.com@gmail.com>
2021-11-03 21:07:35 +0300

    fix, api: initializing

--- machines/eb-app-api/home/api/galaxy/index.ts
+++ machines/eb-app-api/home/api/galaxy/index.ts
@@ -1,20 +1,32 @@
-import { serve, Server } from "https://deno.land/std/http/server.ts";
 import { HOSTNAME, PORT } from "./config.ts";
 
-const PRE = "/api";
-const app: Server = serve({
-  hostname: HOSTNAME,
-  port: PORT,
-});
+// ----------------------------------------------------------------------------
+async function handle(cnn: Deno.Conn) {
+  const http = Deno.serveHttp(cnn);
+
+  for await (const req of http) {
+    req.respondWith(
+      new Response("hello", {
+        status: 200,
+      }),
+    );
+
+    const url = new URL(req.request.url);
+    console.log(url);
+    console.log(`path: ${url.pathname}`);
+  }
+}
 
 // ----------------------------------------------------------------------------
 async function main() {
-  for await (const req of app) {
-    if (req.url === `${PRE}/about`) {
-      continue;
-    } else if (req.url === `${PRE}/token/` && req.method === "POST") {
-      continue;
-    }
+  const server = Deno.listen({
+    hostname: HOSTNAME,
+    port: PORT,
+  });
+
+  for await (const cnn of server) {
+    handle(cnn);
+  }
 }
 
 // ----------------------------------------------------------------------------
