d9d1bd84b3fc76fcd1277722dfd7b4531e1a2652
emrah <emrah.com@gmail.com>
2021-11-20 23:30:53 +0300

    database, room: initial

--- machines/eb-app-api/home/api/galaxy/database/02-create-galaxy-tables.sql
+++ machines/eb-app-api/home/api/galaxy/database/02-create-galaxy-tables.sql
@@ -44,7 +44,8 @@ ALTER TABLE identity OWNER TO galaxy;
 -- -----------------------------------------------------------------------------
 -- DOMAIN
 -- -----------------------------------------------------------------------------
-CREATE TYPE domain_auth_type AS ENUM ('none', 'token', 'moderated');
+CREATE TYPE domain_auth_type AS ENUM
+    ('none', 'token', 'moderated');
 CREATE TABLE domain (
     "id" uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     "identity_id" uuid NOT NULL REFERENCES identity(id) ON DELETE CASCADE,
@@ -62,6 +63,38 @@ CREATE INDEX ON domain("enabled");
 CREATE INDEX ON domain("created_at");
 CREATE INDEX ON domain("updated_at");
 ALTER TABLE domain OWNER TO galaxy;
+
+-- -----------------------------------------------------------------------------
+-- ROOM
+-- -----------------------------------------------------------------------------
+-- - update suffix if accessed_at is older than 6 hours
+-- - dont show name to others, always use title as display name or description
+-- - room schedule, how?
+-- - subscription, how?
+-- - visibility, how?
+-- - accessibility, how?
+-- -----------------------------------------------------------------------------
+CREATE TYPE room_schedule_type AS ENUM
+    ('permanent', 'scheduled', 'periodic');
+CREATE TABLE room (
+    "id" uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    "domain_id" uuid NOT NULL REFERENCES domain(id) ON DELETE CASCADE,
+    "name" varchar(250) NOT NULL,
+    "title" varchar(250) NOT NULL,
+    "has_suffix" boolean NOT NULL DEFAULT TRUE,
+    "suffix" uuid NOT NULL DEFAULT gen_random_uuid(),
+    "enabled" boolean NOT NULL DEFAULT TRUE,
+    "created_at" timestamp with time zone NOT NULL DEFAULT NOW(),
+    "updated_at" timestamp with time zone NOT NULL DEFAULT NOW(),
+    "accessed_at" timestamp with time zone NOT NULL DEFAULT NOW()
+);
+CREATE UNIQUE INDEX ON room("domain_id", "name");
+CREATE INDEX ON room("name");
+CREATE INDEX ON room("title");
+CREATE INDEX ON room("enabled");
+CREATE INDEX ON room("created_at");
+CREATE INDEX ON room("updated_at");
+ALTER TABLE room OWNER TO galaxy;
 
 -- -----------------------------------------------------------------------------
 COMMIT;
