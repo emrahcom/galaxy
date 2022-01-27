eaabf34cc4d8e1efa12702e8b937b5eb8ac65570
emrah <emrah.com@gmail.com>
2022-01-27 18:49:52 +0300

    feat, ui: add public navbar

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/navbar-pub.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/navbar-pub.svelte
@@ -1,10 +1,23 @@
-<nav class="navbar navbar-light bg-light fixed-top shadow">
+<nav class="navbar navbar-expand navbar-light bg-light fixed-top shadow">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">
       <img src="/logo.svg" alt="logo" width="32rem" height="32rem" />
     </a>
-    <a class="navbar-link" href="/panel" title="Public">
-      <i class="bi bi-gear" />
-    </a>
+
+    <div class="collapse navbar-collapse justify-content-end">
+      <ul class="navbar-nav">
+        <li class="nav-item me-2">
+          <a class="btn btn-sm btn-outline-primary" href="/registration">
+            Sign Up
+          </a>
+        </li>
+        <li class="nav-item me-2">
+          <a class="btn btn-sm btn-outline-primary" href="/login">
+            <span class="bi bi-emoji-smile" />
+            Login
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
