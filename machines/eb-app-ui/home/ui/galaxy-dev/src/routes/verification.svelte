17ffa1d625d73917765e754f728c3923e6d68faa
emrah <emrah.com@gmail.com>
2022-02-10 18:51:46 +0300

    refactor, ui: the verification page

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/verification.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/verification.svelte
@@ -11,6 +11,7 @@
 <script lang="ts">
   import type { KratosForm, KratosError } from "$lib/kratos-types";
   import Form from "$lib/components/kratos/form.svelte";
+  import Messages from "$lib/components/kratos/messages.svelte";
 
   export let dm: KratosForm | KratosError;
 
@@ -18,12 +19,24 @@
 </script>
 
 <!-- -------------------------------------------------------------------------->
-{#if dm.instanceOf === "KratosForm"}
-  <div class="container" id="verification">
-    <h2 class="subheading">verification</h2>
+<section class="container-fluid" id="verification">
+  {#if dm.instanceOf === "KratosForm"}
+    <div class="row justify-content-center">
+      <div class="col text-center" style="max-width:540px;">
+        <p class="h3 text-muted">Verify your email address</p>
+        <p class="small text-muted my-4 text-start">
+          Enter the email address associated with your account and we will send
+          you a link to verify your email address.
+        </p>
 
-    <Form {dm} groups={["default", "link"]} />
-  </div>
-{:else}
-  <p>Something went wrong</p>
-{/if}
+        {#if dm.ui.messages}
+          <Messages messages={dm.ui.messages} />
+        {:else}
+          <Form {dm} groups={["default", "link"]} />
+        {/if}
+      </div>
+    </div>
+  {:else}
+    <p class="text-center">Something went wrong</p>
+  {/if}
+</section>
