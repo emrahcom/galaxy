29227ff77d39abd2adc2ca9e3f1ab9fdbbe65bef
emrah <emrah.com@gmail.com>
2022-02-11 23:45:50 +0300

    refactor, ui: readonly email in verification

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/kratos/fieldset-email.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/kratos/fieldset-email.svelte
@@ -1,24 +1,40 @@
 <script lang="ts">
+  import { page } from "$app/stores";
+  import { getContext } from "svelte";
   import Messages from "$lib/components/kratos/messages.svelte";
-  import type { Node } from "$lib/kratos-types";
+  import type { KratosIdentity, Node } from "$lib/kratos-types";
 
   export let node: Node;
 
   const attr = node.attributes;
+  const identity: KratosIdentity = getContext("identity");
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <div class="form-floating my-3">
-  <input
-    type="email"
-    id="email"
-    class="form-control"
-    name={attr.name}
-    value={attr.value || ""}
-    placeholder="email"
-    disabled={attr.disabled}
-    required={attr.required}
-  />
+  {#if $page.url.pathname === "/verification"}
+    <input
+      type="email"
+      id="email"
+      class="form-control"
+      name={attr.name}
+      value={identity.traits.email}
+      placeholder="email"
+      readonly
+      required={attr.required}
+    />
+  {:else}
+    <input
+      type="email"
+      id="email"
+      class="form-control"
+      name={attr.name}
+      value={attr.value || ""}
+      placeholder="email"
+      disabled={attr.disabled}
+      required={attr.required}
+    />
+  {/if}
   <label for="email">Email</label>
 </div>
 
