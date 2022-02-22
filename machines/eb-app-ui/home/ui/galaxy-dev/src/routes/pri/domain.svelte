75d9638c2617b6566abb5c82577a6c0e225a7505
emrah <emrah.com@gmail.com>
2022-02-22 15:59:36 +0300

    feat, ui: initialize the domain page

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { listDomain } from "$lib/pri/domain";
+
+  const domains = await listDomain();
+</script>
+
+<!-- -------------------------------------------------------------------------->
+{domains}
