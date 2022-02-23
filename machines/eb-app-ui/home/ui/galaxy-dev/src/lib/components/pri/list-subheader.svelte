df31f16faea11ca9551a08e413b23a71e63b376a
emrah <emrah.com@gmail.com>
2022-02-23 23:45:01 +0300

    feat, ui: list subheader

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/list-subheader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let subheader: string;
+  export let href: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="subheader">
+  <div class="row mx-auto" style="max-width:1080px;">
+    <div class="col">
+      <h3 class="text-muted">{subheader}</h3>
+    </div>
+    <div class="col justify-content-end text-end">
+      <a class="btn btn-outline-secondary" role="button" {href}>
+        <span class="bi bi-plus-lg" />
+      </a>
+    </div>
+  </div>
+</section>
--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/profiles.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/profiles.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import Subheader from "$lib/components/pri/subheader.svelte";
+  import Subheader from "$lib/components/pri/list-subheader.svelte";
   import { listProfiles } from "$lib/pri/profile";
 
   const promise = listProfiles();
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<Subheader />
+<Subheader subheader="Your profiles" href="/pri/profile/add" />
 
 {#await promise}
   <p class="text-center">Waiting...</p>
