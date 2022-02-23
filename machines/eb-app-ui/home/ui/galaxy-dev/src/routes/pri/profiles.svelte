4844a9c299e0cf17a7ba5f53c88a6832a424eb00
emrah <emrah.com@gmail.com>
2022-02-23 22:34:51 +0300

    feat, ui: initialize the subheader

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/profiles.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/profiles.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import Subheader from "$lib/components/pri/sunheader.svelte";
   import { listProfiles } from "$lib/pri/profile";
 
   const promise = listProfiles();
 </script>
 
 <!-- -------------------------------------------------------------------------->
+<Subheader />
+
 {#await promise}
   <p class="text-center">Waiting...</p>
 {:then profiles}
