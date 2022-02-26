771404b5ceb2b9c224afd4fe0ae5c1d149e72dba
emrah <emrah.com@gmail.com>
2022-02-26 21:56:20 +0300

    refactor, ui: profile cards

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/list-profiles.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/list-profiles.svelte
@@ -5,10 +5,45 @@
 </script>
 
 <!-- -------------------------------------------------------------------------->
-{#each profiles as p}
-  {p.id}<br />
-  {p.name}<br />
-  {p.email}<br />
-  {p.is_default}<br />
-  <br />
-{/each}
+<section id="profiles">
+  <div class="row mx-auto mt-2 g-2" style="max-width:1080px;">
+    {#each profiles as p}
+      <div class="col-sm-6 col-lg-4">
+        <div class="card {!p.is_default ? 'border border-primary' : ''}">
+          <div class="card-body text-center">
+            <h5 class="card-title text-muted">{p.name}</h5>
+            <p class="card-text text-muted">{p.email}</p>
+          </div>
+
+          <div class="card-footer bg-body text-center">
+            <a
+              href="/pri/profile/update/{p.id}"
+              class="btn btn-outline-secondary btn-sm"
+              role="button"
+            >
+              <i class="bi bi-pencil" />
+            </a>
+
+            {#if !p.is_default}
+              <a
+                href="/pri/profile/set/default/{p.id}"
+                class="btn btn-outline-secondary btn-sm"
+                role="button"
+              >
+                <i class="bi bi-check" />
+              </a>
+            {/if}
+
+            <a
+              href="/pri/profile/del/{p.id}"
+              class="btn btn-outline-secondary btn-sm"
+              role="button"
+            >
+              <i class="bi bi-trash" />
+            </a>
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
+</section>
--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/list-subheader.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/list-subheader.svelte
@@ -5,11 +5,11 @@
 
 <!-- -------------------------------------------------------------------------->
 <section id="subheader">
-  <div class="row mx-auto" style="max-width:1080px;">
-    <div class="col">
+  <div class="row mx-auto border-bottom border-3" style="max-width:1080px;">
+    <div class="col my-auto">
       <h3 class="text-muted">{subheader}</h3>
     </div>
-    <div class="col text-end">
+    <div class="col text-end my-3">
       <a class="btn btn-outline-secondary" role="button" {href}>
         <span class="bi bi-plus-lg" />
       </a>
