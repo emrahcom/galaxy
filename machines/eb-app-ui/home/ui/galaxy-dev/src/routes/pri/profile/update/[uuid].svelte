caa4439885482b35cd17c6448b645b1be7a27426
emrah <emrah.com@gmail.com>
2022-02-26 18:53:26 +0300

    feat, ui: initialize profile update

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/profile/update/[uuid].svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  console.log($page);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+profile update
