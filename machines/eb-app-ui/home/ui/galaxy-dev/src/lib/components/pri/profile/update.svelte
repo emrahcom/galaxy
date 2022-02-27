d10053e7ba73d1e2e3fa96d41cc551e511b80bd8
emrah <emrah.com@gmail.com>
2022-02-27 21:06:46 +0300

    feat, ui: profile update form

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/update.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/update.svelte
@@ -3,13 +3,61 @@
   import type { Profile } from "$lib/types";
 
   export let p: Profile;
+
+  function cancel() {
+    window.location.href = "/pri/profile";
+  }
+
+  function update() {
+    console.log("update");
+  }
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <section id="update">
-  <div class="row mx-auto mt-2 g-2" style="max-width:{MAX_WIDTH};">
-    <p>{p.id}</p>
-    <p>{p.name}</p>
-    <p>{p.email}</p>
+  <div class="row mt-2 justify-content-center" style="max-width:{MAX_WIDTH};">
+    <div class="col-sm-6 col-lg-4">
+      <form on:submit|preventDefault={update}>
+        <div class="form-floating my-3">
+          <input
+            type="text"
+            id="name"
+            class="form-control"
+            name="name"
+            placeholder="Name"
+            value={p.name}
+            required
+          />
+          <label for="name">Name</label>
+        </div>
+
+        <div class="form-floating my-3">
+          <input
+            type="email"
+            id="email"
+            class="form-control"
+            name="email"
+            placeholder="Email"
+            value={p.email}
+            required
+          />
+          <label for="email">Email</label>
+        </div>
+
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            on:click={cancel}
+          >
+            Cancel
+          </button>
+
+          <button class="btn btn-outline-secondary" type="submit">
+            Update
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </section>
