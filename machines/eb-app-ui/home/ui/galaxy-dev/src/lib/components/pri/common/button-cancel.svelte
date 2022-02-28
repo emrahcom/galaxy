9db84419015e4f6c096339be0fa0afeb5a533677
emrah <emrah.com@gmail.com>
2022-02-28 20:49:39 +0300

    feat, ui: eventdispatcher for cancel button

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/common/button-cancel.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/common/button-cancel.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
   export let label = "Cancel";
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<button class="btn btn-outline-secondary" type="button">{label}</button>
+<button
+  class="btn btn-outline-secondary"
+  type="button"
+  on:click={() => dispatch("click")}
+>
+  {label}
+</button>
