5c662cf518e4b1c316104d0bc9263d816ccffb23
emrah <emrah.com@gmail.com>
2022-03-04 23:42:54 +0300

    refactor, ui: remove border tag

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/list.svelte
@@ -12,7 +12,7 @@
   <div class="row mx-auto mt-2 g-3">
     {#each profiles as p}
       <div class="col-sm-6 col-lg-4 col-xxl-3">
-        <div class="card {p.is_default ? 'border border-primary' : ''}">
+        <div class="card {p.is_default ? 'border-primary' : ''}">
           <div class="card-body text-center">
             <h5 class="card-title text-muted">{p.name}</h5>
             <p class="card-text text-muted">{p.email ?? "-"}</p>
