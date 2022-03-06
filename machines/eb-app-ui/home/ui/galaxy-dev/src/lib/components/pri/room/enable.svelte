16a40bb86ca707591802b22a1f55d63ebd39e90f
emrah <emrah.com@gmail.com>
2022-03-06 16:42:38 +0300

    feat, ui: room actions

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/enable.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { Room } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import Switch from "$lib/components/common/form-switch.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/warning.svelte";
+
+  export let p: Room;
+
+  let warning = false;
+
+  function cancel() {
+    window.location.href = "/pri/room";
+  }
+
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/room/enable", p.id);
+      window.location.href = "/pri/room";
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="enable">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text name="name" label="Name" value={p.name} readonly={true} />
+      <Text
+        name="domain"
+        label="Jitsi Domain"
+        value={p.domain_name}
+        readonly={true}
+      />
+      <Switch
+        name="has_suffix"
+        label="Enable unpredictable room name generator"
+        value={p.has_suffix}
+        disabled={true}
+      />
+
+      {#if warning}
+        <Warning>The enable request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <Submit label="Enable" />
+      </div>
+    </form>
+  </div>
+</section>
---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/update.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { action, domainsAsOptions } from "$lib/api";
+  import type { Room } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Select from "$lib/components/common/form-select.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import Switch from "$lib/components/common/form-switch.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/warning.svelte";
+
+  export let p: Room;
+
+  let warning = false;
+  let promise = domainsAsOptions();
+
+  function cancel() {
+    window.location.href = "/pri/room";
+  }
+
+  async function onSubmit() {
+    try {
+      warning = false;
+      await action("/api/pri/room/update", p);
+      window.location.href = "/pri/room";
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="update">
+  {#await promise then domains}
+    <div class="d-flex mt-2 justify-content-center">
+      <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+        <Text name="name" label="Name" bind:value={p.name} required={true} />
+        <Select
+          id="domain_id"
+          label="Jitsi Domain"
+          bind:value={p.domain_id}
+          options={domains}
+        />
+        <Switch
+          name="has_suffix"
+          label="Enable unpredictable room name generator"
+          bind:value={p.has_suffix}
+        />
+
+        {#if warning}
+          <Warning>
+            The update request is not accepted. Please check your inputs.
+          </Warning>
+        {/if}
+
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <Cancel on:click={cancel} />
+          <Submit label="Update" />
+        </div>
+      </form>
+    </div>
+  {:catch}
+    <Warning>Something went wrong</Warning>
+  {/await}
+</section>
