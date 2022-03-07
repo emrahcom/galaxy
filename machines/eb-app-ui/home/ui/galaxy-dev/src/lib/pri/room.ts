4c107071af5d3f971a69543dc50d8cd7287e56e8
emrah <emrah.com@gmail.com>
2022-03-07 20:45:46 +0300

    refactor, ui: href replace while connecting to the room

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/room.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/room.ts
@@ -1,15 +1,7 @@
-import { get, getById } from "$lib/api";
-//import type { Domain, Profile, Room } from "$lib/types";
+import { getById } from "$lib/api";
 
 // -----------------------------------------------------------------------------
 export async function connect(roomId: string) {
-  const room = await getById("/api/pri/room/get", roomId);
-  const domain = await getById("/api/pri/domain/get", room.domain_id);
-  const profile = await get("/api/pri/profile/get/default");
-
-  console.log(room);
-  console.log(domain);
-  console.log(profile);
-
-  //room get link api call
+  const roomLink = await getById("/api/pri/room/get/link", roomId);
+  window.location.replace(roomLink.link);
 }
