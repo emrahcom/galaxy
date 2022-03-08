eb946466b6f9f5e5f4827b325e15a59474c8af7d
emrah <emrah.com@gmail.com>
2022-03-08 22:27:53 +0300

    feat, ui: initialize the meeting add

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting/add.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { action, list } from "$lib/api";
+  import { SCHEDULE_TYPE_OPTIONS } from "$lib/pri/meeting";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import RadioInline from "$lib/components/common/form-radio-inline.svelte";
+  import Select from "$lib/components/common/form-select.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import Switch from "$lib/components/common/form-switch.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  let warning = false;
+  let p = {
+    profile_id: "",
+    room_id: "",
+    name: "",
+    info: "",
+    schedule_type: SCHEDULE_TYPE_OPTIONS[0][0],
+    schedule_attr: {
+      duration: 60,
+    },
+    hidden: true,
+    restricted: false,
+    subscribable: true,
+  };
+  let promise1 = list("/api/pri/profile/list", 100);
+  let promise2 = list("/api/pri/room/list", 100);
+
+  function cancel() {
+    window.location.href = "/pri/meeting";
+  }
+
+  async function onSubmit() {
+    try {
+      warning = false;
+      await action("/api/pri/meeting/add", p);
+      window.location.href = "/pri/meeting";
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="add">
+  {#await Promise.all([promise1, promise2]) then [profiles, rooms]}
+    <div class="d-flex mt-2 justify-content-center">
+      <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+        <RadioInline
+          bind:value={p.schedule_type}
+          options={SCHEDULE_TYPE_OPTIONS}
+        />
+        <Text name="name" label="Name" bind:value={p.name} required={true} />
+        <Switch name="hidden" label="Hidden" bind:value={p.hidden} />
+
+        {#if warning}
+          <Warning>
+            The add request is not accepted. Please check your inputs.
+          </Warning>
+        {/if}
+
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <Cancel on:click={cancel} />
+          <Submit label="Add" />
+        </div>
+      </form>
+    </div>
+  {:catch}
+    <Warning>Something went wrong</Warning>
+  {/await}
+</section>
