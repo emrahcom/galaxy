a7d32f4628855c10d182a0947b565860ae555f5b
emrah <emrah.com@gmail.com>
2022-03-08 20:13:24 +0300

    feat, ui: schedule type options

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/meeting.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/meeting.ts
@@ -1,6 +1,13 @@
 import { getById } from "$lib/api";
 
 // -----------------------------------------------------------------------------
+export const SCHEDULE_TYPE_OPTIONS = [
+  ["ephemeral", "ephemeral"],
+  ["permenant", "permenant"],
+  ["scheduled", "scheduled"],
+];
+
+// -----------------------------------------------------------------------------
 export async function join(uuid: string) {
   const link = await getById("/api/pri/meeting/get/link", uuid);
   window.location.replace(link.url);
