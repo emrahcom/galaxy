229b04b0741af9e28030f8aeef016e9b90339af7
emrah <emrah.com@gmail.com>
2022-03-09 20:29:36 +0300

    refactor, ui: add required to select component

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-select.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-select.svelte
@@ -3,13 +3,21 @@
   export let id: string;
   export let label: string;
   export let options: string[][];
+  export let required = true;
   export let value: string;
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <div class="form-floating my-3">
-  <select class="form-select" {id} bind:value aria-label={label} {disabled}>
-    <option class="bg-body" disabled />
+  <select
+    class="form-select"
+    {id}
+    bind:value
+    aria-label={label}
+    {disabled}
+    {required}
+  >
+    <option class="bg-body" />
 
     {#each options as opt}
       <option value={opt[0]}>{opt[1]}</option>
