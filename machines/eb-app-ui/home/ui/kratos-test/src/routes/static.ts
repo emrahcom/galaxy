560b962434f8ab9de3d0afb51c4f04879bd8dd82
emrah <emrah.com@gmail.com>
2022-03-10 22:07:31 +0300

    refactor, kratos: add missing routes

--- machines/eb-app-ui/home/ui/kratos-test/src/routes/static.ts
+++ machines/eb-app-ui/home/ui/kratos-test/src/routes/static.ts
@@ -9,10 +9,12 @@ export const registerStaticRoutes: RouteRegistrator = (app) => {
   app.use('/', express.static('node_modules/normalize.css'))
 
   // galaxy redirections
+  app.get('/id/error', (_, res) => res.redirect('/error'))
   app.get('/id/login', (_, res) => res.redirect('/login'))
   app.get('/id/logout', (_, res) => res.redirect('/logout'))
   app.get('/id/recovery', (_, res) => res.redirect('/recovery'))
   app.get('/id/registration', (_, res) => res.redirect('/registration'))
   app.get('/id/settings', (_, res) => res.redirect('/settings'))
   app.get('/id/verification', (_, res) => res.redirect('/verification'))
+  app.get('/id/welcome', (_, res) => res.redirect('/welcome'))
 }
