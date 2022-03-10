a25489e7916a1303ce67b14417875438e23647bc
emrah <emrah.com@gmail.com>
2022-03-10 23:35:02 +0300

    fix, kratos: redirects

--- machines/eb-app-ui/home/ui/kratos-test/src/routes/static.ts
+++ machines/eb-app-ui/home/ui/kratos-test/src/routes/static.ts
@@ -3,18 +3,53 @@ import express from 'express'
 
 import { RouteRegistrator } from '../pkg'
 
+
 export const registerStaticRoutes: RouteRegistrator = (app) => {
   app.get('/theme.css', expressHandler())
   app.use('/', express.static('public'))
   app.use('/', express.static('node_modules/normalize.css'))
 
   // galaxy redirections
-  app.get('/id/error', (_, res) => res.redirect('/error'))
-  app.get('/id/login', (_, res) => res.redirect('/login'))
-  app.get('/id/logout', (_, res) => res.redirect('/logout'))
-  app.get('/id/recovery', (_, res) => res.redirect('/recovery'))
-  app.get('/id/registration', (_, res) => res.redirect('/registration'))
-  app.get('/id/settings', (_, res) => res.redirect('/settings'))
-  app.get('/id/verification', (_, res) => res.redirect('/verification'))
-  app.get('/id/welcome', (_, res) => res.redirect('/welcome'))
+  const url = require('url')
+
+  app.get('/id/error', (req, res) => {
+    const parsed = url.parse(req.url)
+    const query = parsed.query
+    res.redirect((query ? `/error?${query}` : `/error`)
+  })
+  app.get('/id/login', (req, res) => {
+    const parsed = url.parse(req.url)
+    const query = parsed.query
+    res.redirect((query ? `/login?${query}` : `/login`)
+  })
+  app.get('/id/logout', (req, res) => {
+    const parsed = url.parse(req.url)
+    const query = parsed.query
+    res.redirect((query ? `/logout?${query}` : `/logout`)
+  })
+  app.get('/id/recovery', (req, res) => {
+    const parsed = url.parse(req.url)
+    const query = parsed.query
+    res.redirect((query ? `/recovery?${query}` : `/recovery`)
+  })
+  app.get('/id/registration', (req, res) => {
+    const parsed = url.parse(req.url)
+    const query = parsed.query
+    res.redirect((query ? `/registration?${query}` : `/registration`)
+  })
+  app.get('/id/settings', (req, res) => {
+    const parsed = url.parse(req.url)
+    const query = parsed.query
+    res.redirect((query ? `/settings?${query}` : `/settings`)
+  })
+  app.get('/id/verification', (req, res) => {
+    const parsed = url.parse(req.url)
+    const query = parsed.query
+    res.redirect((query ? `/verification?${query}` : `/verification`)
+  })
+  app.get('/id/welcome', (req, res) => {
+    const parsed = url.parse(req.url)
+    const query = parsed.query
+    res.redirect((query ? `/welcome?${query}` : `/welcome`)
+  })
 }
