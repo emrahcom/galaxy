4aad0e7de12770d4de4ad51715f1e629bbbdc04c
emrah <emrah.com@gmail.com>
2022-03-12 21:56:09 +0300

    refactor, ui: throw error when there is no url for room

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/room.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/room.ts
@@ -3,5 +3,8 @@ import { getById } from "$lib/api";
 // -----------------------------------------------------------------------------
 export async function join(uuid: string) {
   const link = await getById("/api/pri/room/get/link", uuid);
+
+  if (!link.url) throw new Error("URL not found");
+
   window.location.replace(link.url);
 }
