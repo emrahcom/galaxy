9b2d479affe7086e82ddc7cde1548141d01344d5
emrah <emrah.com@gmail.com>
2022-03-13 15:51:58 +0300

    feat, ui: disabled check for domain in room actions

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/del.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/del.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { FORM_WIDTH } from "$lib/config";
-  import { actionById } from "$lib/api";
-  import type { Room } from "$lib/types";
+  import { actionById, getById } from "$lib/api";
+  import type { Domain, Room } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
   import Submit from "$lib/components/common/button-submit.svelte";
   import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
@@ -12,6 +12,12 @@
   export let p: Room;
 
   let warning = false;
+
+  const pr = getById("/api/pri/domain/get", p.domain_id).then(
+    (item: Domain) => {
+      if (!item.enabled) p.domain_name = `${p.domain_name} - DISABLED`;
+    },
+  );
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -32,33 +38,37 @@
 
 <!-- -------------------------------------------------------------------------->
 <section id="del">
-  <div class="d-flex mt-2 justify-content-center">
-    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
-      <Text name="name" label="Name" value={p.name} readonly={true} />
-      <Text
-        name="domain"
-        label="Jitsi Domain"
-        value={p.domain_name}
-        readonly={true}
-      />
-      <Switch
-        name="has_suffix"
-        label="Enable unpredictable room name generator"
-        value={p.has_suffix}
-        disabled={true}
-      />
+  {#await pr then _domain}
+    <div class="d-flex mt-2 justify-content-center">
+      <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+        <Text name="name" label="Name" value={p.name} readonly={true} />
+        <Text
+          name="domain"
+          label="Jitsi Domain"
+          value={p.domain_name}
+          readonly={true}
+        />
+        <Switch
+          name="has_suffix"
+          label="Enable unpredictable room name generator"
+          value={p.has_suffix}
+          disabled={true}
+        />
 
-      {#if warning}
-        <Warning>The delete request is not accepted.</Warning>
-      {:else}
-        <Warning>All meetings on this room will be deleted.</Warning>
-      {/if}
+        {#if warning}
+          <Warning>The delete request is not accepted.</Warning>
+        {:else}
+          <Warning>All meetings on this room will be deleted.</Warning>
+        {/if}
 
-      <div class="d-flex gap-5 mt-5 justify-content-center">
-        <Cancel on:click={cancel} />
-        <SubmitBlocker />
-        <Submit label="Delete" />
-      </div>
-    </form>
-  </div>
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <Cancel on:click={cancel} />
+          <SubmitBlocker />
+          <Submit label="Delete" />
+        </div>
+      </form>
+    </div>
+  {:catch}
+    <Warning>Something went wrong</Warning>
+  {/await}
 </section>
