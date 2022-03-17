d31699d7a7cd7a8bafe7ccdee56f19d75106ee1f
emrah <emrah.com@gmail.com>
2022-03-17 20:08:25 +0300

    feat, ui: domain invite enable routing

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/invite/enable/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Enable from "$lib/components/pri/domain-invite/enable.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/domain/invite/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Enable this partner key" />
+
+{#await pr then invite}
+  <Enable p={invite} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
