0ead67938acf065fffa54cc87c3968cb0ac8011e
emrah <emrah.com@gmail.com>
2022-03-17 22:15:11 +0300

    feat, ui: routing for add partner domain

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/partnership/add/[code].svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getByCode } from "$lib/api";
+  import Add from "$lib/components/pri/domain-partnership/add.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const code = $page.params.code;
+
+  const pr = getByCode("/api/pri/domain/invite/get/bycode", code);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Add a partner domain" />
+
+{#await pr then invite}
+  <Add {invite} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
