6ea17d173d6c817b14cc1048ddb0c894c4b2163e
emrah <emrah.com@gmail.com>
2022-03-17 16:50:38 +0300

    feat, ui: domain invite delete

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-invite/del.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { DomainInvite } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: DomainInvite;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/domain/invite/${p.domain_id}`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/domain/invite/del", p.id);
+      window.location.href = `/pri/domain/invite/${p.domain_id}`;
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="del">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text name="name" label="Name" bind:value={p.name} readonly={true} />
+      <Text
+        name="name"
+        label="Jitsi Domain Name"
+        value={p.domain_name}
+        readonly={true}
+      />
+      <Text
+        name="url"
+        label="Jitsi Domain URL"
+        value={p.domain_url}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>
+          The delete request is not accepted. Please check your inputs.
+        </Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Delete" />
+      </div>
+    </form>
+  </div>
+</section>
---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/invite/del/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/domain-invite/del.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/domain/invite/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Delete this partner key" />
+
+{#await pr then invite}
+  <Del p={invite} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
