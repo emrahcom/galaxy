70d72d7eb829a0ab407fbb175cca0cfabcc239f7
emrah <emrah.com@gmail.com>
2022-03-17 13:43:07 +0300

    feat, ui: form-hidden component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-hidden.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let name: string;
+  export let value: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<input
+  type="hidden"
+  id={name}
+  {name}
+  bind:value
+/>
