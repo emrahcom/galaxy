c61d15ff89b4044e45d98e6effd3eb0d4fa6d10d
emrah <emrah.com@gmail.com>
2022-03-17 13:52:52 +0300

    refactor, ui: remove form-hidden, no need it since there is no direct post

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-hidden.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let name: string;
-  export let value: string;
-</script>
-
-<!-- -------------------------------------------------------------------------->
-<input type="hidden" id={name} {name} bind:value />
