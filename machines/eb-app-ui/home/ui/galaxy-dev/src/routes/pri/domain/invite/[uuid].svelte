2bc43850aa9780f9fdb1886ca44ab998afedbcc9
emrah <emrah.com@gmail.com>
2022-03-17 13:18:24 +0300

    refactor, ui: move subheader outside await

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/invite/[uuid].svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/invite/[uuid].svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import { getById, listById } from "$lib/api";
+  import type { Domain } from "$lib/types";
   import List from "$lib/components/pri/domain-invite/list.svelte";
   import Subheader from "$lib/components/common/subheader-pri-list.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   const uuid = $page.params.uuid;
+  let domain_name = "";
 
-  const pr1 = getById("/api/pri/domain/get", uuid);
+  const pr1 = getById("/api/pri/domain/get", uuid).then((item: Domain) => {
+    domain_name = item.name;
+  });
   const pr2 = listById("/api/pri/domain/invite/list", uuid, 100);
 </script>
 
 <!-- -------------------------------------------------------------------------->
-{#await Promise.all([pr1, pr2]) then [domain, invites]}
-  <Subheader
-    subheader="The partner keys for {domain.name}"
-    href="/pri/domain/invite/add/{uuid}"
-  />
+<Subheader
+  subheader="The partner keys for {domain_name}"
+  href="/pri/domain/invite/add/{uuid}"
+/>
 
+{#await Promise.all([pr1, pr2]) then [_domain, invites]}
   <List {invites} />
 {:catch}
   <Warning>Something went wrong</Warning>
