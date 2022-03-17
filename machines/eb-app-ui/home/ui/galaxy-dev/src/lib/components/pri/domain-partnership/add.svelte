d3a66e964d41dd74a2c2a0ada6812047f0f6cebd
emrah <emrah.com@gmail.com>
2022-03-17 23:56:54 +0300

    refactor, ui: add by code link

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partnership/add.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partnership/add.svelte
@@ -24,7 +24,7 @@
   async function onSubmit() {
     try {
       warning = false;
-      await action("/api/pri/domain/partnership/add", p);
+      await action("/api/pri/domain/partnership/add/bycode", p);
       window.location.href = `/pri/domain`;
     } catch {
       warning = true;
