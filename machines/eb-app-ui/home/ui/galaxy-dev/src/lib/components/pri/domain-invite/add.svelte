a5294e947c63f6604ae66ad790d3ee7baa91fe07
emrah <emrah.com@gmail.com>
2022-03-17 13:50:05 +0300

    feat, ui: domain invite add

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-invite/add.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { action } from "$lib/api";
+  import type { Domain } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let uuid: string;
+  export let domain: Domain;
+
+  let warning = false;
+  let p = {
+    name: "",
+    domain_id: uuid,
+  };
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/domain/invite/${uuid}`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await action("/api/pri/room/add", p);
+      window.location.href = `/pri/domain/invite/${uuid}`;
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="add">
+  {#await pr then domain}
+    <div class="d-flex mt-2 justify-content-center">
+      <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+        <Text name="name" label="Name" bind:value={p.name} required={true} />
+        <Text
+          name="name"
+          label="Jitsi Domain Name"
+          value={domain.name}
+          readonly={true}
+        />
+        <Text
+          name="url"
+          label="Jitsi Domain URL"
+          value={domain.domain_attr.url}
+          readonly={true}
+        />
+
+        {#if warning}
+          <Warning>
+            The add request is not accepted. Please check your inputs.
+          </Warning>
+        {/if}
+
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <Cancel on:click={cancel} />
+          <SubmitBlocker />
+          <Submit label="Add" />
+        </div>
+      </form>
+    </div>
+  {:catch}
+    <Warning>Something went wrong</Warning>
+  {/await}
+</section>
---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/invite/add/[uuid].svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Add from "$lib/components/pri/domain-invite/add.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+
+  const uuid = $page.params.uuid;
+
+  const pr = getById("/api/pri/domain/get", uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Add a partner key" />
+
+{#await pr then domain}
+<Add {uuid} {domain} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
