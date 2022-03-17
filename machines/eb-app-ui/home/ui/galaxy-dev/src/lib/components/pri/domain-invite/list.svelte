6256fa9dedcdc12b1c34a1b855a5ba12bc3b05be
emrah <emrah.com@gmail.com>
2022-03-17 14:23:31 +0300

    feat, ui: expire date into invite card

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-invite/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-invite/list.svelte
@@ -15,6 +15,7 @@
         <div class="card h-100 {!p.enabled ? 'border-danger' : ''}">
           <div class="card-body text-center">
             <h5 class="card-title text-muted">{p.name}</h5>
+            <p class="card-text text-muted small">{p.expired_at}</p>
             <p class="card-text text-muted">
               /pri/domain/partnership/add/{p.code}
             </p>
