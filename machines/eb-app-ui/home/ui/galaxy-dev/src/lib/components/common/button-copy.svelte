aa2c5cbc11e64c12cb4751e8e8c20c1f8a52fe2a
emrah <emrah.com@gmail.com>
2022-03-17 17:33:55 +0300

    feat, ui: copy button component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-copy.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let label = "copy";
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<button class="btn btn-sm btn-outline-primary" type="button" on:click>
+  {label}
+</button>
