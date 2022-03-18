c5a7d73df26e10c3f692dce3648cd41318dc5515
emrah <emrah.com@gmail.com>
2022-03-18 17:51:06 +0300

    feat, ui: delete partnership

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partnership/del.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partnership/del.svelte
@@ -1,19 +1,16 @@
 <script lang="ts">
   import { FORM_WIDTH } from "$lib/config";
-  import { action } from "$lib/api";
-  import type { DomainInviteReduced } from "$lib/types";
+  import { actionById } from "$lib/api";
+  import type { DomainPartnership } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
   import Submit from "$lib/components/common/button-submit.svelte";
   import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
   import Text from "$lib/components/common/form-text.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
-  export let invite: DomainInviteReduced;
+  export let p: DomainPartnership;
 
   let warning = false;
-  let p = {
-    code: invite.code,
-  };
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -24,8 +21,8 @@
   async function onSubmit() {
     try {
       warning = false;
-      await action("/api/pri/domain/partnership/add/bycode", p);
-      window.location.href = `/pri/domain`;
+      await actionById("/api/pri/domain/partnership/del", p.id);
+      window.location.replace(`/pri/domain`);
     } catch {
       warning = true;
     }
@@ -33,30 +30,30 @@
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<section id="add">
+<section id="del">
   <div class="d-flex mt-2 justify-content-center">
     <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
       <Text
         name="name"
         label="Jitsi Domain Name"
-        value={invite.domain_name}
+        value={p.domain_name}
         readonly={true}
       />
       <Text
         name="url"
         label="Jitsi Domain URL"
-        value={invite.domain_url}
+        value={p.domain_url}
         readonly={true}
       />
 
       {#if warning}
-        <Warning>The add request is not accepted.</Warning>
+        <Warning>The delete request is not accepted.</Warning>
       {/if}
 
       <div class="d-flex gap-5 mt-5 justify-content-center">
         <Cancel on:click={cancel} />
         <SubmitBlocker />
-        <Submit label="Add" />
+        <Submit label="Delete" />
       </div>
     </form>
   </div>
