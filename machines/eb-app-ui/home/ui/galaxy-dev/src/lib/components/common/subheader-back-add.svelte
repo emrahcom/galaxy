9070ff76f53349c0fec9f4a810ea4028223585aa
emrah <emrah.com@gmail.com>
2022-03-18 16:00:47 +0300

    feat, ui: subheader-back-add

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/subheader-back-add.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let hrefAdd: string;
+  export let hrefBack: string;
+  export let subheader: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="subheader">
+  <div class="d-flex">
+    <h3 class="text-muted my-auto">
+      {subheader}
+
+      <a
+        class="btn btn-outline-secondary ms-auto me-2 my-auto"
+        role="button"
+        {hrefBack}
+      >
+        <span class="bi bi-box-arrow-in-left" />
+      </a>
+    </h3>
+
+    <a
+      class="btn btn-outline-secondary ms-auto me-2 my-auto"
+      role="button"
+      {hrefAdd}
+    >
+      <span class="bi bi-plus-lg" />
+    </a>
+  </div>
+</section>
