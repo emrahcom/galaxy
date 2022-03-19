06d49b41e94a5d77383c15796708627d8803858f
emrah <emrah.com@gmail.com>
2022-03-19 18:34:55 +0300

    feat, ui: the disable routing for domain partner

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/partner/disable/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Disable from "$lib/components/pri/domain-partner/disable.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/domain/partner/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Disable this partner" />
+
+{#await pr then partner}
+  <Disable p={partner} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
