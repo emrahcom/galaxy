75e8c311e6433e709f6943bd110034d48359d79d
emrah <emrah.com@gmail.com>
2022-03-19 16:59:45 +0300

    refactor, ui: rename as subheader-center

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/partner/[domain_uuid].svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById, listById } from "$lib/api";
+  import type { Domain } from "$lib/types";
+  import List from "$lib/components/pri/domain-invite/list.svelte";
+  import Subheader from "$lib/components/common/subheader-back-add.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const domainId = $page.params.domain_uuid;
+  let domainName = "";
+
+  const pr1 = getById("/api/pri/domain/get", domainId).then((item: Domain) => {
+    domainName = item.name;
+  });
+  const pr2 = listById("/api/pri/domain/invite/list/bydomain", domainId, 100);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader
+  subheader="The partner keys for {domainName}"
+  hrefBack="/pri/domain"
+  hrefAdd="/pri/domain/invite/add/{domainId}"
+/>
+
+{#await Promise.all([pr1, pr2]) then [_domain, invites]}
+  <List {invites} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
