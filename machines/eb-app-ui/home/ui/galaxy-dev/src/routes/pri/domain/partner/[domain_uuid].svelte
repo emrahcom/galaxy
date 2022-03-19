6a23af12c676d3283c1cefa0e4447f55466a7ae4
emrah <emrah.com@gmail.com>
2022-03-19 17:26:12 +0300

    feat, ui: domain partner list routing

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/partner/[domain_uuid].svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/partner/[domain_uuid].svelte
@@ -2,8 +2,8 @@
   import { page } from "$app/stores";
   import { getById, listById } from "$lib/api";
   import type { Domain } from "$lib/types";
-  import List from "$lib/components/pri/domain-invite/list.svelte";
-  import Subheader from "$lib/components/common/subheader-back-add.svelte";
+  import List from "$lib/components/pri/domain-partner/list.svelte";
+  import Subheader from "$lib/components/common/subheader-back.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   const domainId = $page.params.domain_uuid;
@@ -12,18 +12,17 @@
   const pr1 = getById("/api/pri/domain/get", domainId).then((item: Domain) => {
     domainName = item.name;
   });
-  const pr2 = listById("/api/pri/domain/invite/list/bydomain", domainId, 100);
+  const pr2 = listById("/api/pri/domain/partner/list/bydomain", domainId, 100);
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <Subheader
-  subheader="The partner keys for {domainName}"
+  subheader="Partners for {domainName}"
   hrefBack="/pri/domain"
-  hrefAdd="/pri/domain/invite/add/{domainId}"
 />
 
-{#await Promise.all([pr1, pr2]) then [_domain, invites]}
-  <List {invites} />
+{#await Promise.all([pr1, pr2]) then [_domain, partners]}
+  <List {partner} />
 {:catch}
   <Warning>Something went wrong</Warning>
 {/await}
