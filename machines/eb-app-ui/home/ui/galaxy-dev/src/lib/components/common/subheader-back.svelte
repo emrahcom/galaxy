3e18d8db3fe51e8888940aed813005591dc59e35
emrah <emrah.com@gmail.com>
2022-03-19 16:56:34 +0300

    feat, ui: add subheader-back component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/subheader-back.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let hrefBack: string;
+  export let subheader: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="subheader">
+  <div class="d-flex">
+    <h3 class="text-muted my-auto">
+      {subheader}
+
+      <a
+        class="btn btn-lg btn-outline-secondary border-0 ms-auto me-2 my-auto"
+        role="button"
+        href={hrefBack}
+      >
+        <span class="bi bi-arrow-return-left" />
+      </a>
+    </h3>
+  </div>
+</section>
