dcb5b481478ff1bb82184d3fea1f49881adb6a6e
emrah <emrah.com@gmail.com>
2022-03-19 17:25:39 +0300

    feat, ui: domain partner list page

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partner/list.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import type { DomainPartner } from "$lib/types";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Disable from "$lib/components/common/link-disable.svelte";
+  import Enable from "$lib/components/common/link-enable.svelte";
+
+  export let partners: DomainPartner[];
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="list">
+  <div class="row mx-auto mt-2 g-3">
+    {#each partner as p}
+      <div class="col-md-6 col-xl-4">
+        <div class="card h-100 {!p.enabled ? 'border-danger' : ''}">
+          <div class="card-body text-center">
+            <h5 class="card-title text-muted">{p.profile_name}</h5>
+
+            <p class="card-text text-muted">
+              {p.profile_email}
+            </p>
+          </div>
+
+          <div class="card-footer bg-body border-0 text-center">
+            <Del href="/pri/domain/partner/del/{p.id}" />
+
+            {#if p.enabled}
+              <Disable href="/pri/domain/partner/disable/{p.id}" />
+            {:else}
+              <Enable href="/pri/domain/partner/enable/{p.id}" />
+            {/if}
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
+</section>
