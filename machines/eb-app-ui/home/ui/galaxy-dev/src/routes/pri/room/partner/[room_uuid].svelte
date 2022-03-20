6fe8107ede29e9286bf016be762682b7b7ecde02
emrah <emrah.com@gmail.com>
2022-03-20 23:51:08 +0300

    feat, ui: room partner list routing

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/room/partner/[room_uuid].svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById, listById } from "$lib/api";
+  import type { Room } from "$lib/types";
+  import List from "$lib/components/pri/room-partner/list.svelte";
+  import Subheader from "$lib/components/common/subheader-back.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const roomId = $page.params.room_uuid;
+  let roomName = "";
+
+  const pr1 = getById("/api/pri/room/get", roomId).then((item: Room) => {
+    roomName = item.name;
+  });
+  const pr2 = listById("/api/pri/room/partner/list/byroom", roomId, 100);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Partners of {roomName}" hrefBack="/pri/room" />
+
+{#await Promise.all([pr1, pr2]) then [_room, partners]}
+  <List {partners} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
