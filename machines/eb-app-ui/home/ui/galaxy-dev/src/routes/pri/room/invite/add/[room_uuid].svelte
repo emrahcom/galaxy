25ef41a0d5c8bde6113984a6ac955a67d735781f
emrah <emrah.com@gmail.com>
2022-03-20 23:47:48 +0300

    feat, ui: room add routing

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/room/invite/add/[room_uuid].svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Add from "$lib/components/pri/room-invite/add.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const roomId = $page.params.room_uuid;
+
+  const pr = getById("/api/pri/room/get", roomId);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Add a partner key" />
+
+{#await pr then room}
+  <Add {room} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
