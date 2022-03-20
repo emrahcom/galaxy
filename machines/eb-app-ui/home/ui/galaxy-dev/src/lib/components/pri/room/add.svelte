a06ff8b7fd5e566e503b377b0537e7eb974aa6ce
emrah <emrah.com@gmail.com>
2022-03-20 11:35:48 +0300

    refactor, ui: domain listing while adding room

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/add.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/add.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { FORM_WIDTH } from "$lib/config";
   import { action, list } from "$lib/api";
-  import type { Domain } from "$lib/types";
+  import type { DomainReduced } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
   import Select from "$lib/components/common/form-select.svelte";
   import Submit from "$lib/components/common/button-submit.svelte";
@@ -17,17 +17,19 @@
     has_suffix: false,
   };
 
-  const pr = list("/api/pri/domain/list", 100).then((items: Domain[]) => {
-    const enableds = items
-      .filter((i) => i.enabled)
-      .sort((i, j) => (i.updated_at > j.updated_at ? -1 : 1));
-    if (enableds[0]) p.domain_id = enableds[0].id;
+  const pr = list("/api/pri/domain/list", 100).then(
+    (items: DomainReduced[]) => {
+      const enableds = items
+        .filter((i) => i.enabled)
+        .sort((i, j) => (i.updated_at > j.updated_at ? -1 : 1));
+      if (enableds[0]) p.domain_id = enableds[0].id;
 
-    return items.map((i) => [
-      i.id,
-      `${i.name}${i.enabled ? "" : " - DISABLED"}`,
-    ]);
-  });
+      return items.map((i) => [
+        i.id,
+        `${i.name}${i.enabled ? "" : " - DISABLED"}`,
+      ]);
+    },
+  );
 
   // ---------------------------------------------------------------------------
   function cancel() {
