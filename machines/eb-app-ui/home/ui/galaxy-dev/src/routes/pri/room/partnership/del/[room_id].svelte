7ada9c1fc936ebd65702bb7022428a25462e28ba
emrah <emrah.com@gmail.com>
2022-03-20 23:52:57 +0300

    feat, ui: room partnership del routing

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/room/partnership/del/[room_id].svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/room-partnership/del.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById(
+    "/api/pri/room/partnership/get/byroom",
+    $page.params.room_id,
+  );
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Delete this partner room" />
+
+{#await pr then partnership}
+  <Del p={partnership} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
