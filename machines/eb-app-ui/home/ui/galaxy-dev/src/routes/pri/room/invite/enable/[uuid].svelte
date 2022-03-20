344960a0312eafcae5b4b50bae858d700f9506a9
emrah <emrah.com@gmail.com>
2022-03-20 23:48:33 +0300

    feat, ui: room enable routing

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/room/invite/enable/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Enable from "$lib/components/pri/room-invite/enable.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/room/invite/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Enable this partner key" />
+
+{#await pr then invite}
+  <Enable p={invite} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
