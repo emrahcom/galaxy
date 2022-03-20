083f3e0722ba2ef3c22c8eae20eaa6e535a098af
emrah <emrah.com@gmail.com>
2022-03-20 22:03:10 +0300

    feat, ui: buttons depending on room ownership in list

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/list.svelte
@@ -18,19 +18,25 @@
           <div class="card-body text-center">
             <h5 class="card-title text-muted">{p.name}</h5>
             <p class="card-text text-muted">{p.domain_name}</p>
+            <p class="card-text text-muted small">{p.ownership}</p>
           </div>
 
           <div class="card-footer bg-body border-0 text-center">
-            <Del href="/pri/room/del/{p.id}" />
+            {#if p.ownership === "private"}
+              <Del href="/pri/room/del/{p.id}" />
 
-            {#if p.enabled}
-              <Disable href="/pri/room/disable/{p.id}" />
-            {:else}
-              <Enable href="/pri/room/enable/{p.id}" />
+              {#if p.enabled}
+                <Disable href="/pri/room/disable/{p.id}" />
+              {:else}
+                <Enable href="/pri/room/enable/{p.id}" />
+              {/if}
+
+              <Update href="/pri/room/update/{p.id}" />
+              <Join href="/pri/room/join/{p.id}" />
+            {:else if p.ownership === "partner"}
+              <Del href="/pri/room/del/{p.id}" />
+              <Join href="/pri/room/join/{p.id}" />
             {/if}
-
-            <Update href="/pri/room/update/{p.id}" />
-            <Join href="/pri/room/join/{p.id}" />
           </div>
         </div>
       </div>
