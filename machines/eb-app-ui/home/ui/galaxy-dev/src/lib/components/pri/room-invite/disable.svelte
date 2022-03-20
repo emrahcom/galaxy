71b341a8589f46728fda621f383e1d1edf56c99f
emrah <emrah.com@gmail.com>
2022-03-21 00:26:52 +0300

    feat, ui: room invite disable component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-invite/disable.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { RoomInvite } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: RoomInvite;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/room/invite/${p.room_id}`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/room/invite/disable", p.id);
+      window.location.href = `/pri/room/invite/${p.room_id}`;
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="disable">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text name="name" label="Name" value={p.name} readonly={true} />
+      <Text
+        name="room_name"
+        label="Room"
+        value={p.room_name}
+        readonly={true}
+      />
+      <Text
+        name="domain_name"
+        label="Jitsi Domain Name"
+        value={p.domain_name}
+        readonly={true}
+      />
+      <Text
+        name="domain_url"
+        label="Jitsi Domain URL"
+        value={p.domain_url}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The disable request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Disable" />
+      </div>
+    </form>
+  </div>
+</section>
