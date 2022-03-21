74d02bb44b13b5dd938d4c48abcc01244ca17324
emrah <emrah.com@gmail.com>
2022-03-21 17:13:50 +0300

    feat, api: add room-partnersjip into the main routing

--- machines/eb-app-api/home/api/galaxy/index-pri.ts
+++ machines/eb-app-api/home/api/galaxy/index-pri.ts
@@ -20,6 +20,7 @@ import profile from "./lib/pri/profile.ts";
 import room from "./lib/pri/room.ts";
 import roomInvite from "./lib/pri/room-invite.ts";
 import roomPartner from "./lib/pri/room-partner.ts";
+import roomPartnership from "./lib/pri/room-partnership.ts";
 
 const PRE = "/api/pri";
 
@@ -55,6 +56,8 @@ async function route(
     return await roomInvite(req, path, identityId);
   } else if (path.match(`^${PRE}/room/partner`)) {
     return await roomPartner(req, path, identityId);
+  } else if (path.match(`^${PRE}/room/partnership`)) {
+    return await roomPartnership(req, path, identityId);
   } else if (path.match(`^${PRE}/room/`)) {
     return await room(req, path, identityId);
   } else {
