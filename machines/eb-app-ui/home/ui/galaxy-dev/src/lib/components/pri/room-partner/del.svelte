5c35e62d3af9291cb6a0d5ab542cdbaf419c0c83
emrah <emrah.com@gmail.com>
2022-03-21 19:04:10 +0300

    feat, ui: add the del room-partner component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-partner/del.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { RoomPartner } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: RoomPartner;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/room/partner/${p.room_id}`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/room/partner/del", p.id);
+      window.location.replace(`/pri/room/partner/${p.room_id}`);
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="del">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="name"
+        label="Partner Name"
+        value={p.profile_name}
+        readonly={true}
+      />
+      <Text
+        name="email"
+        label="Partner Email"
+        value={p.profile_email}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The delete request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Delete" />
+      </div>
+    </form>
+  </div>
+</section>
