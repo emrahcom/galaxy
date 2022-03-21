d8ba2b24084a508ecfdc1e485816f92c9023cbdf
emrah <emrah.com@gmail.com>
2022-03-21 17:53:42 +0300

    feat, ui: add the room-partnership del component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-partnership/del.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { RoomPartnership } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: RoomPartnership;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/room`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/room/partnership/del", p.id);
+      window.location.replace(`/pri/room`);
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="del">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="room_name"
+        label="Room"
+        value={invite.room_name}
+        readonly={true}
+      />
+      <Text
+        name="domain_name"
+        label="Jitsi Domain Name"
+        value={invite.domain_name}
+        readonly={true}
+      />
+      <Text
+        name="domain_url"
+        label="Jitsi Domain URL"
+        value={invite.domain_url}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The delete request is not accepted.</Warning>
+      {:else}
+        <Warning>
+          All meetings on this room will be disabled.
+        </Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Delete" />
+      </div>
+    </form>
+  </div>
+</section>
