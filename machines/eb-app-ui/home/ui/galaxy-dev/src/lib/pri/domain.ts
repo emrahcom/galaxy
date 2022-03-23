c134b5f12b7d95812479e11175278317868c01f8
emrah <emrah.com@gmail.com>
2022-03-23 13:07:46 +0300

    refactor, ui: remove unnecessary domainsAsOptions function

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/domain.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/domain.ts
@@ -1,29 +1,4 @@
-import { list } from "$lib/api";
-
-// -----------------------------------------------------------------------------
 export const AUTH_TYPE_OPTIONS = [
   ["none", "anonymous"],
   ["token", "token"],
 ];
-
-// -----------------------------------------------------------------------------
-export async function domainsAsOptions() {
-  interface Option {
-    id: string;
-    name: string;
-    enabled: boolean;
-  }
-
-  const options: Option[] = [];
-
-  const priDomains = await list("/api/pri/domain/list");
-  for (const p of priDomains) {
-    options.push({
-      id: p.id,
-      name: p.name,
-      enabled: p.enabled,
-    });
-  }
-
-  return options;
-}
