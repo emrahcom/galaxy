2866fab0d9dc709250d383ebe11e201442fc206c
emrah <emrah.com@gmail.com>
2022-03-24 21:58:22 +0300

    feat, ui: meeting invite add component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-invite/add.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { action } from "$lib/api";
+  import type { Meeting } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let meeting: Meeting;
+
+  const date = new Date();
+
+  let warning = false;
+  let p = {
+    name: `invite-${date.getTime() % 10000000000}`,
+    meeting_id: meeting.id,
+  };
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/meeting/invite/${meeting.id}`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await action("/api/pri/meeting/invite/add", p);
+      window.location.href = `/pri/meeting/invite/${meeting.id}`;
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="add">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text name="name" label="Name" bind:value={p.name} required={true} />
+      <Text
+        name="meeting_name"
+        label="Meeting"
+        value={meeting.name}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>
+          The add request is not accepted. Please check your inputs.
+        </Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Add" />
+      </div>
+    </form>
+  </div>
+</section>
