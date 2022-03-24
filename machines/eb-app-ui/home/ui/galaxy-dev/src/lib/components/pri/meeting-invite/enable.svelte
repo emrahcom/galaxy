83484f17ae85ed57248bbc8c7397563e6b2b9e2e
emrah <emrah.com@gmail.com>
2022-03-24 21:58:58 +0300

    feat, ui: meeting invite enable component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-invite/enable.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { MeetingInvite } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: MeetingInvite;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/meeting/invite/${p.meeting_id}`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/meeting/invite/enable", p.id);
+      window.location.href = `/pri/meeting/invite/${p.meeting_id}`;
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="enable">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text name="name" label="Name" value={p.name} readonly={true} />
+      <Text
+        name="meeting_name"
+        label="Meeting"
+        value={p.meeting_name}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The enable request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Enable" />
+      </div>
+    </form>
+  </div>
+</section>
