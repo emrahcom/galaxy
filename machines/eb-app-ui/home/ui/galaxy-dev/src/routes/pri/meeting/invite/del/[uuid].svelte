def2d06bdde62cea9cc70ed5c8ab3b59a425d1e2
emrah <emrah.com@gmail.com>
2022-03-24 22:00:36 +0300

    feat, ui: meeting invite del rooting

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/invite/del/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/meeting-invite/del.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/meeting/invite/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Delete this participant key" />
+
+{#await pr then invite}
+  <Del p={invite} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
