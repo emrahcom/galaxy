f998ed02877df985b0015921d29bde478face6dc
emrah <emrah.com@gmail.com>
2022-03-24 22:00:24 +0300

    feat, ui: meeting invite add rooting

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/invite/add/[meeting_uuid].svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Add from "$lib/components/pri/meeting-invite/add.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const meetingId = $page.params.meeting_uuid;
+
+  const pr = getById("/api/pri/meeting/get", meetingId);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Add a participant key" />
+
+{#await pr then meeting}
+  <Add {meeting} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
