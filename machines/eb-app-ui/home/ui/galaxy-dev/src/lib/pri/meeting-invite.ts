4036ae9fde5fc80cd8144640bcd18fa02f982d48
emrah <emrah.com@gmail.com>
2022-03-24 22:32:35 +0300

    feat, ui: options for meeting invite form

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/meeting-invite.ts
@@ -0,0 +1,10 @@
+export const INVITE_TYPE_OPTIONS = [
+  ["audience", "audience"],
+  ["member", "member"],
+];
+
+// -----------------------------------------------------------------------------
+export const AFFILIATION_OPTIONS = [
+  ["guest", "guest"],
+  ["host", "host"],
+];
