cf2ef0f51764405a059980c634d5ea16ea8de68e
emrah <emrah.com@gmail.com>
2022-03-25 22:52:29 +0300

    feat, ui: del membership page

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/membership/del/[meeting_id].svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/meeting-membership/del.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById(
+    "/api/pri/meeting/membership/get/byroom",
+    $page.params.room_id,
+  );
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Unsubscribe from this meeting" />
+
+{#await pr then membership}
+  <Del p={membership} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
