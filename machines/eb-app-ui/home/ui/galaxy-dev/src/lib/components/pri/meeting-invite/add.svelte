6ab57e4311530824483696cf45827cf4e62a7d11
emrah <emrah.com@gmail.com>
2022-03-25 15:14:50 +0300

    refactor, ui: update button labels for add invite components

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-invite/add.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-invite/add.svelte
@@ -74,14 +74,14 @@
 
       {#if warning}
         <Warning>
-          The add request is not accepted. Please check your inputs.
+          The create request is not accepted. Please check your inputs.
         </Warning>
       {/if}
 
       <div class="d-flex gap-5 mt-5 justify-content-center">
         <Cancel on:click={cancel} />
         <SubmitBlocker />
-        <Submit label="Add" />
+        <Submit label="Create" />
       </div>
     </form>
   </div>
