2e4fa67249a809f7b012f522d1b35ca283b60d89
emrah <emrah.com@gmail.com>
2022-03-25 22:51:51 +0300

    feat, ui: add membership page

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/membership/add/[code].svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getByCode } from "$lib/api";
+  import Add from "$lib/components/pri/meeting-membership/add.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const code = $page.params.code;
+
+  const pr = getByCode("/api/pri/meeting/invite/get/bycode", code);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Subscribe to this meeting" />
+
+{#await pr then invite}
+  <Add {invite} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
