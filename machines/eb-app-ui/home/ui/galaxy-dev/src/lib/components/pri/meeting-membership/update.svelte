68ce3709663e10934777df678dbc27f5de79c5c4
emrah <emrah.com@gmail.com>
2022-03-26 20:59:25 +0300

    feat, ui: membership update

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-membership/update.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { action, list } from "$lib/api";
+  import type { MeetingMembership, Profile } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Select from "$lib/components/common/form-select.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Textarea from "$lib/components/common/form-textarea.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: MeetingMembership;
+
+  let warning = false;
+
+  const pr = list("/api/pri/profile/list", 100).then((items: Profile[]) => {
+    return items.map((i) => {
+      let desc: string;
+
+      if (i.email) {
+        desc = `${i.name} (${i.email})`;
+      } else {
+        desc = i.name;
+      }
+
+      return [i.id, desc];
+    });
+  });
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/meeting`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await action("/api/pri/meeting/membership/update", p);
+      window.location.href = `/pri/meeting`;
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="add">
+  {#await pr then profiles}
+    <div class="d-flex mt-2 justify-content-center">
+      <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+        <Text
+          name="meeting_name"
+          label="Meeting"
+          value={p.meeting_name}
+          readonly={true}
+        />
+        <Textarea
+          name="meeting_info"
+          label="Info"
+          value={p.meeting_info}
+          readonly={true}
+        />
+        <Select
+          id="profile_id"
+          label="Profile"
+          bind:value={p.profile_id}
+          options={profiles}
+        />
+
+        {#if warning}
+          <Warning>The update request is not accepted.</Warning>
+        {/if}
+
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <Cancel on:click={cancel} />
+          <SubmitBlocker />
+          <Submit label="Update" />
+        </div>
+      </form>
+    </div>
+  {:catch}
+    <Warning>Something went wrong</Warning>
+  {/await}
+</section>
