033ed52ee39c4fa093be4fdef34161fa981dba0d
emrah <emrah.com@gmail.com>
2022-03-27 21:15:14 +0300

    feat, ui: join_as in member list

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member/list.svelte
@@ -18,6 +18,7 @@
             <h5 class="card-title text-muted">{p.profile_name}</h5>
 
             <p class="card-text text-muted">{p.profile_email}</p>
+            <p class="card-text text-muted">{p.join_as}</p>
           </div>
 
           <div class="card-footer bg-body border-0 text-center">
