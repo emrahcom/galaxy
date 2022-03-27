99ea40561b36d47273a7c34b538cecc9ec020eae
emrah <emrah.com@gmail.com>
2022-03-27 20:46:55 +0300

    feat, ui: add the list member component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member/list.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import type { MeetingMember } from "$lib/types";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Disable from "$lib/components/common/link-disable.svelte";
+  import Enable from "$lib/components/common/link-enable.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let members: MeetingMember[];
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="list">
+  <div class="row mx-auto mt-2 g-3">
+    {#each members as p}
+      <div class="col-md-6 col-xl-4">
+        <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
+          <div class="card-body text-center">
+            <h5 class="card-title text-muted">{p.profile_name}</h5>
+
+            <p class="card-text text-muted">{p.profile_email}</p>
+          </div>
+
+          <div class="card-footer bg-body border-0 text-center">
+            <Del href="/pri/meeting/member/del/{p.id}" />
+
+            {#if p.enabled}
+              <Disable href="/pri/meeting/member/disable/{p.id}" />
+            {:else}
+              <Enable href="/pri/meeting/member/enable/{p.id}" />
+            {/if}
+          </div>
+        </div>
+      </div>
+    {:else}
+      <Warning>No member found for this meeting</Warning>
+    {/each}
+  </div>
+</section>
