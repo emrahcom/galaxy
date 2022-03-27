78f33b0d63120826954eaca53163e241dcf7628c
emrah <emrah.com@gmail.com>
2022-03-27 20:48:40 +0300

    feat, ui: add the disable member component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member/disable.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { MeetingMember } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: MeetingMember;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/meeting/member/${p.meeting_id}`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/meeting/member/disable", p.id);
+      window.location.href = `/pri/meeting/member/${p.meeting_id}`;
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="disable">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="name"
+        label="Member Name"
+        value={p.profile_name}
+        readonly={true}
+      />
+      <Text
+        name="email"
+        label="Member Email"
+        value={p.profile_email}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The disable request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Disable" />
+      </div>
+    </form>
+  </div>
+</section>
