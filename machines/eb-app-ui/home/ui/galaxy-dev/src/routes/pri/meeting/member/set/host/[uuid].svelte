7fb3ca40013323b7d3493fbfca0d61eee250adf7
emrah <emrah.com@gmail.com>
2022-03-27 23:52:21 +0300

    feat, ui: set host routing

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/member/set/host/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import SetHost from "$lib/components/pri/meeting-member/set-host.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/meeting/member/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Set this member as host" />
+
+{#await pr then member}
+  <SetHost p={member} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
