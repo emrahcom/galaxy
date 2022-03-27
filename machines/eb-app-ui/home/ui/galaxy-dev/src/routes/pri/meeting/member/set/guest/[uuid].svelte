f117bb9b1125712a0bdb5d976da032ddd6e7113a
emrah <emrah.com@gmail.com>
2022-03-27 23:52:33 +0300

    feat, ui: set guest routing

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/member/set/guest/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import SetGuest from "$lib/components/pri/meeting-member/set-guest.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/meeting/member/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Set this member as guest" />
+
+{#await pr then member}
+  <SetGuest p={member} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
