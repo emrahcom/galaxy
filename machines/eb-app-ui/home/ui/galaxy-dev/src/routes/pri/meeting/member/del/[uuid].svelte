ce7cf029147733065f7b87b4a456bebdf545cb02
emrah <emrah.com@gmail.com>
2022-03-27 20:57:13 +0300

    feat, ui: add routing for the meeting del

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/member/del/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/meeting-member/del.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/meeting/member/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Delete this member" />
+
+{#await pr then member}
+  <Del p={member} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
