bb39dce382e4c1a3e8ca18fb7c5067c229972ce0
emrah <emrah.com@gmail.com>
2022-03-28 14:12:58 +0300

    feat, api: listMeetingScheduleByMeeting

--- machines/eb-app-api/home/api/galaxy/lib/database/meeting-schedule.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/meeting-schedule.ts
@@ -24,3 +24,33 @@ export async function getMeetingSchedule(
 
   return await fetch(sql) as MeetingSchedule[];
 }
+
+// -----------------------------------------------------------------------------
+export async function listMeetingScheduleByMeeting(
+  identityId: string,
+  meetingId: string,
+  limit: number,
+  offset: number,
+) {
+  const sql = {
+    text: `
+      SELECT id, meeting_id, name, started_at, ended_at, duration
+      FROM meeting_schedule
+      WHERE meeting_id = $2
+        AND EXISTS (SELECT 1
+                    FROM meeting
+                    WHERE id = meeting_id
+                      AND identity_id = $1
+                   )
+      ORDER BY started_at
+      LIMIT $3 OFFSET $4`,
+    args: [
+      identityId,
+      meetingId,
+      limit,
+      offset,
+    ],
+  };
+
+  return await fetch(sql) as MeetingSchedule[];
+}
