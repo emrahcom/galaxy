32e517cfcae557f2c6a8e9c0ca08082c7f6f5da3
emrah <emrah.com@gmail.com>
2022-03-28 20:07:53 +0300

    fix, ui: convert meeting time to locale

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/list.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { toLocaleTime } from "$lib/common";
   import type { MeetingSchedule } from "$lib/types";
   import Del from "$lib/components/common/link-del.svelte";
   import Update from "$lib/components/common/link-update.svelte";
@@ -14,7 +15,9 @@
       <div class="col-md-6 col-xl-4">
         <div class="card h-100">
           <div class="card-body text-center">
-            <h5 class="card-title text-muted">{p.started_at}</h5>
+            <h5 class="card-title text-muted">
+              {toLocaleTime(p.started_at)}
+            </h5>
 
             <p class="card-text text-muted">{p.duration} mins</p>
           </div>
