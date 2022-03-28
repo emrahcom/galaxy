a35c1c2c4daed2c468eae9410c9a7d5f41f01a11
emrah <emrah.com@gmail.com>
2022-03-28 15:05:07 +0300

    feat, ui: add routing for schedule update

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/schedule/update/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Update from "$lib/components/pri/meeting-schedule/update.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/meeting/schedule/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Update this schedule" />
+
+{#await pr then schedule}
+  <Update p={schedule} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
