5a90809f6a8392b9b4f2514757b5ddfc8aa60bcb
emrah <emrah.com@gmail.com>
2022-03-28 21:40:53 +0300

    feat, ui: datetime input component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-datetime.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let label: string;
+  export let min: string;
+  export let name: string;
+  export let readonly = false;
+  export let required = false;
+  export let value: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="form-floating my-3">
+  <input
+    type="datetime-local"
+    id={name}
+    class="form-control"
+    {name}
+    placeholder=""
+    bind:value
+    {min}
+    {readonly}
+    {required}
+    tabindex={readonly ? -1 : undefined}
+  />
+  <label for={name}>{label}</label>
+</div>
