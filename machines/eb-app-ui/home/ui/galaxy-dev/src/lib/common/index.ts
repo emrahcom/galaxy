75c81014fc1d201ce6679e5a8819fc46ce641c71
emrah <emrah.com@gmail.com>
2022-03-29 00:20:02 +0300

    feat, ui: toInputTime function

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/common/index.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/common/index.ts
@@ -3,3 +3,18 @@ export function toLocaleTime(date: string) {
 
   return _date.toLocaleString();
 }
+
+// -----------------------------------------------------------------------------
+export function toInputTime(date: string) {
+  let _time1 = new Date();
+
+  if (date) {
+    _time1 = new Date(date);
+  }
+
+  const _time2 = new Date(
+    _time1.getTime() - 60 * 1000 * _time1.getTimezoneOffset(),
+  );
+
+  return _time2.toISOString().slice(0, 16);
+}
