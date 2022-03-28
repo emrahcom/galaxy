82c61eb44b65d5f57bf47877688223c2a55c07fd
emrah <emrah.com@gmail.com>
2022-03-28 15:04:56 +0300

    feat, ui: add routing for schedule del

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/schedule/del/[uuid].svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/meeting-schedule/del.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/meeting/schedule/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Delete this schedule" />
+
+{#await pr then schedule}
+  <Del p={schedule} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
