90aa76d888613fcf4e5e0bc8f7abd22aff13d26a
emrah <emrah.com@gmail.com>
2022-03-28 23:15:53 +0300

    feat, ui: range component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-range.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let label: string;
+  export let max: number;
+  export let min: number;
+  export let name: string;
+  export let readonly = false;
+  export let required = false;
+  export let step: number;
+  export let value: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="form-floating my-3">
+  <input
+    type="text"
+    id={name}
+    class="form-control"
+    {name}
+    placeholder=""
+    bind:value
+    {readonly}
+    {required}
+    tabindex={readonly ? -1 : undefined}
+  />
+  <label for={name}>{label}</label>
+</div>
+
+<input
+  type="range"
+  id="range-{name}"
+  class="form-range"
+  name="range-{name}"
+  bind:value
+  {readonly}
+  {min}
+  {max}
+  {step}
+  tabindex={readonly ? -1 : undefined}
+/>
