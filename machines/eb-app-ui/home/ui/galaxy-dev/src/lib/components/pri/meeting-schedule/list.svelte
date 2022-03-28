c3da77630f187371ebde56370819e87adb29acf2
emrah <emrah.com@gmail.com>
2022-03-28 15:23:39 +0300

    feat, ui: schedule list component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/list.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { MeetingMember } from "$lib/types";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Update from "$lib/components/common/link-update.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let schedules: MeetingSchedule[];
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="list">
+  <div class="row mx-auto mt-2 g-3">
+    {#each members as p}
+      <div class="col-md-6 col-xl-4">
+        <div class="card h-100">
+          <div class="card-body text-center">
+            <h5 class="card-title text-muted">{p.started_at}</h5>
+
+            <p class="card-text text-muted">{p.duration} mins</p>
+          </div>
+
+          <div class="card-footer bg-body border-0 text-center">
+            <Del href="/pri/meeting/schedule/del/{p.id}" />
+            <Update href="/pri/meeting/schedule/update/{p.id}" />
+          </div>
+        </div>
+      </div>
+    {:else}
+      <Warning>No schedule found for this meeting</Warning>
+    {/each}
+  </div>
+</section>
