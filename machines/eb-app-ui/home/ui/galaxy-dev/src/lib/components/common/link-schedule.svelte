0c93d2cc59a15b81f63a5e0986d6f1dbc8fb7bff
emrah <emrah.com@gmail.com>
2022-03-28 14:28:24 +0300

    feat, ui: meeting schedule link component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-schedule.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<a {href} class="btn btn-outline-secondary btn-sm" role="button">
+  <i class="bi bi-clock" />
+</a>
