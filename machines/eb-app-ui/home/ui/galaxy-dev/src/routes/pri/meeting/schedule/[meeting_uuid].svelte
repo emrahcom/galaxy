33f3d027525a15a8a29a1a8b776fbb78c75c08fa
emrah <emrah.com@gmail.com>
2022-03-28 15:04:31 +0300

    feat, ui: add routing for schedule list

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/schedule/[meeting_uuid].svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById, listById } from "$lib/api";
+  import type { Meeting } from "$lib/types";
+  import List from "$lib/components/pri/meeting-schedule/list.svelte";
+  import Subheader from "$lib/components/common/subheader-back.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const meetingId = $page.params.meeting_uuid;
+  let meetingName = "";
+
+  const pr1 = getById("/api/pri/meeting/get", meetingId).then(
+    (item: Meeting) => {
+      meetingName = item.name;
+    },
+  );
+  const pr2 = listById(
+    "/api/pri/meeting/schedule/list/bymeeting",
+    meetingId,
+    100,
+  );
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Schedule of {meetingName}" hrefBack="/pri/meeting" />
+
+{#await Promise.all([pr1, pr2]) then [_meeting, schedules]}
+  <List {schedules} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
