bbd5f7b80c570e89431d8061a7988d40db0de347
emrah <emrah.com@gmail.com>
2022-03-28 13:05:15 +0300

    feat, api: MeetingSchedule interface

--- machines/eb-app-api/home/api/galaxy/lib/database/types.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/types.ts
@@ -191,6 +191,16 @@ export interface MeetingRequest {
 }
 
 // -----------------------------------------------------------------------------
+export interface MeetingSchedule {
+  id: string;
+  meeting_id: string;
+  name: string;
+  started_at: string;
+  ended_at: string;
+  duration: number;
+}
+
+// -----------------------------------------------------------------------------
 export interface MeetingLinkSet {
   name: string;
   room_name: string;
