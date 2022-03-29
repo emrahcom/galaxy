788fd930fa622b3dcca126a2542abbab53fdccd7
emrah <emrah.com@gmail.com>
2022-03-29 14:58:46 +0300

    feat, api: routing for /api/pub/meeting/get/bycode

--- machines/eb-app-api/home/api/galaxy/lib/pub/meeting.ts
+++ machines/eb-app-api/home/api/galaxy/lib/pub/meeting.ts
@@ -1,7 +1,11 @@
 import { notFound } from "../http/response.ts";
 import { pub as wrapper } from "../http/wrapper.ts";
 import { getLimit, getOffset } from "../database/common.ts";
-import { getPublicMeeting, listPublicMeeting } from "../database/meeting.ts";
+import {
+  getPublicMeeting,
+  getMeetingByCode,
+  listPublicMeeting,
+} from "../database/meeting.ts";
 
 const PRE = "/api/pub/meeting";
 
@@ -11,6 +15,14 @@ async function get(req: Request): Promise<unknown> {
   const meetingId = pl.id;
 
   return await getPublicMeeting(meetingId);
+}
+
+// -----------------------------------------------------------------------------
+async function getByCode(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const code = pl.code;
+
+  return await getMeetingByCode(code);
 }
 
 // -----------------------------------------------------------------------------
@@ -26,6 +38,8 @@ async function listEnabled(req: Request): Promise<unknown> {
 export default async function (req: Request, path: string): Promise<Response> {
   if (path === `${PRE}/get`) {
     return await wrapper(get, req);
+  } else if (path === `${PRE}/get/bycode`) {
+    return await wrapper(getByCode, req);
   } else if (path === `${PRE}/list`) {
     return await wrapper(listEnabled, req);
   } else {
