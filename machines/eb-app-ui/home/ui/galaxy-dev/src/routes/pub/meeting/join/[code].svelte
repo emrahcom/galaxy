66b00995e99587be3bbd8fead7008b9ec20d9458
emrah <emrah.com@gmail.com>
2022-03-29 14:54:12 +0300

    feat, ui: waiting room for audience

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pub/meeting/join/[code].svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getByCode } from "$lib/api";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const code = $page.params.code;
+
+  const pr = getByCode("/api/pub/meeting/get/bycode", code);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Welcome to the meeting" />
+
+{#await pr then meeting}
+  {console.log(meeting)}
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
