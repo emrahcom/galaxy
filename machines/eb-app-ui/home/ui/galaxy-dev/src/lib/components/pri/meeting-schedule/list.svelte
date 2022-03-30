e50b2a1cb96a26b781107f76c0104f959baf60c4
emrah <emrah.com@gmail.com>
2022-03-30 14:02:20 +0300

    refactor, ui: no meeting schedule warning

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/list.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   import { toLocaleTime } from "$lib/common";
   import type { MeetingSchedule } from "$lib/types";
+  import Add from "$lib/components/common/link-add.svelte";
   import Del from "$lib/components/common/link-del.svelte";
   import Update from "$lib/components/common/link-update.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   export let schedules: MeetingSchedule[];
+  const meeting_id = $page.params.meeting_uuid;
 </script>
 
 <!-- -------------------------------------------------------------------------->
@@ -33,7 +36,10 @@
         </div>
       </div>
     {:else}
-      <Warning>This meeting has no schedules.</Warning>
+      <Warning>
+        This meeting has no schedules. Click
+        <Add href="/pri/meeting/schedule/add/{meeting_id}" /> to create a new schedule.
+      </Warning>
     {/each}
   </div>
 </section>
