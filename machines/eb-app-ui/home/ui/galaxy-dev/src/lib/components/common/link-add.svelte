db44dffa657e529d5f7b602ad6e466d20ccb7e94
emrah <emrah.com@gmail.com>
2022-03-30 09:55:51 +0300

    feat, ui: add link-add component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-add.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<a {href} class="btn btn-outline-secondary btn-sm mx-2" role="button">
+  <i class="bi bi-plus" />
+</a>
