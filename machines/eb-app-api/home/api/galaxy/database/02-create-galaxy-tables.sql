94317bad552f5c1aabf3850cf605a3ace05e7d8e
emrah <emrah.com@gmail.com>
2022-03-31 21:32:38 +0300

    feat, database: a new index for meeting_schedule

--- machines/eb-app-api/home/api/galaxy/database/02-create-galaxy-tables.sql
+++ machines/eb-app-api/home/api/galaxy/database/02-create-galaxy-tables.sql
@@ -307,6 +307,7 @@ CREATE TABLE meeting_schedule (
     "ended_at" timestamp with time zone NOT NULL
 );
 CREATE INDEX ON meeting_schedule(meeting_id, started_at);
+CREATE INDEX ON meeting_schedule(meeting_id, ended_at);
 ALTER TABLE meeting_schedule OWNER TO galaxy;
 
 -- -----------------------------------------------------------------------------
