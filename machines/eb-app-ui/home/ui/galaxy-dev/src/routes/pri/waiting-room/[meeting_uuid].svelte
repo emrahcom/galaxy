bc94870c38800b5ecc1688e1ca8f04797136f9b4
emrah <emrah.com@gmail.com>
2022-03-31 22:18:22 +0300

    fix, ui: get by id

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/waiting-room/[meeting_uuid].svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/waiting-room/[meeting_uuid].svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { getByCode } from "$lib/api";
+  import { getById } from "$lib/api";
   import Subheader from "$lib/components/common/subheader-center.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   const meetingId = $page.params.meeting_uuid;
 
-  const pr = getByCode("/api/pri/meeting/schedule/get/bymeeting", meetingId);
+  const pr = getById("/api/pri/meeting/schedule/get/bymeeting", meetingId);
 </script>
 
 <!-- -------------------------------------------------------------------------->
