b7934b87157ed1c34cbb89c8b2c0f52d2fc4cec7
emrah <emrah.com@gmail.com>
2022-03-31 14:54:48 +0300

    feat, ui: private waiting room

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/waiting-room/[meeting_uuid].svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getByCode } from "$lib/api";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const meetingId = $page.params.meeting_uuid;
+
+  const pr = getByCode("/api/pri/meeting/get/schedule", meedingId);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Welcome to the meeting" />
+
+{#await pr then meeting}
+  {console.log(meeting)}
+{:catch}
+  <Warning>Invite code is not accepted</Warning>
+{/await}
