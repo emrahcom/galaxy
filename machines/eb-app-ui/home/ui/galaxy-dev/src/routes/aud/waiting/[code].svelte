1bb71d9c6ea47f506de19646a662fb2437081878
emrah <emrah.com@gmail.com>
2022-04-01 15:51:31 +0300

    feat, ui: go home page into aud waiting

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/aud/waiting/[code].svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/aud/waiting/[code].svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import { getByCode } from "$lib/api";
+  import Home from "$lib/components/common/button-on-click.svelte";
   import Subheader from "$lib/components/common/subheader-center.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   const code = $page.params.code;
 
   const pr = getByCode("/api/pub/meeting/get/bycode", code);
+
+  // ---------------------------------------------------------------------------
+  function goHome() {
+    window.location.href = `/`;
+  }
 </script>
 
 <!-- -------------------------------------------------------------------------->
@@ -16,4 +22,8 @@
   {console.log(meeting)}
 {:catch}
   <Warning>No scheduled meeting found</Warning>
+
+  <center>
+    <Home label="Home Page" on:click={goHome} />
+  </center>
 {/await}
