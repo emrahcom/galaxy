e215c1cf7b6fa0da10c68e50fddaad42d923fb09
emrah <emrah.com@gmail.com>
2022-04-01 15:14:11 +0300

    feat, ui: back button component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-back.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let label = "Back";
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<button class="btn btn-outline-secondary" type="button" on:click>
+  {label}
+</button>
