545be2d0e1c6199366632c0659680818b96694e2
emrah <emrah.com@gmail.com>
2022-04-01 17:38:02 +0300

    feat, ui: join for aud

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/aud/join/[code].svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/aud/join/[code].svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { getByCode } from "$lib/api";
+  import { join } from "$lib/aud/meeting";
+  import Spinner from "$lib/components/common/spinner.svelte";
   import Subheader from "$lib/components/common/subheader-center.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
-  const code = $page.params.code;
-
-  const pr = getByCode("/api/pub/meeting/get/bycode", code);
+  const pr = join($page.params.code);
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<Subheader subheader="Welcome to the meeting" />
+<Subheader subheader="Join the meeting" />
 
-{#await pr then meeting}
-  {console.log(meeting)}
+{#await pr}
+  <Spinner>joining...</Spinner>
+{:then}
+  <Spinner>joining...</Spinner>
 {:catch}
-  <Warning>Invite code is not accepted</Warning>
+  <Warning>Something went wrong</Warning>
 {/await}
