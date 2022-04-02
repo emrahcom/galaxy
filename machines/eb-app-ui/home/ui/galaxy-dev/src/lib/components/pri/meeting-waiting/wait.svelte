520182e02f6fac886a45bb5c19f27b1ec0dfa3dd
emrah <emrah.com@gmail.com>
2022-04-02 14:44:21 +0300

    feat, ui: bak button into waiting

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-waiting/wait.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-waiting/wait.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   import { toLocaleTime } from "$lib/common";
   import type { MeetingSchedule222 } from "$lib/types";
+  import Back from "$lib/components/common/button-on-click.svelte";
   import Join from "$lib/components/common/button-on-click.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   export let p: MeetingSchedule222;
 
   let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function goBack() {
+    window.location.href = `/pri/meeting`;
+  }
 
   // ---------------------------------------------------------------------------
   async function join(meetingId: string) {
@@ -41,20 +47,21 @@
             {/each}
           {/if}
 
-          {#if p.join_as === "host"}
-            {#if warning}
-              <Warning>The join request is not accepted.</Warning>
-            {/if}
-
+          <div class="card-footer bg-body border-0 text-center mt-3">
+            <Back label="Back" on:click={goBack} />
             <Join
               label="Join Now"
               on:click={() => {
                 join(p.meeting_id);
               }}
             />
-          {/if}
+          </div>
         </div>
       </div>
     </div>
+
+    {#if warning}
+      <Warning>The join request is not accepted.</Warning>
+    {/if}
   </div>
 </section>
