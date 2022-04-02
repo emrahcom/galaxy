79131765445926ac008e7ada41057314f4703fb9
emrah <emrah.com@gmail.com>
2022-04-03 01:23:02 +0300

    feat, api: public meeting schedule endpoints

---
+++ machines/eb-app-api/home/api/galaxy/lib/pub/meeting-schedule.ts
@@ -0,0 +1,22 @@
+import { notFound } from "../http/response.ts";
+import { pub as wrapper } from "../http/wrapper.ts";
+import { getMeetingScheduleByCode } from "../database/meeting-schedule.ts";
+
+const PRE = "/api/pub/meeting/schedule";
+
+// -----------------------------------------------------------------------------
+async function getByCode(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const code = pl.code;
+
+  return await getMeetingScheduleByCode(code);
+}
+
+// -----------------------------------------------------------------------------
+export default async function (req: Request, path: string): Promise<Response> {
+  if (path === `${PRE}/get/bycode`) {
+    return await wrapper(getByMeeting, req, identityId);
+  } else {
+    return notFound();
+  }
+}
