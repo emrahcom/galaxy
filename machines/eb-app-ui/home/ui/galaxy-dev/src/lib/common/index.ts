5a6fe19d372d382de1f7c74423f4b82bc60d3a70
emrah <emrah.com@gmail.com>
2022-04-02 16:49:30 +0300

    feat, ui: epochToIntervalString

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/common/index.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/common/index.ts
@@ -1,7 +1,30 @@
-export function toLocaleTime(date: string) {
-  const _date = new Date(date);
+export function epochToIntervalString(time: number) {
+  try {
+    const day = Math.floor(time / 86400);
+    let remainder = time % 86400;
 
-  return _date.toLocaleString();
+    const hour = Math.floor(remainder / 3600);
+    remainder = remainder % 3600;
+
+    const min = Math.floor(remainder / 60);
+    const sec = remainder % 60;
+
+    let interval = "";
+
+    if (day > 1) {
+      interval = `${day} days`;
+    } else if (day === 1) {
+      interval = `${day} day`;
+    }
+
+    interval = `${interval} ${String(hour).padStart(2, "0")}`;
+    interval = `${interval}:${String(min).padStart(2, "0")}`;
+    interval = `${interval}:${String(sec).padStart(2, "0")}`;
+
+    return interval;
+  } catch {
+    return "";
+  }
 }
 
 // -----------------------------------------------------------------------------
@@ -15,4 +38,11 @@ export function toInputTime(date = "") {
   );
 
   return _time2.toISOString().slice(0, 16);
+}
+
+// -----------------------------------------------------------------------------
+export function toLocaleTime(date: string) {
+  const _date = new Date(date);
+
+  return _date.toLocaleString();
 }
