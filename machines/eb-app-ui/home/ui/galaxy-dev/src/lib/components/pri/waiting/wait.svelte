ecb20e1b5d77b8df93994846769a6db61c54bbd6
emrah <emrah.com@gmail.com>
2022-04-02 14:20:21 +0300

    feat, ui: initialize wait for member

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/waiting/wait.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/waiting/wait.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-  import { FORM_WIDTH } from "$lib/config";
+  import { toLocaleTime } from "$lib/common";
   import type { MeetingSchedule222 } from "$lib/types";
-  import Submit from "$lib/components/common/button-submit.svelte";
-  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
-  import Text from "$lib/components/common/form-text.svelte";
+  import Join from "$lib/components/common/button-on-click.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   export let p: MeetingSchedule222;
@@ -11,10 +9,10 @@
   let warning = false;
 
   // ---------------------------------------------------------------------------
-  async function onSubmit() {
+  async function join(meetingId: string) {
     try {
       warning = false;
-      window.location.href = "/pri/meeting";
+      window.location.href = `/pri/meeting/join/${meetingId}`;
     } catch {
       warning = true;
     }
@@ -22,21 +20,41 @@
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<section id="del">
-  <div class="d-flex mt-2 justify-content-center">
-    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
-      <Text name="name" label="Name" value={p.meeting_name} readonly={true} />
-      <SubmitBlocker />
+<section id="waiting">
+  <div class="row mx-auto mt-2">
+    <div class="col">
+      <div class="card border-0">
+        <div class="card-body text-center">
+          <h5 class="card-title text-muted">{p.meeting_name}</h5>
 
-      {#if p.join_as === "host"}
-        {#if warning}
-          <Warning>The join request is not accepted.</Warning>
-        {/if}
+          {#if p.schedule_name}
+            <h5 class="card-title text-muted">{p.schedule_name}</h5>
+          {/if}
 
-        <div class="d-flex gap-5 mt-5 justify-content-center">
-          <Submit label="Join" />
+          <p class="card-text text-muted small">
+            {toLocaleTime(p.started_at)}
+          </p>
+
+          {#if p.meeting_info}
+            {#each p.meeting_info.split("\n") as line}
+              <p class="card-text text-muted my-0">{line}&nbsp;</p>
+            {/each}
+          {/if}
+
+          {#if p.join_as === "host"}
+            {#if warning}
+              <Warning>The join request is not accepted.</Warning>
+            {/if}
+
+            <Join
+              label="Join Now"
+              on:click={() => {
+                join(p.meeting_id);
+              }}
+            />
+          {/if}
         </div>
-      {/if}
-    </form>
+      </div>
+    </div>
   </div>
 </section>
