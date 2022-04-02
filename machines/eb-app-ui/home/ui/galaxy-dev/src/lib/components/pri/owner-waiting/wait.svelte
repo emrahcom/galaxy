3d20bce6d50383f1dc05aa6c286e9854b36d5690
emrah <emrah.com@gmail.com>
2022-04-02 23:44:33 +0300

    refactor, ui: payload id in owner waiting

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/owner-waiting/wait.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { epochToIntervalString, toLocaleTime } from "$lib/common";
+  import { getById } from "$lib/api";
+  import type { MeetingSchedule222 } from "$lib/types";
+  import Back from "$lib/components/common/button-on-click.svelte";
+  import Join from "$lib/components/common/button-on-click.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: MeetingSchedule222;
+  const REFRESH_SEC = 60;
+
+  let warning = false;
+  let started_at = new Date(Date.now() + p.waiting_time * 1000);
+  let remainingTime = "";
+  let counter = 0;
+
+  getRemainingTime();
+
+  // ---------------------------------------------------------------------------
+  async function getRemainingTime() {
+    const interval = (started_at.getTime() - Date.now()) / 1000;
+
+    if (interval < 0) {
+      join(p.id);
+      return;
+    }
+
+    counter++;
+    if (counter > REFRESH_SEC) {
+      counter = 0;
+
+      await getById("/api/pri/meeting/schedule/get/bymeeting", p.id)
+        .then((s) => {
+          p = s;
+          started_at = new Date(Date.now() + p.waiting_time * 1000);
+        })
+        .catch();
+    }
+
+    remainingTime = epochToIntervalString(interval);
+
+    setTimeout(getRemainingTime, 1000);
+  }
+
+  // ---------------------------------------------------------------------------
+  function goBack() {
+    window.location.href = `/pri/meeting`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function join(meetingId: string) {
+    try {
+      warning = false;
+      window.location.href = `/pri/meeting/join/${meetingId}`;
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="waiting">
+  <div class="d-flex mt-2 justify-content-center">
+    <div class="card border-0 mx-auto" style="max-width:{FORM_WIDTH};">
+      <div class="card-body text-center">
+        <h2 class="card-title text-muted mt-3 mb-4">{remainingTime}</h2>
+
+        <h5 class="card-title text-muted">{p.meeting_name}</h5>
+
+        {#if p.schedule_name}
+          <h5 class="card-title text-muted">{p.schedule_name}</h5>
+        {/if}
+
+        <p class="card-text text-muted small">
+          {toLocaleTime(p.started_at)}
+        </p>
+
+        {#if p.meeting_info}
+          {#each p.meeting_info.split("\n") as line}
+            <p class="card-text text-muted my-0">{line}&nbsp;</p>
+          {/each}
+        {/if}
+
+        <div
+          class="card-footer d-flex justify-content-center bg-body border-0
+          mt-3 gap-5"
+        >
+          <Back label="Back" on:click={goBack} />
+          <Join
+            label="Join Now"
+            on:click={() => {
+              join(p.id);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+
+    {#if warning}
+      <Warning>The join request is not accepted.</Warning>
+    {/if}
+  </div>
+</section>
