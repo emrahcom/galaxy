384050805abdc07fa4ee79dfbfca2a3349d48c90
emrah <emrah.com@gmail.com>
2022-04-04 10:15:59 +0300

    refactor, ui: sign up message in welcome page

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/index.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/index.svelte
@@ -6,12 +6,17 @@
 <section id="welcome">
   <div class="row d-flex mt-5 justify-content-center">
     <div class="col mx-auto text-center" style="max-width:{FORM_WIDTH};">
-      <h2 class="text-muted">Welcome to eparto.net</h2>
+      <h2 class="text-muted mb-5">Welcome to eparto.net</h2>
 
-      <h3 class="text-muted">
+      <h4 class="text-muted mb-5">
         This site is for Fellow Jitsters to organize Jitsi resources (servers,
         meeting rooms, etc), meeting schedules and attendees.
-      </h3>
+      </h4>
+
+      <h4 class="text-muted mb-5">
+        Please <a class="text-primary" href="/id/registration">Sign Up</a> if
+        you don't have an account.
+      </h4>
     </div>
   </div>
 </section>
