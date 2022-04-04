f91ff45094f27a7ceeeafb32f200da8566f266b0
emrah <emrah.com@gmail.com>
2022-04-04 10:52:21 +0300

    refactor, ui: dashboard

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/index.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/index.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
   import identity from "$lib/stores/kratos/identity";
+
+  let profileName = "";
+
+  const pr = get("/api/pri/profile/get/default")
+    .then((item: Profile) => {
+      profileName = item.profile_name;
+    })
+    .catch(() => {
+      // do nothing
+    });
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<h2>Dashboard</h2>
-<p>Hello {$identity.traits.email}</p>
+<section id="dashboard">
+  <div class="row d-flex mt-5 justify-content-center">
+    <div class="col mx-auto text-center" style="max-width:{FORM_WIDTH};">
+      <h2 class="text-muted mb-5">Hello {profile_name}</h2>
+
+      <h4 class="text-muted mb-5">
+        Click
+        <button
+          class="btn btn-outline-primary"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasMenu"
+          aria-controls="offcanvasMenu"
+        >
+          <span class="bi bi-list" />
+        </button>
+        to start.
+      </h4>
+    </div>
+  </div>
+</section>
