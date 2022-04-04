e57109ac55b26c65c033b637d6c353f06d0e11c3
emrah <emrah.com@gmail.com>
2022-04-04 10:00:54 +0300

    refactor, ui: welcome message

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/index.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/index.svelte
@@ -6,7 +6,12 @@
 <section id="welcome">
   <div class="row d-flex mt-5 justify-content-center">
     <div class="col mx-auto text-center" style="max-width:{FORM_WIDTH};">
-      <h2 class="text-muted">hello</h2>
+      <h2 class="text-muted">Welcome to eparto.net</h2>
+
+      <h3 class="text-muted">
+        This site is for Fellow Jitsters to organize Jitsi resources (servers,
+        meeting rooms, etc), meeting schedules and attendees.
+      </h3>
     </div>
   </div>
 </section>
