79205b51107697230d3b5b518fb567b6bc3fd212
emrah <emrah.com@gmail.com>
2022-04-04 09:26:03 +0300

    fix, ui: missing import

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/index.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/index.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+</script>
+
+<!-- -------------------------------------------------------------------------->
 <section id="welcome">
   <div class="row d-flex mt-2 justify-content-center">
     <div class="col mx-auto" style="max-width:{FORM_WIDTH};">
