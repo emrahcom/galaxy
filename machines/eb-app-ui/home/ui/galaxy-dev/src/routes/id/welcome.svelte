412a9aebce1b8072396d22bb61b6cd5c8b6ce69d
emrah <emrah.com@gmail.com>
2022-07-08 16:55:01 +0300

    ui, fix: disable redirect in server side

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/id/welcome.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/id/welcome.svelte
@@ -1,5 +1,9 @@
 <script lang="ts" context="module">
+  import { browser } from "$app/env";
+
   export async function load() {
+    if (!browser) return {};
+
     return {
       status: 302,
       redirect: "/pri",
--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/__layout.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/__layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts" context="module">
+  import { browser } from "$app/env";
   import { KRATOS } from "$lib/config";
   import { get } from "svelte/store";
   import identity from "$lib/stores/kratos/identity";
 
   // ---------------------------------------------------------------------------
   export async function load() {
+    if (!browser) return {};
+
     const _identity = get(identity);
 
     if (!_identity.id) {
