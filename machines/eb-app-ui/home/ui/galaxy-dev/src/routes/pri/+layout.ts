243330e6dc60fc3772db8c672beffac7d3cd279f
emrah <emrah.com@gmail.com>
2022-08-20 11:19:12 +0300

    ui, fix: identity check in pri

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/+layout.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/+layout.ts
@@ -1,13 +1,10 @@
 import { redirect } from "@sveltejs/kit";
-import { browser } from "$app/env";
 import { KRATOS } from "$lib/config";
 import { get } from "svelte/store";
 import identity from "$lib/stores/kratos/identity";
 
 // ---------------------------------------------------------------------------
 export async function load() {
-  if (!browser) return {};
-
   const _identity = get(identity);
 
   if (!_identity.id) {
