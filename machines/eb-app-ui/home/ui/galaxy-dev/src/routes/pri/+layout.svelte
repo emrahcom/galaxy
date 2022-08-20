ccf9c684ba29dc1201fa1ba947d2ca74297b781b
emrah <emrah.com@gmail.com>
2022-08-20 14:20:49 +0300

    ui, fix: redirection for private pages

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/+layout.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/+layout.svelte
@@ -1,1 +1,15 @@
+<script lang="ts">
+  import { KRATOS } from "$lib/config";
+  import { get } from "svelte/store";
+  import identity from "$lib/stores/kratos/identity";
+
+  if (browser) {
+    const _identity = get(identity);
+    const loginUrl = `${KRATOS}/self-service/login/browser`;
+
+    if (!_identity.id) window.location.replace(loginUrl);
+  }
+</script>
+
+// ---------------------------------------------------------------------------
 <slot />
