76a49e0df41e9d3684737e2365e40fe762ea94a5
emrah <emrah@emrah.com>
2022-09-21 11:41:32 +0300

    api, fix: deno download link

--- installer-sub-scripts/eb-galaxy/306-app-api.sh
+++ installer-sub-scripts/eb-galaxy/306-app-api.sh
@@ -140,12 +140,9 @@ EOS
 # deno
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
-LATEST=$(curl -sSf https://github.com/denoland/deno/releases | \
-    grep -o "/denoland/deno/releases/download/.*/deno-.*linux.*\.zip" | \
-    head -n1)
-
 cd /tmp
-wget -T 30 -O deno.zip https://github.com/\$LATEST
+wget -T 30 -O deno.zip \
+    https://github.com/denoland/deno/releases/latest/download/deno-x86_64-unknown-linux-gnu.zip
 unzip deno.zip
 cp /tmp/deno /usr/local/bin/
 deno --version
