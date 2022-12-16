9274568985b352a8d3e0e7bcd9f85a0282aa1b16
emrah <emrah.eryilmaz@nordeck.net>
2022-12-16 10:50:21 +0300

    installer, refactor: TAG in certificates.sh

--- installer-sub-scripts/eb-galaxy/301-certificates.sh
+++ installer-sub-scripts/eb-galaxy/301-certificates.sh
@@ -18,20 +18,14 @@ echo
 echo "---------------------- CERTIFICATES -----------------------"
 
 # ------------------------------------------------------------------------------
-# EXTERNAL IP
-# ------------------------------------------------------------------------------
-EXTERNAL_IP=$(dig -4 +short myip.opendns.com a @resolver1.opendns.com) || true
-echo EXTERNAL_IP="$EXTERNAL_IP" >> $INSTALLER/000-source
-
-# ------------------------------------------------------------------------------
 # SELF-SIGNED CERTIFICATE
 # ------------------------------------------------------------------------------
-cd /root/eb-ssl
-rm -f /root/eb-ssl/eb-galaxy.*
+cd /root/$TAG-ssl
+rm -f /root/$TAG-ssl/$TAG-galaxy.*
 
 # the extension file for multiple hosts:
 # the container IP, the host IP and the host names
-cat >eb-galaxy.ext <<EOF
+cat >$TAG-galaxy.ext <<EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -41,27 +35,27 @@ subjectAltName = @alt_names
 EOF
 
 # FQDNs
-echo "DNS.1 = $APP_FQDN" >>eb-galaxy.ext
-echo "DNS.2 = $KRATOS_FQDN" >>eb-galaxy.ext
+echo "DNS.1 = $APP_FQDN" >>$TAG-galaxy.ext
+echo "DNS.2 = $KRATOS_FQDN" >>$TAG-galaxy.ext
 
 # internal IPs
 i=1
-for addr in $(egrep '^address=' /etc/dnsmasq.d/eb-galaxy); do
+for addr in $(egrep '^address=' /etc/dnsmasq.d/$TAG-galaxy); do
     ip=$(echo $addr | rev | cut -d '/' -f1 | rev)
-    echo "IP.$i = $ip" >> eb-galaxy.ext
+    echo "IP.$i = $ip" >> $TAG-galaxy.ext
     (( i += 1 ))
 done
 
 # external IPs
-echo "IP.$i = $REMOTE_IP" >>eb-galaxy.ext
+echo "IP.$i = $REMOTE_IP" >>$TAG-galaxy.ext
 (( i += 1 ))
 [[ -n "$EXTERNAL_IP" ]] && [[ "$EXTERNAL_IP" != "$REMOTE_IP" ]] \
-    && echo "IP.$i = $EXTERNAL_IP" >>eb-galaxy.ext \
+    && echo "IP.$i = $EXTERNAL_IP" >>$TAG-galaxy.ext \
     || true
 
 # the domain key and the domain certificate
 openssl req -nodes -newkey rsa:2048 \
-    -keyout eb-galaxy.key -out eb-galaxy.csr \
-    -subj "/O=emrah-bullseye/OU=eb-galaxy/CN=$APP_FQDN"
-openssl x509 -req -CA eb-CA.pem -CAkey eb-CA.key -CAcreateserial -days 10950 \
-    -in eb-galaxy.csr -out eb-galaxy.pem -extfile eb-galaxy.ext
+    -keyout $TAG-galaxy.key -out $TAG-galaxy.csr \
+    -subj "/O=emrah-bullseye/OU=$TAG-galaxy/CN=$APP_FQDN"
+openssl x509 -req -CA $TAG-CA.pem -CAkey $TAG-CA.key -CAcreateserial -days 10950 \
+    -in $TAG-galaxy.csr -out $TAG-galaxy.pem -extfile $TAG-galaxy.ext
