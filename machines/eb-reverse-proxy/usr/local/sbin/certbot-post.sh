f902beb0599bacb2f0872b7e23f308b0bb2968dc
emrah <emrah.eryilmaz@nordeck.net>
2022-12-25 15:51:44 +0300

    reverse-proxy, refactor: certbot-post.sh

---
+++ machines/eb-reverse-proxy/usr/local/sbin/certbot-post.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -d "/etc/letsencrypt/archive" ]]; then
+    find /etc/letsencrypt/archive -name 'privkey*.pem' -exec chmod 640 {} \;
+    chmod 750 /etc/letsencrypt/archive
+    chown root:ssl-cert /etc/letsencrypt/archive -R
+fi
+
+if [[ -d "/etc/letsencrypt/live" ]]; then
+    chmod 750 /etc/letsencrypt/live
+    chown root:ssl-cert /etc/letsencrypt/live -R
+fi
+
+systemctl is-active nginx.service && systemctl reload nginx.service
