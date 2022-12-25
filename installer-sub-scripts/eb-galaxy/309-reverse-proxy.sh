10314d6a0e3753619299fd41161b19221a6a9094
emrah <emrah.eryilmaz@nordeck.net>
2022-12-25 15:27:49 +0300

    installer, refactor: TAG in 309-reverse-proxy.sh

--- installer-sub-scripts/eb-galaxy/309-reverse-proxy.sh
+++ installer-sub-scripts/eb-galaxy/309-reverse-proxy.sh
@@ -7,11 +7,11 @@ source $INSTALLER/000-source
 # ------------------------------------------------------------------------------
 # ENVIRONMENT
 # ------------------------------------------------------------------------------
-MACH="eb-reverse-proxy"
+MACH="$TAG-reverse-proxy"
 cd $MACHINES/$MACH
 
 ROOTFS="/var/lib/lxc/$MACH/rootfs"
-DNS_RECORD=$(grep "address=/$MACH/" /etc/dnsmasq.d/eb-galaxy | head -n1)
+DNS_RECORD=$(grep "address=/$MACH/" /etc/dnsmasq.d/$TAG-galaxy | head -n1)
 IP=${DNS_RECORD##*/}
 SSH_PORT="30$(printf %03d ${IP##*.})"
 echo REVERSE_PROXY="$IP" >> $INSTALLER/000-source
@@ -20,25 +20,25 @@ echo REVERSE_PROXY="$IP" >> $INSTALLER/000-source
 # NFTABLES RULES
 # ------------------------------------------------------------------------------
 # the public ssh
-nft delete element eb-nat tcp2ip { $SSH_PORT } 2>/dev/null || true
-nft add element eb-nat tcp2ip { $SSH_PORT : $IP }
-nft delete element eb-nat tcp2port { $SSH_PORT } 2>/dev/null || true
-nft add element eb-nat tcp2port { $SSH_PORT : 22 }
+nft delete element $TAG-nat tcp2ip { $SSH_PORT } 2>/dev/null || true
+nft add element $TAG-nat tcp2ip { $SSH_PORT : $IP }
+nft delete element $TAG-nat tcp2port { $SSH_PORT } 2>/dev/null || true
+nft add element $TAG-nat tcp2port { $SSH_PORT : 22 }
 # the public http
-nft delete element eb-nat tcp2ip { 80 } 2>/dev/null || true
-nft add element eb-nat tcp2ip { 80 : $IP }
-nft delete element eb-nat tcp2port { 80 } 2>/dev/null || true
-nft add element eb-nat tcp2port { 80 : 80 }
+nft delete element $TAG-nat tcp2ip { 80 } 2>/dev/null || true
+nft add element $TAG-nat tcp2ip { 80 : $IP }
+nft delete element $TAG-nat tcp2port { 80 } 2>/dev/null || true
+nft add element $TAG-nat tcp2port { 80 : 80 }
 # the public https
-nft delete element eb-nat tcp2ip { 443 } 2>/dev/null || true
-nft add element eb-nat tcp2ip { 443 : $IP }
-nft delete element eb-nat tcp2port { 443 } 2>/dev/null || true
-nft add element eb-nat tcp2port { 443 : 443 }
+nft delete element $TAG-nat tcp2ip { 443 } 2>/dev/null || true
+nft add element $TAG-nat tcp2ip { 443 : $IP }
+nft delete element $TAG-nat tcp2port { 443 } 2>/dev/null || true
+nft add element $TAG-nat tcp2port { 443 : 443 }
 # tcp/3000 sveltekit wss (only for development environment)
-nft delete element eb-nat tcp2ip { 3000 } 2>/dev/null || true
-nft add element eb-nat tcp2ip { 3000 : $IP }
-nft delete element eb-nat tcp2port { 3000 } 2>/dev/null || true
-nft add element eb-nat tcp2port { 3000 : 3000 }
+nft delete element $TAG-nat tcp2ip { 3000 } 2>/dev/null || true
+nft add element $TAG-nat tcp2ip { 3000 : $IP }
+nft delete element $TAG-nat tcp2port { 3000 } 2>/dev/null || true
+nft add element $TAG-nat tcp2port { 3000 : 3000 }
 
 # ------------------------------------------------------------------------------
 # INIT
@@ -68,8 +68,8 @@ fi
 # ------------------------------------------------------------------------------
 # stop the template container if it's running
 set +e
-lxc-stop -n eb-bullseye
-lxc-wait -n eb-bullseye -s STOPPED
+lxc-stop -n $TAG-bullseye
+lxc-wait -n $TAG-bullseye -s STOPPED
 set -e
 
 # remove the old container if exists
@@ -82,7 +82,7 @@ sleep 1
 set -e
 
 # create the new one
-lxc-copy -n eb-bullseye -N $MACH -p /var/lib/lxc/
+lxc-copy -n $TAG-bullseye -N $MACH -p /var/lib/lxc/
 
 # the shared directories
 mkdir -p $SHARED/cache
@@ -97,7 +97,7 @@ cat >> /var/lib/lxc/$MACH/config <<EOF
 lxc.start.auto = 1
 lxc.start.order = 309
 lxc.start.delay = 2
-lxc.group = eb-group
+lxc.group = $TAG-group
 lxc.group = onboot
 EOF
 
@@ -156,19 +156,19 @@ EOS
 # SYSTEM CONFIGURATION
 # ------------------------------------------------------------------------------
 # eb-cert
-cp /root/eb-ssl/eb-galaxy.key $ROOTFS/etc/ssl/private/eb-cert.key
-cp /root/eb-ssl/eb-galaxy.pem $ROOTFS/etc/ssl/certs/eb-cert.pem
+cp /root/$TAG-ssl/$TAG-galaxy.key $ROOTFS/etc/ssl/private/$TAG-cert.key
+cp /root/$TAG-ssl/$TAG-galaxy.pem $ROOTFS/etc/ssl/certs/$TAG-cert.pem
 
 # nginx
 rm $ROOTFS/etc/nginx/sites-enabled/default
-cp etc/nginx/sites-available/eb-default.conf $ROOTFS/etc/nginx/sites-available/
-ln -s ../sites-available/eb-default.conf $ROOTFS/etc/nginx/sites-enabled/
-cp etc/nginx/sites-available/eb-kratos.conf $ROOTFS/etc/nginx/sites-available/
-ln -s ../sites-available/eb-kratos.conf $ROOTFS/etc/nginx/sites-enabled/
-cp etc/nginx/sites-available/eb-app.conf $ROOTFS/etc/nginx/sites-available/
-ln -s ../sites-available/eb-app.conf $ROOTFS/etc/nginx/sites-enabled/
-cp etc/nginx/sites-available/eb-app-wss.conf $ROOTFS/etc/nginx/sites-available/
-ln -s ../sites-available/eb-app-wss.conf $ROOTFS/etc/nginx/sites-enabled/
+cp etc/nginx/sites-available/$TAG-default.conf $ROOTFS/etc/nginx/sites-available/
+ln -s ../sites-available/$TAG-default.conf $ROOTFS/etc/nginx/sites-enabled/
+cp etc/nginx/sites-available/$TAG-kratos.conf $ROOTFS/etc/nginx/sites-available/
+ln -s ../sites-available/$TAG-kratos.conf $ROOTFS/etc/nginx/sites-enabled/
+cp etc/nginx/sites-available/$TAG-app.conf $ROOTFS/etc/nginx/sites-available/
+ln -s ../sites-available/$TAG-app.conf $ROOTFS/etc/nginx/sites-enabled/
+cp etc/nginx/sites-available/$TAG-app-wss.conf $ROOTFS/etc/nginx/sites-available/
+ln -s ../sites-available/$TAG-app-wss.conf $ROOTFS/etc/nginx/sites-enabled/
 
 sed -i "s/___KRATOS_FQDN___/$KRATOS_FQDN/g" $ROOTFS/etc/nginx/sites-available/*
 sed -i "s/___APP_FQDN___/$APP_FQDN/g" $ROOTFS/etc/nginx/sites-available/*
