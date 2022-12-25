fb55c4b2b4cf73684bcb778cc194d710e1e57875
emrah <emrah.eryilmaz@nordeck.net>
2022-12-25 15:24:05 +0300

    installer, refactor: FQDN checks in init.sh

--- installer-sub-scripts/eb-galaxy/init.sh
+++ installer-sub-scripts/eb-galaxy/init.sh
@@ -16,6 +16,17 @@ cd $INSTALLER
 [[ "$DONT_RUN_INIT" = true ]] && exit
 
 # ------------------------------------------------------------------------------
+# CHECKS
+# ------------------------------------------------------------------------------
+echo
+
+[[ -z "$APP_FQDN" ]] && echo "APP_FQDN not found" && false
+[[ -z "$KRATOS_FQDN" ]] && echo "KRATOS_FQDN not found" && false
+[[ -z "$(dig +short $APP_FQDN)" ]] && echo "unresolvable APP_FQDN" && false
+[[ -z "$(dig +short $KRATOS_FQDN)" ]] && \
+    echo "unresolvable KRATOS_FQDN" && false
+
+# ------------------------------------------------------------------------------
 # INSTALLER CONFIGURATION
 # ------------------------------------------------------------------------------
-cp -ap ../eb-base/* .
+cp -ap ../$TAG-base/* .
