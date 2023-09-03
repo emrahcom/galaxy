6b5620b5ab0643f8ffa25c2668707aa4b11b4272
emrah <emrah.eryilmaz@nordeck.net>
2023-09-03 22:00:50 +0300

    eb-kratos-ui, fix: logout

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/nav/button-account.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/nav/button-account.svelte
@@ -42,12 +42,12 @@
   <div class="offcanvas-body">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="/id/settings" on:click={closeMenu}>
+        <a class="nav-link" href="/id/settings" target="_blank" rel="noopener noreferrer" on:click={closeMenu}>
           Settings
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/id/logout" on:click={closeMenu}> Log Out </a>
+        <a class="nav-link" href="/logout" on:click={closeMenu}> Log Out </a>
       </li>
     </ul>
   </div>
