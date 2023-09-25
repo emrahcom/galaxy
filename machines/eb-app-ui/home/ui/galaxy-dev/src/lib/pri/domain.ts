a4dec516a207fa79a9668e2942babec950c289a4
emrah <emrah.eryilmaz@nordeck.net>
2023-09-25 15:04:04 +0300

    eb-app-ui, feat; add JaaS constants

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/domain.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/domain.ts
@@ -1,5 +1,11 @@
 export const AUTH_TYPE_OPTIONS = [
   ["none", "anonymous"],
-  ["token", "token"],
   ["jaas", "JaaS"],
+  ["token", "token"],
 ];
+
+export const JAAS_ALGO = "RS256";
+export const JAAS_AUD = "jitsi";
+export const JAAS_ISS = "chat";
+export const JAAS_URL = "https://8x8.vc";
+export const TOKEN_ALGO = "HS256";
