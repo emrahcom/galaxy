288ef3f0ade3a92220e53743dc37081248e1b216
emrah <emrah.eryilmaz@nordeck.net>
2023-10-07 18:29:20 +0300

    eb-app-ui, refactor: disabled fields for meeting-member set-guest

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member/set-guest.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member/set-guest.svelte
@@ -33,14 +33,27 @@
 <section id="set-guest">
   <div class="d-flex mt-2 justify-content-center">
     <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
-      <Text name="name" label="Name" value={p.profile_name} readonly={true} />
+      <Text
+        name="name"
+        label="Name"
+        value={p.profile_name}
+        disabled={true}
+        readonly={true}
+      />
       <Text
         name="email"
         label="Email"
         value={p.profile_email}
+        disabled={true}
         readonly={true}
       />
-      <Text name="type" label="Type" value={p.join_as} readonly={true} />
+      <Text
+        name="type"
+        label="Type"
+        value={p.join_as}
+        disabled={true}
+        readonly={true}
+      />
 
       {#if warning}
         <Warning>The set request is not accepted.</Warning>
