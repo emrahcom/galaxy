5c5c16bb9c1cbf8ab52b7707280c5489ad00c625
emrah <emrah.eryilmaz@nordeck.net>
2023-10-07 15:37:18 +0300

    eb-app-ui, refactor: disabled fields in the domain-invite add

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-invite/add.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-invite/add.svelte
@@ -44,12 +44,14 @@
         name="name"
         label="Jitsi Domain Name"
         value={domain.name}
+        disabled={true}
         readonly={true}
       />
       <Text
         name="url"
         label="Jitsi Domain URL"
         value={domain.domain_attr.url}
+        disabled={true}
         readonly={true}
       />
 
