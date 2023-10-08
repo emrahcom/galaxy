adfdcc0d92172755da95c05ee5d6beba621a8ed8
emrah <emrah.eryilmaz@nordeck.net>
2023-10-08 16:53:55 +0300

    eb-app-api, refactor: generateMeetingUrl

--- machines/eb-app-api/home/api/galaxy/lib/common/helper.ts
+++ machines/eb-app-api/home/api/galaxy/lib/common/helper.ts
@@ -17,10 +17,9 @@ async function generateRoomUrlJaas(
   exp = 3600,
 ): Promise<string> {
   const sub = encodeURIComponent(linkset.domain_attr.jaas_app_id);
-  let url: string;
+  let url = encodeURI(linkset.domain_attr.jaas_url);
   let roomName = encodeURIComponent(linkset.name);
 
-  url = encodeURI(linkset.domain_attr.jaas_url);
   if (linkset.has_suffix) roomName = `${roomName}-${linkset.suffix}`;
   url = `${url}/${sub}/${roomName}`;
 
@@ -46,10 +45,9 @@ async function generateRoomUrlToken(
   profile: Profile,
   exp = 3600,
 ): Promise<string> {
-  let url: string;
+  let url = encodeURI(linkset.domain_attr.url);
   let roomName = encodeURIComponent(linkset.name);
 
-  url = encodeURI(linkset.domain_attr.url);
   if (linkset.has_suffix) roomName = `${roomName}-${linkset.suffix}`;
   url = `${url}/${roomName}`;
 
@@ -101,6 +99,110 @@ export async function generateRoomUrl(
 }
 
 // -----------------------------------------------------------------------------
+async function generateMeetingUrlJaasHost(
+  linkset: MeetingLinkset,
+  exp = 3600,
+): Promise<string> {
+  const sub = encodeURIComponent(linkset.domain_attr.jaas_app_id);
+  let url = encodeURI(linkset.domain_attr.jaas_url);
+  let roomName = encodeURIComponent(linkset.room_name);
+
+  if (linkset.has_suffix) roomName = `${roomName}-${linkset.suffix}`;
+  url = `${url}/${sub}/${roomName}`;
+
+  const jwt = await generateHostTokenJaas(
+    linkset.domain_attr.jaas_app_id,
+    linkset.domain_attr.jaas_kid,
+    linkset.domain_attr.jaas_key,
+    linkset.domain_attr.jaas_alg,
+    linkset.domain_attr.jaas_aud,
+    linkset.domain_attr.jaas_iss,
+    roomName,
+    linkset.profile_name,
+    linkset.profile_email,
+    exp,
+  );
+
+  return `${url}?jwt=${jwt}`;
+}
+
+// -----------------------------------------------------------------------------
+async function generateMeetingUrlJaasGuest(
+  linkset: MeetingLinkset,
+  exp = 3600,
+): Promise<string> {
+  const sub = encodeURIComponent(linkset.domain_attr.jaas_app_id);
+  let url = encodeURI(linkset.domain_attr.jaas_url);
+  let roomName = encodeURIComponent(linkset.room_name);
+
+  if (linkset.has_suffix) roomName = `${roomName}-${linkset.suffix}`;
+  url = `${url}/${sub}/${roomName}`;
+
+  const jwt = await generateGuestTokenJaas(
+    linkset.domain_attr.jaas_app_id,
+    linkset.domain_attr.jaas_kid,
+    linkset.domain_attr.jaas_key,
+    linkset.domain_attr.jaas_alg,
+    linkset.domain_attr.jaas_aud,
+    linkset.domain_attr.jaas_iss,
+    roomName,
+    linkset.profile_name,
+    linkset.profile_email,
+    exp,
+  );
+
+  return `${url}?jwt=${jwt}`;
+}
+
+// -----------------------------------------------------------------------------
+async function generateMeetingUrlTokenHost(
+  linkset: MeetingLinkset,
+  exp = 3600,
+): Promise<string> {
+  let url = encodeURI(linkset.domain_attr.url);
+  let roomName = encodeURIComponent(linkset.room_name);
+
+  if (linkset.has_suffix) roomName = `${roomName}-${linkset.suffix}`;
+  url = `${url}/${roomName}`;
+
+  const jwt = await generateHostTokenHS(
+    linkset.domain_attr.app_id,
+    linkset.domain_attr.app_secret,
+    linkset.domain_attr.app_alg,
+    roomName,
+    linkset.profile_name,
+    linkset.profile_email,
+    exp,
+  );
+
+  return `${url}?jwt=${jwt}`;
+}
+
+// -----------------------------------------------------------------------------
+async function generateMeetingUrlTokenGuest(
+  linkset: MeetingLinkset,
+  exp = 3600,
+): Promise<string> {
+  let url = encodeURI(linkset.domain_attr.url);
+  let roomName = encodeURIComponent(linkset.room_name);
+
+  if (linkset.has_suffix) roomName = `${roomName}-${linkset.suffix}`;
+  url = `${url}/${roomName}`;
+
+  const jwt = await generateGuestTokenHS(
+    linkset.domain_attr.app_id,
+    linkset.domain_attr.app_secret,
+    linkset.domain_attr.app_alg,
+    roomName,
+    linkset.profile_name,
+    linkset.profile_email,
+    exp,
+  );
+
+  return `${url}?jwt=${jwt}`;
+}
+
+// -----------------------------------------------------------------------------
 export async function generateMeetingUrl(
   linkset: MeetingLinkset,
   exp = 3600,
@@ -110,79 +212,20 @@ export async function generateMeetingUrl(
   if (!linkset.profile_name) linkset.profile_name = "";
   if (!linkset.profile_email) linkset.profile_email = "";
 
-  if (linkset.auth_type === "jaas") {
-    const sub = encodeURIComponent(linkset.domain_attr.jaas_app_id);
-    let roomName = encodeURIComponent(linkset.name);
-    url = encodeURI(linkset.domain_attr.jaas_url);
-
-    if (linkset.has_suffix) roomName = `${roomName}-${linkset.suffix}`;
-    url = `${url}/${sub}/${roomName}`;
-
-    let jwt: string;
-
-    if (linkset.join_as === "host") {
-      jwt = await generateHostTokenJaas(
-        linkset.domain_attr.jaas_app_id,
-        linkset.domain_attr.jaas_kid,
-        linkset.domain_attr.jaas_key,
-        linkset.domain_attr.jaas_alg,
-        linkset.domain_attr.jaas_aud,
-        linkset.domain_attr.jaas_iss,
-        roomName,
-        linkset.profile_name,
-        linkset.profile_email,
-        exp,
-      );
-    } else {
-      jwt = await generateGuestTokenJaas(
-        linkset.domain_attr.jaas_app_id,
-        linkset.domain_attr.jaas_kid,
-        linkset.domain_attr.jaas_key,
-        linkset.domain_attr.jaas_alg,
-        linkset.domain_attr.jaas_aud,
-        linkset.domain_attr.jaas_iss,
-        roomName,
-        linkset.profile_name,
-        linkset.profile_email,
-        exp,
-      );
-    }
-
-    url = `${url}?jwt=${jwt}`;
+  if (linkset.auth_type === "jaas" && linkset.join_as === "host") {
+    url = await generateMeetingUrlJaasHost(linkset, exp);
+  } else if (linkset.auth_type === "jaas") {
+    url = await generateMeetingUrlJaasGuest(linkset, exp);
+  } else if (linkset.auth_type === "token" && linkset.join_as === "host") {
+    url = await generateMeetingUrlTokenHost(linkset, exp);
+  } else if (linkset.auth_type === "token") {
+    url = await generateMeetingUrlTokenGuest(linkset, exp);
   } else {
     let roomName = encodeURIComponent(linkset.room_name);
-    url = encodeURI(linkset.domain_attr.url);
 
+    url = encodeURI(linkset.domain_attr.url);
     if (linkset.has_suffix) roomName = `${roomName}-${linkset.suffix}`;
     url = `${url}/${roomName}`;
-
-    if (linkset.auth_type === "token") {
-      let jwt: string;
-
-      if (linkset.join_as === "host") {
-        jwt = await generateHostTokenHS(
-          linkset.domain_attr.app_id,
-          linkset.domain_attr.app_secret,
-          linkset.domain_attr.app_alg,
-          roomName,
-          linkset.profile_name,
-          linkset.profile_email,
-          exp,
-        );
-      } else {
-        jwt = await generateGuestTokenHS(
-          linkset.domain_attr.app_id,
-          linkset.domain_attr.app_secret,
-          linkset.domain_attr.app_alg,
-          roomName,
-          linkset.profile_name,
-          linkset.profile_email,
-          exp,
-        );
-      }
-
-      url = `${url}?jwt=${jwt}`;
-    }
   }
 
   let subject: string;
