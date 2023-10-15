ff8adbf19766f0715ad3a1444726513697f8da1d
emrah <emrah.eryilmaz@nordeck.net>
2023-10-15 11:50:20 +0300

    installer, refactor: allow skipping DNS check

--- installer-sub-scripts/eb-galaxy/init.sh
+++ installer-sub-scripts/eb-galaxy/init.sh
@@ -20,10 +20,26 @@ echo
 
 [[ -z "$APP_FQDN" ]] && echo "APP_FQDN not found" && false
 [[ -z "$KRATOS_FQDN" ]] && echo "KRATOS_FQDN not found" && false
-[[ -z "$(dig +short $APP_FQDN)" ]] && \
-    echo "unresolvable APP_FQDN: $APP_FQDN" && false
-[[ -z "$(dig +short $KRATOS_FQDN)" ]] && \
-    echo "unresolvable KRATOS_FQDN: $KRATOS_FQDN" && false
+
+if [[ "$SKIP_DNS_CHECK" != true ]]; then
+  if [[ -z "$(dig +short $APP_FQDN)" ]]; then
+    cat <<EOF
+Unresolvable APP_FQDN: $APP_FQDN
+if this is a test setup, please set SKIP_DNS_CHECK before installation
+    export SKIP_DNS_CHECK=true
+EOF
+    false
+  fi
+
+  if [[ -z "$(dig +short $KRATOS_FQDN)" ]]; then
+    cat <<EOF
+Unresolvable APP_FQDN: $KRATOS_FQDN
+if this is a test setup, please set SKIP_DNS_CHECK before installation
+    export SKIP_DNS_CHECK=true
+EOF
+    false
+  fi
+fi
 
 # ------------------------------------------------------------------------------
 # INSTALLER CONFIGURATION
