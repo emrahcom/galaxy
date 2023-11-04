c01dbff861b5301b0fc836e23682a50ed7e91ad7
emrah <emrah.eryilmaz@nordeck.net>
2023-11-04 18:00:24 +0300

    eb-app-ui, refactor: info for meeting list

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting/list.svelte
@@ -54,9 +54,12 @@
             {/if}
 
             {#if p.info}
-              {#each p.info.split("\n") as line}
-                <p class="card-text text-muted my-0">{line}&nbsp;</p>
-              {/each}
+              <p
+                class="d-inline-block card-text text-muted text-start text-truncate bg-light w-auto"
+                style="max-width: 90%; white-space: pre"
+              >
+                {p.info}
+              </p>
             {/if}
           </div>
 
