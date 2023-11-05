c532eeda8434548e04cbd42581479e19443f119a
emrah <emrah.eryilmaz@nordeck.net>
2023-11-05 14:52:06 +0300

    eb-app-ui, fix: no need to check browser environment

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/logout/+page.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/logout/+page.ts
@@ -1,10 +1,7 @@
-import { browser } from "$app/environment";
 import { getLogoutDataModels } from "$lib/kratos";
 
 // -----------------------------------------------------------------------------
 export async function load() {
-  if (!browser) return {};
-
   const dm = await getLogoutDataModels();
 
   if (dm.instanceOf === "KratosLogout") {
