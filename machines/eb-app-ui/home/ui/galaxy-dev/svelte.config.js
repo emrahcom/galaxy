58777e3d5740120faac96f4b9d5b7d9f7326d8ac
emrah <emrah.eryilmaz@nordeck.net>
2023-11-05 13:13:56 +0300

    eb-app-ui, refactor: load the static adapter

--- machines/eb-app-ui/home/ui/galaxy-dev/svelte.config.js
+++ machines/eb-app-ui/home/ui/galaxy-dev/svelte.config.js
@@ -1,5 +1,5 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,9 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: "index.html",
+    }),
   },
 };
 
