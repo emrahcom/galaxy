9719711b6ffa17f570f575192343d307482890a5
emrah <emrah.eryilmaz@nordeck.net>
2023-11-05 14:10:39 +0300

    eb-app-ui, refactor: save identity to sessionStorage

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.ts
@@ -1,18 +1,22 @@
 import { getIdentity } from "$lib/kratos";
-import identity from "$lib/stores/kratos/identity";
-
-export const csr = true;
-export const prerender = false;
 
 // -----------------------------------------------------------------------------
 export async function load() {
+  // Get config
+  if (!window.localStorage.getItem("kratos_fqdn")) {
+    const config = await get("/api/adm/config");
+
+    window.localStorage.setItem("kratos_fqdn", config.kratos_fqdn);
+  }
+
+  window.sessionStorage.removeItem("identity_id");
+  window.sessionStorage.removeItem("identity_email");
   await getIdentity()
     .then((_identity) => {
-      identity.set(_identity);
+      window.sessionStorage.setItem("identity_id", _identity.id);
+      window.sessionStorage.setItem("identity_email", _identity.traits.email);
     })
     .catch(() => {
       //no identity
     });
-
-  return {};
 }
