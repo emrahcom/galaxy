a3abab6dc6b772e7b34bfee790341018540c17d8
emrah <emrah.eryilmaz@nordeck.net>
2023-11-05 15:37:51 +0300

    eb-app-ui, refactor: logout in svelte page

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/logout/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { getLogoutDataModels } from "$lib/kratos";
+
+  async function load() {
+    const dm = await getLogoutDataModels();
+
+    window.localStorage.removeItem("identity_id");
+    window.localStorage.removeItem("identity_email");
+
+    if (dm.instanceOf === "KratosLogout") {
+      window.location.replace(dm.logout_url);
+    } else {
+      window.location.replace("/");
+    }
+  }
+
+  load();
+</script>
