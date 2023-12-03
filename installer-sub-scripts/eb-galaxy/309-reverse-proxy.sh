708e44cd08d5bada0e18e4504a6fe333a99116c4
emrah <emrah.eryilmaz@nordeck.net>
2023-12-03 21:00:13 +0300

    eb-reverse-proxy, refactor: lxc start order

--- installer-sub-scripts/eb-galaxy/309-reverse-proxy.sh
+++ installer-sub-scripts/eb-galaxy/309-reverse-proxy.sh
@@ -95,7 +95,7 @@ cat >> /var/lib/lxc/$MACH/config <<EOF
 
 # Start options
 lxc.start.auto = 1
-lxc.start.order = 310
+lxc.start.order = 309
 lxc.start.delay = 2
 lxc.group = $TAG-group
 lxc.group = onboot
