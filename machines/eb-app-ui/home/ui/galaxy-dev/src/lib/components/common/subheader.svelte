ba5337ca32f7f77f8a19afbb602d0d7b54b26eb0
emrah <emrah.eryilmaz@nordeck.net>
2024-02-18 19:13:27 +0300

    eb-app-ui, feat: add subheader without a button

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/subheader.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let subheader: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="subheader">
+  <div class="d-flex">
+    <h3 class="text-muted my-auto">{subheader}</h3>
+  </div>
+</section>
