67d24b040ad98b053e862c74aa2e70752bd153d3
emrah <emrah.eryilmaz@nordeck.net>
2024-02-18 19:14:46 +0300

    eb-app-ui, feat: add the list contacts component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/list.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { Contact } from "$lib/types";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Update from "$lib/components/common/link-update.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let contacts: Contact[];
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="list">
+  <div class="row mx-auto mt-2 g-3">
+    {#each contacts as p}
+      <div class="col-md-6 col-xl-4">
+        <div class="card h-100 {p.is_default ? 'border-primary' : ''}">
+          <div class="card-body text-center">
+            <h5 class="card-title text-muted">{p.name}</h5>
+            <p class="card-text text-muted">{p.profile_name}</p>
+            <p class="card-text text-muted">{p.profile_email ?? "-"}</p>
+          </div>
+
+          <div class="card-footer bg-body border-0 text-center">
+            <Del href="/pri/contact/del/{p.id}" />
+            <Update href="/pri/contact/update/{p.id}" />
+          </div>
+        </div>
+      </div>
+    {:else}
+      <Warning>There is no contact in the list.</Warning>
+    {/each}
+  </div>
+</section>
