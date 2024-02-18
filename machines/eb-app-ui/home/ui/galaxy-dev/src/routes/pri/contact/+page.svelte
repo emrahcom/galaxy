18c7bc595a2033e4763f899f333f4b5e81f9a89d
emrah <emrah.eryilmaz@nordeck.net>
2024-02-18 19:15:14 +0300

    eb-app-ui, feat: add list contacts route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/contact/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { list } from "$lib/api";
+  import List from "$lib/components/pri/contact/list.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = list("/api/pri/contact/list", 100);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="My contacts" />
+
+{#await pr then contacts}
+  <List {contacts} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
