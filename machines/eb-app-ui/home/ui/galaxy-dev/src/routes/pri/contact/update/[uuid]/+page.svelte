cac33b645bccc5c27f24611a9d4d18baa27fa3c5
emrah <emrah.eryilmaz@nordeck.net>
2024-02-18 20:17:12 +0300

    eb-app-api, feat: contact update route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/contact/update/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Update from "$lib/components/pri/contact/update.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/contact/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Update this contact" />
+
+{#await pr then contact}
+  <Update p={contact} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
