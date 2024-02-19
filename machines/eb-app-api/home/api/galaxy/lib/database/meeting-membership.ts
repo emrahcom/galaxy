75f81a56a12b710758159147c10e8710bdada1a4
emrah <emrah.eryilmaz@nordeck.net>
2024-02-19 09:40:05 +0300

    eb-app-api, feat: add meeting owner as partner

--- machines/eb-app-api/home/api/galaxy/lib/database/meeting-membership.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/meeting-membership.ts
@@ -110,6 +110,33 @@ export async function addMeetingMembershipByCode(
   };
   if (rows[0] !== undefined) await query(sql2);
 
+  // add meeting owner to the partner's contact list
+  const sql3 = {
+    text: `
+      INSERT INTO contact (identity_id, contact_id, name)
+      VALUES (
+        $1,
+        (SELECT identity_id
+         FROM meeting_invite
+         WHERE code = $2
+        ),
+        (SELECT name
+         FROM profile
+         WHERE identity_id = (SELECT identity_id
+                              FROM meeting_invite
+                              WHERE code = $2
+                             )
+           AND is_default
+        )
+      )
+      ON CONFLICT DO NOTHING`,
+    args: [
+      identityId,
+      code,
+    ],
+  };
+  if (rows[0] !== undefined) await query(sql3);
+
   return rows;
 }
 
