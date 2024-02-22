e6ba303730f6418d0ed264f749ebd0c902f7505a
emrah <emrah.eryilmaz@nordeck.net>
2024-02-22 19:21:03 +0300

    eb-app-api, refactor: add contact_name into DomainPartner type

--- machines/eb-app-api/home/api/galaxy/lib/database/types.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/types.ts
@@ -85,6 +85,7 @@ export interface DomainInvite111 {
 export interface DomainPartner {
   id: string;
   domain_id: string;
+  contact_name: string;
   profile_name: string;
   profile_email: string;
   enabled: boolean;
