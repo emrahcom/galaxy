a92bfa3bd5b5626e95aa021493b58b4fa573f943
emrah <emrah.eryilmaz@nordeck.net>
2024-02-23 19:46:10 +0300

    eb-app-ui, feat: add domain candidate page

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/candidate/add/[domain_uuid]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  //import Add from "$lib/components/pri/domain-invite/add.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const domainId = $page.params.domain_uuid;
+
+  const pr = getById("/api/pri/domain/get", domainId);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Add candidate for domain partnership" />
+
+{#await pr then domain}
+  Add domain candidate
+  { console.log(domain) }
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
