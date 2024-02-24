895b3bf8752ae2b33460c09c1674190f819eff3e
emrah <emrah.eryilmaz@nordeck.net>
2024-02-24 20:39:46 +0300

    eb-app-ui, feat: add accept domain-candidacy component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-candidacy/accept.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { DomainCandidacy } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: DomainCandidacy;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = "/pri/domain";
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/domain/candidacy/accept", p.id);
+      window.location.href = "/pri/domain";
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="accept">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="name"
+        label="Jitsi Domain Name"
+        value={p.domain_name}
+        disabled={true}
+        readonly={true}
+      />
+      <Text
+        name="url"
+        label="Jitsi Domain URL"
+        value={p.domain_url}
+        disabled={true}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The accept request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Accept" />
+      </div>
+    </form>
+  </div>
+</section>
