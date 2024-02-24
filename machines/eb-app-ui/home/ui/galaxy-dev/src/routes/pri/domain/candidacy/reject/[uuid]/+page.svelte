074ebe64d6a65322b5dca9d1776f47d55c46e731
emrah <emrah.eryilmaz@nordeck.net>
2024-02-24 20:40:34 +0300

    eb-app-ui, feat: add reject domain-candidacy route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/candidacy/reject/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Reject from "$lib/components/pri/domain-candidacy/reject.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/domain/candidacy/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Reject partnership" />
+
+{#await pr then candidacy}
+  <Reject p={candidacy} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
