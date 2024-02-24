190859207a590f0dc51cda3af6b0fb4d737c709a
emrah <emrah.eryilmaz@nordeck.net>
2024-02-24 21:32:49 +0300

    eb-app-ui, refactor: candidacy accept.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partner-candidacy/accept.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partner-candidacy/accept.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { FORM_WIDTH } from "$lib/config";
   import { actionById } from "$lib/api";
-  import type { DomainCandidacy } from "$lib/types";
+  import type { DomainPartnerCandidacy } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
   import Submit from "$lib/components/common/button-submit.svelte";
   import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
   import Text from "$lib/components/common/form-text.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
-  export let p: DomainCandidacy;
+  export let p: DomainPartnerCandidacy;
 
   let warning = false;
 
@@ -21,7 +21,7 @@
   async function onSubmit() {
     try {
       warning = false;
-      await actionById("/api/pri/domain/candidacy/accept", p.id);
+      await actionById("/api/pri/domain/partner/candidacy/accept", p.id);
       window.location.href = "/pri/domain";
     } catch {
       warning = true;
