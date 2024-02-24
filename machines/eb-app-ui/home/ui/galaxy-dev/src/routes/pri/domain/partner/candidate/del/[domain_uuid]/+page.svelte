fa06466a7eaa330046c16b3a730ad1bcef684a2b
emrah <emrah.eryilmaz@nordeck.net>
2024-02-25 00:08:13 +0300

    eb-app-ui, feat: del route for the domain partner candidate

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/partner/candidate/del/[domain_uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/domain-partner-candidate/del.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/domain/partner/candidate/get", domainId);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Delete partnership" />
+
+{#await pr then candidate}
+  <Del {candidate} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
