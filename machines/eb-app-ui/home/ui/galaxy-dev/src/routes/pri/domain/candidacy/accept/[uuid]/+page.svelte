1f53191919d29bc3e3707e4f59128bd727775bfb
emrah <emrah.eryilmaz@nordeck.net>
2024-02-24 20:40:23 +0300

    eb-app-ui, feat: add accept domain-candidacy route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/candidacy/accept/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Accept from "$lib/components/pri/domain-candidacy/accept.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/domain/candidacy/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Accept partnership" />
+
+{#await pr then candidacy}
+  <Accept p={candidacy} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
