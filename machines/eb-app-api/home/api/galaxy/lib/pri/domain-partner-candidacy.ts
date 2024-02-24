ac2a325d2978bd838c4fc26fa0fb76c0681c02a2
emrah <emrah.eryilmaz@nordeck.net>
2024-02-24 22:23:19 +0300

    eb-app-api, feat: add the route for the domain candidacy accept

--- machines/eb-app-api/home/api/galaxy/lib/pri/domain-partner-candidacy.ts
+++ machines/eb-app-api/home/api/galaxy/lib/pri/domain-partner-candidacy.ts
@@ -4,6 +4,7 @@ import { getLimit, getOffset } from "../database/common.ts";
 import {
   getDomainPartnerCandidacy,
   listDomainPartnerCandidacy,
+  updateDomainPartnerCandidacyStatus,
 } from "../database/domain-partner-candidacy.ts";
 
 const PRE = "/api/pri/domain/partner/candidacy";
@@ -26,6 +27,18 @@ async function list(req: Request, identityId: string): Promise<unknown> {
 }
 
 // -----------------------------------------------------------------------------
+async function accept(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const candidacyId = pl.id;
+
+  return await updateDomainPartnerCandidacyStatus(
+    identityId,
+    candidacyId,
+    "accept",
+  );
+}
+
+// -----------------------------------------------------------------------------
 export default async function (
   req: Request,
   path: string,
@@ -35,6 +48,10 @@ export default async function (
     return await wrapper(get, req, identityId);
   } else if (path === `${PRE}/list`) {
     return await wrapper(list, req, identityId);
+  } else if (path === `${PRE}/accept`) {
+    return await wrapper(accept, req, identityId);
+  } else if (path === `${PRE}/reject`) {
+    return await wrapper(reject, req, identityId);
   } else {
     return notFound();
   }
