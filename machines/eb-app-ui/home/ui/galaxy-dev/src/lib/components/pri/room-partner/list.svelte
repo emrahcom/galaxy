40a2e94381f864356e9597b0d6ba61b5861f3163
emrah <emrah.eryilmaz@nordeck.net>
2024-02-25 18:24:36 +0300

    eb-app-ui, feat: add candidates into room-partner list

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-partner/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-partner/list.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-  import type { RoomPartner } from "$lib/types";
+  import type { RoomPartner, RoomPartnerCandidate } from "$lib/types";
   import Del from "$lib/components/common/link-del.svelte";
   import Disable from "$lib/components/common/link-disable.svelte";
   import Enable from "$lib/components/common/link-enable.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   export let partners: RoomPartner[];
+  export let candidates: RoomPartnerCandidate[];
+
+  const isEmpty = !(partners.length || candidates.length);
 </script>
 
 <!-- -------------------------------------------------------------------------->
@@ -15,7 +18,9 @@
       <div class="col-md-6 col-xl-4">
         <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
           <div class="card-body text-center">
-            <h5 class="card-title text-muted">{p.profile_name}</h5>
+            <h5 class="card-title text-muted">
+              {p.contact_name || p.profile_name}
+            </h5>
 
             <p class="card-text text-muted">
               {p.profile_email}
@@ -33,8 +38,38 @@
           </div>
         </div>
       </div>
-    {:else}
-      <Warning>This room has no partner.</Warning>
     {/each}
+
+    {#each candidates as c}
+      <div class="col-md-6 col-xl-4">
+        <div class="card h-100">
+          <div class="card-body text-center">
+            <h5 class="card-title text-muted">
+              {p.contact_name || p.profile_name}
+            </h5>
+
+            <p class="card-text text-muted">
+              {p.profile_email}
+            </p>
+
+            {#if c.status == "pending"}
+              <p class="card-text fw-bold text-success">pending</p>
+            {:else}
+              <p class="card-text fw-bold text-warning">rejected</p>
+            {/if}
+          </div>
+
+          <div class="card-footer bg-body border-0 text-center">
+            {#if c.status == "pending"}
+              <Del href="/pri/room/partner/candidate/del/{c.id}" />
+            {/if}
+          </div>
+        </div>
+      </div>
+    {/each}
+
+    {#if isEmpty}
+      <Warning>This room has no partner.</Warning>
+    {/if}
   </div>
 </section>
