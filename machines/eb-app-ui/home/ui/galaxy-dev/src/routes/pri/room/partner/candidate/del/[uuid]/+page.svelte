7866989cacaa37099c904d4b86a6c087c33a6adc
emrah <emrah.eryilmaz@nordeck.net>
2024-02-25 18:02:37 +0300

    eb-app-ui, feat: add room-partner candidate delete route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/room/partner/candidate/del/[uuid]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/room-partner-candidate/del.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById(
+    "/api/pri/room/partner/candidate/get",
+    $page.params.uuid,
+  );
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Delete partnership" />
+
+{#await pr then candidate}
+  <Del p={candidate} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
