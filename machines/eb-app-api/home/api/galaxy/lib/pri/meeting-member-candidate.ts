a52d1ee88a54e26ed59586868cc9c544862fa799
emrah <emrah.eryilmaz@nordeck.net>
2024-02-25 23:07:40 +0300

    eb-app-api, feat: meeting-member-candidate.ts

---
+++ machines/eb-app-api/home/api/galaxy/lib/pri/meeting-member-candidate.ts
@@ -0,0 +1,73 @@
+import { notFound } from "../http/response.ts";
+import { pri as wrapper } from "../http/wrapper.ts";
+import { getLimit, getOffset } from "../database/common.ts";
+import {
+  addMeetingMemberCandidate,
+  delMeetingMemberCandidate,
+  getMeetingMemberCandidate,
+  listMeetingMemberCandidateByMeeting,
+} from "../database/meeting-member-candidate.ts";
+
+const PRE = "/api/pri/meeting/member/candidate";
+
+// -----------------------------------------------------------------------------
+async function get(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const candidacyId = pl.id;
+
+  return await getMeetingMemberCandidate(identityId, candidacyId);
+}
+
+// -----------------------------------------------------------------------------
+async function listByMeeting(
+  req: Request,
+  identityId: string,
+): Promise<unknown> {
+  const pl = await req.json();
+  const meeting = pl.id;
+  const limit = getLimit(pl.limit);
+  const offset = getOffset(pl.offset);
+
+  return await listMeetingMemberCandidateByMeeting(
+    identityId,
+    meetingId,
+    limit,
+    offset,
+  );
+}
+
+// -----------------------------------------------------------------------------
+async function add(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const meetingId = pl.meeting_id;
+  const contactId = pl.contact_id;
+
+  return await addMeetingMemberCandidate(identityId, meetingId, contactId);
+}
+
+// -----------------------------------------------------------------------------
+async function del(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const candidacyId = pl.id;
+
+  return await delMeetingMemberCandidate(identityId, candidacyId);
+}
+
+// -----------------------------------------------------------------------------
+export default async function (
+  req: Request,
+  path: string,
+  identityId: string,
+): Promise<Response> {
+  if (path === `${PRE}/get`) {
+    return await wrapper(get, req, identityId);
+  } else if (path === `${PRE}/list/bymeeting`) {
+    return await wrapper(listByMeeting, req, identityId);
+  } else if (path === `${PRE}/add`) {
+    return await wrapper(add, req, identityId);
+  } else if (path === `${PRE}/del`) {
+    return await wrapper(del, req, identityId);
+  } else {
+    return notFound();
+  }
+}
