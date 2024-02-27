dbfa491135dbc93484c61c78a1242f78f9bca86a
emrah <emrah.eryilmaz@nordeck.net>
2024-02-27 22:41:23 +0300

    eb-app-ui, feat: add the meeting-membership accept route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/member/candidacy/accept/[uuid]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Accept from "$lib/components/pri/meeting-member-candidacy/accept.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById(
+    "/api/pri/meeting/member/candidacy/get",
+    $page.params.uuid,
+  );
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Accept membership" />
+
+{#await pr then candidacy}
+  <Accept p={candidacy} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
