a494ca709c1f8265f7aff9af1a9caac38cb7562f
emrah <emrah.eryilmaz@nordeck.net>
2024-02-27 22:31:24 +0300

    eb-app-ui, feat: candidacy into the meeting route page

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/+page.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/+page.svelte
@@ -4,14 +4,15 @@
   import Subheader from "$lib/components/common/subheader-add.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
-  const pr = list("/api/pri/meeting/list", 100);
+  const pr1 = list("/api/pri/meeting/list", 100);
+  const pr2 = list("/api/pri/meeting/member/candidacy/list", 100);
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <Subheader subheader="My meetings" hrefAdd="/pri/meeting/add" />
 
-{#await pr then meetings}
-  <List {meetings} />
+{#await Promise.all([pr1, pr2]) then [meetings, candidacies]}
+  <List {meetings} {candidacies} />
 {:catch}
   <Warning>Something went wrong</Warning>
 {/await}
