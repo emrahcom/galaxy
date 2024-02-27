10f9934c2821748349a5627001cc16ebdb23b55a
emrah <emrah.eryilmaz@nordeck.net>
2024-02-27 22:42:52 +0300

    eb-app-ui, feat: add the meeting-membership reject route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/member/candidacy/reject/[uuid]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Reject from "$lib/components/pri/meeting-member-candidacy/reject.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById(
+    "/api/pri/meeting/member/candidacy/get",
+    $page.params.uuid,
+  );
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Reject membership" />
+
+{#await pr then candidacy}
+  <Reject p={candidacy} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
