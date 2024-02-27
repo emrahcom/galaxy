98521d87cf5f2a7a7a63c0786b5f4ebba978959b
emrah <emrah.eryilmaz@nordeck.net>
2024-02-27 23:34:22 +0300

    eb-app-ui, feat: initialize the meeting-member-candidacy reject

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member-candidacy/reject.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { MeetingMemeberCandidacy } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: MeetingMemeberCandidacy;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = "/pri/meeting";
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/meeting/member/candidacy/reject", p.id);
+      window.location.href = "/pri/meeting";
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="reject">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="meeting_name"
+        label="Meeting"
+        value={p.meeting_name}
+        disabled={true}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The reject request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Reject" />
+      </div>
+    </form>
+  </div>
+</section>
