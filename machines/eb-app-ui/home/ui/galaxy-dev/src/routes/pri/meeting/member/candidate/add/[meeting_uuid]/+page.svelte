b7e2030e3aaf57c044bb1516cd2a19defb5ec125
emrah <emrah.eryilmaz@nordeck.net>
2024-02-27 22:47:31 +0300

    eb-app-ui, feat: add the meeting-member candidate add route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/member/candidate/add/[meeting_uuid]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Add from "$lib/components/pri/meeting-member-candidate/add.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const meetingId = $page.params.meeting_uuid;
+
+  const pr = getById("/api/pri/meeting/get", meetingId);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Invite contact to membership" />
+
+{#await pr then meeting}
+  <Add {meeting} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
