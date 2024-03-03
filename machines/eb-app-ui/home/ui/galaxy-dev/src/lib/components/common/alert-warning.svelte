d4546ff0b32277918f359b3e738cfb23ddfca165
emrah <emrah.eryilmaz@nordeck.net>
2024-03-03 20:26:09 +0300

    eb-app-ui, refactor: centerize the alert text

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/alert-warning.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/alert-warning.svelte
@@ -1,5 +1,5 @@
 <div class="d-flex justify-content-center my-5">
-  <div class="alert alert-warning my-auto px-5" role="alert">
+  <div class="alert alert-warning text-center my-auto px-5" role="alert">
     <slot />
   </div>
 </div>
