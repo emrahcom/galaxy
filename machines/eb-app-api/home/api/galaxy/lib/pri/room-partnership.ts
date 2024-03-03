77c117df122d4fa5f638c35b44a1f66b9a0cce79
emrah <emrah.eryilmaz@nordeck.net>
2024-03-03 21:11:06 +0300

    eb-app-api, feat: route for checking room partnership by code

--- machines/eb-app-api/home/api/galaxy/lib/pri/room-partnership.ts
+++ machines/eb-app-api/home/api/galaxy/lib/pri/room-partnership.ts
@@ -2,6 +2,7 @@ import { notFound } from "../http/response.ts";
 import { pri as wrapper } from "../http/wrapper.ts";
 import {
   addRoomPartnershipByCode,
+  checkRoomPartnershipByCode,
   delRoomPartnership,
   getRoomPartnership,
 } from "../database/room-partnership.ts";
@@ -14,6 +15,14 @@ async function get(req: Request, identityId: string): Promise<unknown> {
   const partnershipId = pl.id;
 
   return await getRoomPartnership(identityId, partnershipId);
+}
+
+// -----------------------------------------------------------------------------
+async function checkByCode(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const code = pl.code;
+
+  return await checkRoomPartnershipByCode(identityId, code);
 }
 
 // -----------------------------------------------------------------------------
@@ -40,6 +49,8 @@ export default async function (
 ): Promise<Response> {
   if (path === `${PRE}/get`) {
     return await wrapper(get, req, identityId);
+  } else if (path === `${PRE}/check/bycode`) {
+    return await wrapper(checkByCode, req, identityId);
   } else if (path === `${PRE}/add/bycode`) {
     return await wrapper(addByCode, req, identityId);
   } else if (path === `${PRE}/del`) {
