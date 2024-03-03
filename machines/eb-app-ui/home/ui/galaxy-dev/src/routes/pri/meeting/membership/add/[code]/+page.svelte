edf1f00dc28d50084b4774b33bed924d9b711727
emrah <emrah.eryilmaz@nordeck.net>
2024-03-03 21:30:39 +0300

    eb-app-ui, feat: update the route for membership adding

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/membership/add/[code]/+page.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/membership/add/[code]/+page.svelte
@@ -2,19 +2,38 @@
   import { page } from "$app/stores";
   import { getByCode } from "$lib/api";
   import Add from "$lib/components/pri/meeting-membership/add.svelte";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
   import Subheader from "$lib/components/common/subheader-center.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   const code = $page.params.code;
 
-  const pr = getByCode("/api/pri/meeting/invite/get/bycode", code);
+  const pr1 = getByCode("/api/pri/meeting/invite/get/bycode", code);
+  const pr2 = getByCode("/api/pri/meeting/membership/check/bycode", code);
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = `/pri/meeting`;
+  }
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <Subheader subheader="Subscribe to this meeting" />
 
-{#await pr then invite}
-  <Add {invite} />
+{#await pr1 then invite}
+  <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+  {#await pr2 then _checked}
+    <Add {invite} isExist={true} />
+  {:catch}
+    <Add {invite} isExist={false} />
+  {/await}
 {:catch}
-  <Warning>Something went wrong</Warning>
+  <Warning>
+    Something went wrong.<br />
+    Are you sure this link is valid and not expired?
+  </Warning>
+
+  <div class="d-flex gap-5 mt-5 justify-content-center">
+    <Cancel label="Abort" on:click={cancel} />
+  </div>
 {/await}
