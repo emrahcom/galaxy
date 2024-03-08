ab74614b61694741d379b7d1c9418e4a1d16cf95
emrah <emrah.eryilmaz@nordeck.net>
2024-03-08 23:21:26 +0300

    feat(eb-app-ui): route for disable schedule

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/meeting/schedule/disable/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Disable from "$lib/components/pri/meeting-schedule/disable.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/meeting/schedule/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Disable this schedule" />
+
+{#await pr then schedule}
+  <Disable p={schedule} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
