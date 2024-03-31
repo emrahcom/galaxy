80c9e545b83a5cd01181208df525f5e46644f83a
emrah <emrah.eryilmaz@nordeck.net>
2024-03-31 13:50:04 +0300

    refactor(eb-app-ui): inititalize form-time

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-time.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let label: string;
+  export let name: string;
+  export let disabled = false;
+  export let readonly = false;
+  export let required = false;
+  export let value: string;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="form-floating my-3">
+  <input
+    type="time"
+    id={name}
+    class="form-control"
+    {name}
+    placeholder=""
+    bind:value
+    {disabled}
+    {readonly}
+    {required}
+    tabindex={disabled || readonly ? -1 : undefined}
+  />
+  <label for={name}>{label}</label>
+</div>
