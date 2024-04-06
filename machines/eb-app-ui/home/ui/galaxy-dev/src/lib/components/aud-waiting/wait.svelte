a11332cf0a6aed983f1903c7a840e6f2b8099ad0
emrah <emrah.eryilmaz@nordeck.net>
2024-04-06 15:03:17 +0300

    refactor(eb-app-ui): renamed as showLocaleDatetime in aud waiting

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/aud-waiting/wait.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/aud-waiting/wait.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { FORM_WIDTH } from "$lib/config";
-  import { epochToIntervalString, toLocaleDatetime } from "$lib/common";
+  import { epochToIntervalString, showLocaleDatetime } from "$lib/common";
   import { getByCode } from "$lib/api";
   import type { MeetingSchedule111 } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
@@ -77,7 +77,7 @@
         {/if}
 
         <p class="card-text text-muted small">
-          {toLocaleDatetime(p.started_at)}
+          {showLocaleDatetime(p.started_at)}
         </p>
 
         {#if p.meeting_info}
