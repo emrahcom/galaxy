a21b9f76ba4ef2c208cb8389992372226722b2d7
emrah <emrah.eryilmaz@nordeck.net>
2024-04-07 16:41:03 +0300

    feat(eb-app-ui): add form-select-every

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-select-every.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let disabled = false;
+  export let id: string;
+  export let label: string;
+  export let max = 100;
+  export let min = 1;
+  export let required = true;
+  export let unit: string;
+  export let value: number;
+
+  const options = Array.from(
+    { length: max - min + 1 },
+    (_, index) => min + index,
+  );
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="form-floating my-3">
+  <select
+    class="form-select"
+    {id}
+    bind:value
+    aria-label={label}
+    {disabled}
+    {required}
+  >
+    {#each options as opt}
+      <option value={opt}>{opt} {unit}{opt > 1 && "s"}</option>
+    {/each}
+  </select>
+  <label for={id}>{label}</label>
+</div>
