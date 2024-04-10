486289bd77219050a7f7a698994c0226dc0fe005
emrah <emrah.eryilmaz@nordeck.net>
2024-04-10 21:56:01 +0300

    feat(eb-app-ui): add form-checkbox component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-checkbox.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let disabled = false;
+  export let label: string;
+  export let name: string;
+  export let value: boolean;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="form-check">
+  <input
+    class="form-check-input"
+    type="checkbox"
+    id={name}
+    bind:checked={value}
+    aria-checked={value}
+    {disabled}
+  />
+  <label class="form-check-label" for={name}>{label}</label>
+</div>
