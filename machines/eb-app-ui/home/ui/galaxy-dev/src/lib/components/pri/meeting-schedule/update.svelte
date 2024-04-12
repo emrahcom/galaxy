89b82f8e000528a0d197e6d1abfe0f4fe2a82325
emrah <emrah.eryilmaz@nordeck.net>
2024-04-12 21:27:33 +0300

    refactor(eb-app-ui): weekly in the meeting-schedule update

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/update.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/update.svelte
@@ -12,6 +12,7 @@
   } from "$lib/common";
   import type { MeetingSchedule } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Checkbox from "$lib/components/common/form-checkbox.svelte";
   import Day from "$lib/components/common/form-date.svelte";
   import Numeric from "$lib/components/common/form-select-number.svelte";
   import Range from "$lib/components/common/form-range.svelte";
@@ -27,6 +28,9 @@
   const defaultDuration = Number(p.schedule_attr.duration);
   let duration = defaultDuration;
   let date0 = toLocaleDate(p.schedule_attr.started_at);
+  let date1 = p.schedule_attr.rep_end_at
+    ? toLocaleDate(p.schedule_attr.rep_end_at)
+    : date0;
   let time0 = toLocaleTime(p.schedule_attr.started_at);
   let time1 = getEndTime(time0, duration);
   let allDay = isAllDay(p.schedule_attr.started_at, p.schedule_attr.duration);
@@ -34,6 +38,27 @@
   let times = Number(p.schedule_attr.rep_end_x) || 10;
   let notBefore = today();
   if (date0 < notBefore) notBefore = date0;
+  let d0 = p.schedule_attr.rep_days
+    ? Boolean(Number(p.schedule_attr.rep_days[0]))
+    : false;
+  let d1 = p.schedule_attr.rep_days
+    ? Boolean(Number(p.schedule_attr.rep_days[1]))
+    : false;
+  let d2 = p.schedule_attr.rep_days
+    ? Boolean(Number(p.schedule_attr.rep_days[2]))
+    : false;
+  let d3 = p.schedule_attr.rep_days
+    ? Boolean(Number(p.schedule_attr.rep_days[3]))
+    : false;
+  let d4 = p.schedule_attr.rep_days
+    ? Boolean(Number(p.schedule_attr.rep_days[4]))
+    : false;
+  let d5 = p.schedule_attr.rep_days
+    ? Boolean(Number(p.schedule_attr.rep_days[5]))
+    : false;
+  let d6 = p.schedule_attr.rep_days
+    ? Boolean(Number(p.schedule_attr.rep_days[6]))
+    : false;
   let warning = false;
 
   // ---------------------------------------------------------------------------
@@ -105,6 +130,7 @@
     }
 
     const started_at = new Date(`${date0}T${time0}`);
+    const ended_at = new Date(`${date1}T23:59:59`);
 
     if (p.schedule_attr.type === "o") {
       // if the end time of the only session is over, throw an error
@@ -120,6 +146,25 @@
       p.schedule_attr.rep_end_type = "x";
       p.schedule_attr.rep_end_x = String(times);
       p.schedule_attr.rep_every = String(every);
+    } else if (p.schedule_attr.type === "w") {
+      // If the end date is over, throw an error.
+      if (isOver(ended_at, 0)) throw new Error("it is already over");
+      // if the last date is earlier than the first date, throw an error.
+      if (date1 < date0) throw new Error("invalid period");
+      // if no selected day, throw an error.
+      if (!(d0 || d1 || d2 || d3 || d4 || d5 || d6)) throw new Error("no day");
+
+      p.schedule_attr.rep_end_type = "at";
+      p.schedule_attr.rep_end_at = ended_at.toISOString();
+      p.schedule_attr.rep_every = String(every);
+      p.schedule_attr.rep_days =
+        (d0 ? "1" : "0") +
+        (d1 ? "1" : "0") +
+        (d2 ? "1" : "0") +
+        (d3 ? "1" : "0") +
+        (d4 ? "1" : "0") +
+        (d5 ? "1" : "0") +
+        (d6 ? "1" : "0");
     }
 
     p.schedule_attr.started_at = started_at.toISOString();
@@ -176,9 +221,42 @@
           min={2}
           max={99}
         />
+      {:else if p.schedule_attr.type === "w"}
+        <Day
+          name="date0"
+          label="From"
+          bind:value={date0}
+          min={notBefore}
+          required={true}
+        />
+        <Day
+          name="date1"
+          label="To"
+          bind:value={date1}
+          min={notBefore}
+          required={true}
+        />
+        <Numeric
+          id="every"
+          label="Every"
+          bind:value={every}
+          unit="week"
+          max={13}
+        />
+        <div class="d-flex justify-content-center">
+          <Checkbox name="d0" label="Sun" bind:value={d0} />
+          <Checkbox name="d1" label="Mon" bind:value={d1} />
+          <Checkbox name="d2" label="Tue" bind:value={d2} />
+          <Checkbox name="d3" label="Wed" bind:value={d3} />
+          <Checkbox name="d4" label="Thu" bind:value={d4} />
+          <Checkbox name="d5" label="Fri" bind:value={d5} />
+          <Checkbox name="d6" label="Sat" bind:value={d6} />
+        </div>
       {/if}
 
-      <Switch name="all_day" label="All day meeting" bind:value={allDay} />
+      <div class="mt-4">
+        <Switch name="all_day" label="All day meeting" bind:value={allDay} />
+      </div>
 
       {#if !allDay}
         <Time
