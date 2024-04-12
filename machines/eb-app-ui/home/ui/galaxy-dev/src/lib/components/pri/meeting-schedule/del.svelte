7a4416c9a8badddddd33224869215907bee50dda
emrah <emrah.eryilmaz@nordeck.net>
2024-04-12 15:42:41 +0300

    refactor(eb-app-ui): weekly in meeting-schedule del

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/del.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/del.svelte
@@ -4,6 +4,7 @@
   import { isAllDay, toLocaleDate, toLocaleInterval } from "$lib/common";
   import type { MeetingSchedule } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Checkbox from "$lib/components/common/form-checkbox.svelte";
   import Day from "$lib/components/common/form-date.svelte";
   import Submit from "$lib/components/common/button-submit.svelte";
   import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
@@ -13,6 +14,14 @@
   export let p: MeetingSchedule;
 
   const date0 = toLocaleDate(p.schedule_attr.started_at);
+  const date1 = toLocaleDate(p.schedule_attr.rep_end_at);
+  const d0 = p.schedule_attr.rep_days[0] === "1" ? true : false;
+  const d1 = p.schedule_attr.rep_days[1] === "1" ? true : false;
+  const d2 = p.schedule_attr.rep_days[2] === "1" ? true : false;
+  const d3 = p.schedule_attr.rep_days[3] === "1" ? true : false;
+  const d4 = p.schedule_attr.rep_days[4] === "1" ? true : false;
+  const d5 = p.schedule_attr.rep_days[5] === "1" ? true : false;
+  const d6 = p.schedule_attr.rep_days[6] === "1" ? true : false;
   const interval = toLocaleInterval(
     p.schedule_attr.started_at,
     Number(p.schedule_attr.duration),
@@ -32,6 +41,12 @@
       times = "1 time";
     } else {
       times = `${p.schedule_attr.rep_end_x} times`;
+    }
+  } else if (p.schedule_attr.type === "w") {
+    if (p.schedule_attr.rep_every === "1") {
+      every = "1 week";
+    } else {
+      every = `${p.schedule_attr.rep_every} weeks`;
     }
   }
 
@@ -86,6 +101,31 @@
           disabled={true}
           readonly={true}
         />
+      {:else if p.schedule_attr.type === "w"}
+        <Day
+          name="date0"
+          label="From"
+          value={date0}
+          disabled={true}
+          readonly={true}
+        />
+        <Day
+          name="date1"
+          label="To"
+          value={date1}
+          disabled={true}
+          readonly={true}
+        />
+        <Text name="every" label="Every" value={every} disabled={true} />
+        <div class="d-flex justify-content-center">
+          <Checkbox name="d0" label="Sun" value={d0} disabled={true} />
+          <Checkbox name="d1" label="Mon" value={d1} disabled={true} />
+          <Checkbox name="d2" label="Tue" value={d2} disabled={true} />
+          <Checkbox name="d3" label="Wed" value={d3} disabled={true} />
+          <Checkbox name="d4" label="Thu" value={d4} disabled={true} />
+          <Checkbox name="d5" label="Fri" value={d5} disabled={true} />
+          <Checkbox name="d6" label="Sat" value={d6} disabled={true} />
+        </div>
       {/if}
 
       {#if isAllDay(p.schedule_attr.started_at, p.schedule_attr.duration)}
