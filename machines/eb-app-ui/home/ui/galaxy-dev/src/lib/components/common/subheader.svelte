6c47ff5b1de2a7d8377187810289bbdba331ebed
emrah <emrah.eryilmaz@nordeck.net>
2024-04-20 17:31:49 +0300

    refactor(eb-app-ui): hrefAdd into subheader.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/subheader.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/subheader.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
-  export let subheader: string;
+  export let hrefAdd = "";
+  export let subheader = "";
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <section id="subheader">
   <div class="d-flex">
     <h3 class="text-muted my-auto">{subheader}</h3>
+
+    {#if hrefAdd}
+      <a
+        class="btn btn-outline-secondary ms-auto me-2 my-auto"
+        role="button"
+        href={hrefAdd}
+      >
+        <span class="bi bi-plus-lg" />
+      </a>
+    {/if}
   </div>
 </section>
