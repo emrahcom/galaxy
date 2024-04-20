92ba326c04e7b673caf87aaf99de3fb26f842495
emrah <emrah.eryilmaz@nordeck.net>
2024-04-20 17:23:39 +0300

    feat(eb-app-ui): add the calendar route

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/calendar/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Subheader from "$lib/components/common/subheader.svelte";
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="My calendar" />
+
+Hello
