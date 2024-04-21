f67fcd02895cebe3a162b0a9efce8c5f32e768d2
emrah <emrah.eryilmaz@nordeck.net>
2024-04-21 16:06:24 +0300

    refactor(eb-app-ui): validate date in calendar

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/calendar/month/[date]/+page.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/calendar/month/[date]/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import { today, toLocaleDate } from "$lib/common";
   import Subheader from "$lib/components/common/subheader.svelte";
+
+  // validate date which comes from the path
+  try {
+    const date = toLocaleDate($page.params.date);
+
+    if (date != $page.params.date) {
+      window.location.href = `/pri/calendar/month/${date}`;
+    }
+  } catch {
+    const date = today();
+    window.location.href = `/pri/calendar/month/${date}`;
+  }
 
   console.error($page.params.date);
 </script>
