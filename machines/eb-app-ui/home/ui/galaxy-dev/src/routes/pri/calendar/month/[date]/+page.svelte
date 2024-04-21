c26b88f9a56c36deac9152babe908f7ad30486f0
emrah <emrah.eryilmaz@nordeck.net>
2024-04-21 17:53:26 +0300

    refactor(eb-app-ui): firstOfMonth and firstOfWeek in calendar

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/calendar/month/[date]/+page.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/calendar/month/[date]/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { today, toLocaleDate } from "$lib/common";
+  import {
+    firstDayOfMonth,
+    firstDayOfWeek,
+    today,
+    toLocaleDate,
+  } from "$lib/common";
   import Subheader from "$lib/components/common/subheader.svelte";
 
   // validate date which comes from the path
@@ -15,7 +20,14 @@
     window.location.href = `/pri/calendar/month/${date}`;
   }
 
+  const date = $page.params.date;
+  const firstOfMonth = firstDayOfMonth(date);
+  const firstOfWeek = firstDayOfWeek(firstOfMonth);
+
   console.error($page.params.date);
+  console.error(date);
+  console.error(firstOfMonth);
+  console.error(firstOfWeek);
 </script>
 
 <!-- -------------------------------------------------------------------------->
