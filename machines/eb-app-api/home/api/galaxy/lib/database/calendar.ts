f3f067baabf9b7f043357bb041e16e3ea32c86db
emrah <emrah.eryilmaz@nordeck.net>
2024-04-21 19:10:40 +0300

    feat(eb-app-api): initialize the calendar database module

---
+++ machines/eb-app-api/home/api/galaxy/lib/database/calendar.ts
@@ -0,0 +1,17 @@
+import { fetch } from "./common.ts";
+
+// -----------------------------------------------------------------------------
+export async function listSessionByMonth(
+  identityId: string,
+  date: string,
+  limit: number,
+  offset: number,
+) {
+
+  console.log(identityId);
+  console.log(date);
+  console.log(limit);
+  console.log(offset);
+
+  return [];
+}
