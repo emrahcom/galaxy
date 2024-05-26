a57d0713b8c00bcb41a9a5e69e128a4ff9b925b7
emrah <emrah.eryilmaz@nordeck.net>
2024-05-26 17:13:52 +0300

    refactor(eb-app-ui): calendar table

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/calendar/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/calendar/list.svelte
@@ -18,11 +18,30 @@
 
 <!-- -------------------------------------------------------------------------->
 <section id="list">
-  {#each WEEKS as week}
-    {week} -
-    {#each DAYS as day}
-      {day},
-    {/each}
-    <br />
-  {/each}
+  <div class="row">
+    <div class="col-12">
+      <table>
+        <thead>
+          <tr>
+            <th>Sun</th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each WEEKS as week}
+            <tr>
+              {#each DAYS as day}
+                <td>{week}-{day}</td>
+              {/each}
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  </div>
 </section>
