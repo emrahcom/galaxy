d847b7af5db8f961667e02e74e6bc9f9b5d91cab
emrah <emrah.eryilmaz@nordeck.net>
2024-06-09 19:02:58 +0300

    fix(eb-app-api): membership session into listSessionByMonth

--- machines/eb-app-api/home/api/galaxy/lib/database/calendar.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/calendar.ts
@@ -63,6 +63,50 @@ export async function listSessionByMonth(
             )
         AND ses.started_at > $2
         AND ses.started_at < $3
+
+      UNION
+
+      SELECT m.id, m.name as meeting_name, m.info as meeting_info,
+        s.name as schedule_name, ses.started_at, ses.ended_at, ses.duration,
+        0 as waiting_time, mem.join_as
+      FROM meeting_member mem
+        JOIN meeting m ON mem.meeting_id = m.id
+                          AND m.enabled
+        JOIN room r ON m.room_id = r.id
+                       AND r.enabled
+        JOIN domain d ON r.domain_id = d.id
+                         AND d.enabled
+        JOIN identity i1 ON d.identity_id = i1.id
+                            AND i1.enabled
+        JOIN identity i2 ON r.identity_id = i2.id
+                            AND i2.enabled
+        JOIN identity i3 ON m.identity_id = i3.id
+                            AND i3.enabled
+        JOIN meeting_schedule s ON m.id = s.meeting_id
+                            AND s.enabled
+        JOIN meeting_session ses ON s.id = ses.meeting_schedule_id
+      WHERE mem.identity_id = $1
+        AND mem.enabled
+        AND (r.identity_id = m.identity_id
+             OR EXISTS (SELECT 1
+                        FROM room_partner
+                        WHERE identity_id = m.identity_id
+                          AND room_id = r.id
+                          AND enabled
+                       )
+            )
+        AND (d.public
+             OR d.identity_id = r.identity_id
+             OR EXISTS (SELECT 1
+                        FROM domain_partner
+                        WHERE identity_id = r.identity_id
+                          AND domain_id = d.id
+                          AND enabled
+                       )
+            )
+        AND ses.started_at > $2
+        AND ses.started_at < $3
+
       ORDER BY ses.started_at
       LIMIT $4 OFFSET $5`,
     args: [
