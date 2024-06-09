af70a845e8a1609104057b860fcb52f244cd8f4b
emrah <emrah.eryilmaz@nordeck.net>
2024-06-09 18:34:45 +0300

    fix(eb-app-api): listSessionByMonth

--- machines/eb-app-api/home/api/galaxy/lib/database/calendar.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/calendar.ts
@@ -21,27 +21,54 @@ export async function listSessionByMonth(
   const firstOfMonth = getFirstDayOfMonth(date);
   const firstOfWeek = getFirstDayOfWeek(firstOfMonth);
   const firstDay = dateAfterXDays(firstOfWeek, -1);
+  const lastDay = dateAfterXDays(firstDay, 33);
+
   const sql = {
     text: `
-      SELECT pa.id, pa.domain_id, co.name as contact_name,
-        pr.name as profile_name, pr.email as profile_email, pa.enabled,
-        pa.created_at, pa.updated_at
-      FROM domain_partner pa
-        LEFT JOIN contact co ON co.identity_id = $1
-                                AND co.remote_id = pa.identity_id
-        LEFT JOIN profile pr ON pa.identity_id = pr.identity_id
-                                AND pr.is_default
-      WHERE pa.domain_id = $2
-        AND EXISTS (SELECT 1
-                    FROM domain
-                    WHERE id = pa.domain_id
-                      AND identity_id = $1
-                   )
-      ORDER BY profile_name, profile_email
-      LIMIT $3 OFFSET $4`,
+      SELECT m.id, m.name as meeting_name, m.info as meeting_info,
+        s.name as schedule_name, ses.started_at, ses.ended_at, ses.duration,
+        0 as waiting_time, 'host' as join_as
+      FROM meeting m
+        JOIN room r ON m.room_id = r.id
+                       AND r.enabled
+        JOIN domain d ON r.domain_id = d.id
+                         AND d.enabled
+        JOIN identity i1 ON d.identity_id = i1.id
+                            AND i1.enabled
+        JOIN identity i2 ON r.identity_id = i2.id
+                            AND i2.enabled
+        JOIN identity i3 ON m.identity_id = i3.id
+                            AND i3.enabled
+        JOIN meeting_schedule s ON m.id = s.meeting_id
+                                   AND s.enabled
+        JOIN meeting_session ses ON s.id = ses.meeting_schedule_id
+      WHERE m.identity_id = $1
+        AND m.enabled
+        AND (r.identity_id = $1
+             OR EXISTS (SELECT 1
+                        FROM room_partner
+                        WHERE identity_id = $1
+                          AND room_id = r.id
+                          AND enabled
+                       )
+            )
+        AND (d.public
+             OR d.identity_id = r.identity_id
+             OR EXISTS (SELECT 1
+                        FROM domain_partner
+                        WHERE identity_id = r.identity_id
+                          AND domain_id = d.id
+                          AND enabled
+                       )
+            )
+        AND ses.started_at > $2
+        AND ses.started_at < $3
+      ORDER BY ses.started_at
+      LIMIT $4 OFFSET $5`,
     args: [
       identityId,
       firstDay,
+      lastDay,
       limit,
       offset,
     ],
