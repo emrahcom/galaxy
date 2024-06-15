1ff37ae4f0eed2f9066f7a503bf08f0312cde361
emrah <emrah.eryilmaz@nordeck.net>
2024-06-15 20:50:58 +0300

    fix(eb-app-api): period in SQL for calendar

--- machines/eb-app-api/home/api/galaxy/lib/database/calendar.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/calendar.ts
@@ -16,12 +16,16 @@ export async function listSessionByMonth(
   // First, find the first day of the month then find the first day of the week
   // which this day belongs to. Don't use this day as the beginning of the
   // period because dates in databases are in UTC but UI may use a different
-  // time zone. So, take one day earlier as the beginning of the period. UI will
-  // filter out the date which are not in UI time zone.
+  // time zone. So, take one day earlier as the beginning of the period.
+  //
+  // There are 6 weeks (42 days) in UI. So the range should be at least 44 days
+  // because of possible zone differences.
+  //
+  // UI will filter out the date which are not in UI time zone.
   const firstOfMonth = getFirstDayOfMonth(date);
   const firstOfWeek = getFirstDayOfWeek(firstOfMonth);
   const firstDay = dateAfterXDays(firstOfWeek, -1);
-  const lastDay = dateAfterXDays(firstDay, 33);
+  const lastDay = dateAfterXDays(firstDay, 44);
 
   const sql = {
     text: `
