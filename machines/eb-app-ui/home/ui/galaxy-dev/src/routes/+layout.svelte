b2690cff10f11b0d6927d2104d8c61e959c4e133
emrah <emrah.eryilmaz@nordeck.net>
2024-06-22 22:05:19 +0300

    refactor(eb-app-ui): trigger tooltips

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import NavBarPri from "$lib/components/nav/bar-pri.svelte";
   import NavBarPub from "$lib/components/nav/bar-pub.svelte";
 
   const identity_id = window.localStorage.getItem("identity_id");
+
+  onMount(() => {
+    const elementsWithTooltip = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]'),
+    );
+
+    elementsWithTooltip.map(function (el) {
+      return new window.bootstrap.Tooltip(el);
+    });
+  });
 </script>
 
 <!-- -------------------------------------------------------------------------->
