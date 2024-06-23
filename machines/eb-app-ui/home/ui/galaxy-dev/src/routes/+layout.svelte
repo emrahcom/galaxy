95279c1337ae3f9e3d094fe570c0b759a8565e3f
emrah <emrah.eryilmaz@nordeck.net>
2024-06-23 15:02:21 +0300

    fix(eb-app-ui): top space

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import "bootstrap-icons/font/bootstrap-icons.min.css";
   import "bootstrap/dist/css/bootstrap.min.css";
   import "bootstrap/dist/js/bootstrap.bundle.min.js";
+  import Brand from "$lib/components/nav/brand.svelte";
   import NavBarPri from "$lib/components/nav/bar-pri.svelte";
   import NavBarPub from "$lib/components/nav/bar-pub.svelte";
 
@@ -16,11 +17,8 @@
 {/if}
 
 <div class="container-fluid d-flex flex-column h-100">
-  <section id="hidden-top-margin" class="d-flex">
-    <br />
-    <br />
-    <br />
-    <br />
+  <section id="hidden-top-margin" class="d-flex invisible my-4">
+    <Brand />
   </section>
 
   <slot />
