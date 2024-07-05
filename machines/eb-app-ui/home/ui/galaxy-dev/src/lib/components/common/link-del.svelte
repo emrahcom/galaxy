5825616072cb4cd7ac4acbb0c8ec37273071eb61
emrah <emrah.eryilmaz@nordeck.net>
2024-07-05 16:20:09 +0300

    refactor(eb-app-ui): tooltip for del button

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-del.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-del.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
   export let href: string;
+  export let title = "Delete";
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<a {href} class="btn btn-outline-dark btn-sm" role="button">
+<a
+  {href}
+  class="btn btn-outline-dark btn-sm"
+  role="button"
+  data-bs-toggle="tooltip"
+  data-bs-title={title}
+>
   <i class="bi bi-trash" />
 </a>
