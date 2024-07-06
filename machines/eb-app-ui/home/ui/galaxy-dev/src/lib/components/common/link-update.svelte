74d4611e3580da5a0f9aba95dd104c4c9a3d665b
emrah <emrah.eryilmaz@nordeck.net>
2024-07-06 15:29:05 +0300

    refactor(eb-app-ui): the update button based on template

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-update.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-update.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { activateTooltips } from "$lib/common";
+  import LinkButton from "$lib/components/common/link-button-template.svelte";
 
   export let href: string;
   export let title = "Update";
 
-  onMount(() => {
-    activateTooltips();
-  });
+  const icon = "bi-pencil";
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<button
-  class="btn btn-outline-dark btn-sm"
-  data-bs-toggle="tooltip"
-  data-bs-title={title}
-  on:click={() => {
-    window.location.href = href;
-  }}
->
-  <i class="bi bi-pencil" />
-</button>
+<LinkButton {href} {icon} {title} />
