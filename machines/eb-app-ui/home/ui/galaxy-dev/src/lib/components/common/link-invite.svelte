5b32e709c734db3ec9d89c7c5f8b6c517e8ac769
emrah <emrah.eryilmaz@nordeck.net>
2024-07-06 15:29:16 +0300

    refactor(eb-app-ui): the invite button based on template

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-invite.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-invite.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { activateTooltips } from "$lib/common";
+  import LinkButton from "$lib/components/common/link-button-template.svelte";
 
   export let href: string;
   export let title = "Keys";
 
-  onMount(() => {
-    activateTooltips();
-  });
+  const icon = "bi-key";
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<button
-  class="btn btn-outline-dark btn-sm"
-  data-bs-toggle="tooltip"
-  data-bs-title={title}
-  on:click={() => {
-    window.location.href = href;
-  }}
->
-  <i class="bi bi-key" />
-</button>
+<LinkButton {href} {icon} {title} />
