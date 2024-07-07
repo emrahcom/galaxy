da4b0215940a0d16c519fbaa91b316cf9d1d8b62
emrah <emrah.eryilmaz@nordeck.net>
2024-07-07 13:43:29 +0300

    refactor(eb-app-ui): convert the brand to a button

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/nav/brand.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/nav/brand.svelte
@@ -1,3 +1,22 @@
-<a class="navbar-brand py-0" href="/">
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { activateTooltips } from "$lib/common";
+
+  const title = "Go to home page";
+
+  onMount(() => {
+    setTimeout(activateTooltips, 200);
+  });
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<button
+  class="btn navbar-brand py-0"
+  data-bs-toggle="tooltip"
+  data-bs-title={title}
+  on:click={() => {
+    window.location.href = "/";
+  }}
+>
   <img src="/logo.svg" alt="logo" height="50rem" />
-</a>
+</button>
