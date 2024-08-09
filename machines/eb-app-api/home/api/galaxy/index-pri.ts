717e1be9beec277faa9a5ab99db1d7f37391abce
emrah <emrah.eryilmaz@nordeck.net>
2024-08-09 12:52:05 +0300

    refactor(eb-app-api): deno.serve in pri

--- machines/eb-app-api/home/api/galaxy/index-pri.ts
+++ machines/eb-app-api/home/api/galaxy/index-pri.ts
@@ -1,4 +1,3 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import { DB_VERSION, HOSTNAME, PORT_PRIVATE } from "./config.ts";
 import {
   methodNotAllowed,
@@ -122,10 +121,10 @@ async function main() {
     Deno.exit(1);
   }
 
-  serve(handler, {
+  Deno.serve({
     hostname: HOSTNAME,
     port: PORT_PRIVATE,
-  });
+  }, handler);
 }
 
 // -----------------------------------------------------------------------------
