f797c2d59e0cc5644bfa95a9912c51ccb69f831c
emrah <emrah.eryilmaz@nordeck.net>
2024-09-22 14:48:05 +0300

    feat(database): upgrade to 20240922.01

--- machines/eb-app-api/home/api/galaxy/lib/adm/migration.ts
+++ machines/eb-app-api/home/api/galaxy/lib/adm/migration.ts
@@ -1,4 +1,35 @@
 import { getVersion } from "../database/common.ts";
+import { pool } from "../database/common.ts";
+
+// -----------------------------------------------------------------------------
+async function migrateTo2024092201(version: string) {
+  if (version < "20240922.01") {
+    console.log("Upgrade database to 20240922.01");
+
+    using client = await pool.connect();
+    const trans = client.createTransaction("transaction");
+    await trans.begin();
+
+    let sql = {
+      text: `
+        ALTER TABLE identity
+          ADD COLUMN IF NOT EXISTS
+            "seen_at" timestamp with time zone NOT NULL DEFAULT now()`,
+    };
+    await trans.queryObject(sql);
+
+    sql = {
+      text: `
+        UPDATE metadata
+        SET mvalue='20240922.01'
+        WHERE mkey = 'database_version'`,
+    };
+    await trans.queryObject(sql);
+
+    await trans.commit();
+    console.log("Upgraded to database 20240922.01");
+  }
+}
 
 // -----------------------------------------------------------------------------
 export default async function () {
@@ -6,4 +37,6 @@ export default async function () {
 
   const version = await getVersion();
   console.log(`Database version: ${version}`);
+
+  await migrateTo2024092201(version);
 }
