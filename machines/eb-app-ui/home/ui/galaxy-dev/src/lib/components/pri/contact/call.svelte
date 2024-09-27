0f8173268a57552a92842d6fd1e642d0b7963fac
emrah <emrah.eryilmaz@nordeck.net>
2024-09-27 14:55:13 +0300

    refctor(eb-app-ui): add domain into the call form

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/call.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/call.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { FORM_WIDTH } from "$lib/config";
-  import { actionById } from "$lib/api";
-  import type { Contact } from "$lib/types";
+  import { action, list } from "$lib/api";
+  import type { Contact, Domain333 } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
   import Email from "$lib/components/common/form-email.svelte";
+  import Select from "$lib/components/common/form-select.svelte";
   import Submit from "$lib/components/common/button-submit.svelte";
   import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
   import Text from "$lib/components/common/form-text.svelte";
@@ -12,6 +13,16 @@
   export let p: Contact;
 
   let warning = false;
+  let domain_id = "";
+
+  const pr = list("/api/pri/domain/list", 100).then((items: Domain333[]) => {
+    const enableds = items
+      .filter((i) => i.enabled)
+      .sort((i, j) => (i.updated_at > j.updated_at ? -1 : 1));
+    if (enableds[0]) domain_id = enableds[0].id;
+
+    return enableds.map((i) => [i.id, i.name]);
+  });
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -22,7 +33,11 @@
   async function onSubmit() {
     try {
       warning = false;
-      await actionById("/api/pri/contact/call", p.id);
+      const data = {
+        remote_id: p.id,
+        domain_id: domain_id,
+      };
+      await action("/api/pri/contact/call", data);
     } catch {
       warning = true;
     }
@@ -31,39 +46,49 @@
 
 <!-- -------------------------------------------------------------------------->
 <section id="call">
-  <div class="d-flex mt-2 justify-content-center">
-    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
-      <Text
-        name="name"
-        label="Contact"
-        value={p.name}
-        disabled={true}
-        readonly={true}
-      />
-      <Text
-        name="profile_name"
-        label="Name"
-        value={p.profile_name || ""}
-        disabled={true}
-        readonly={true}
-      />
-      <Email
-        name="profile_email"
-        label="Email"
-        value={p.profile_email || ""}
-        disabled={true}
-        readonly={true}
-      />
+  {#await pr then domains}
+    <div class="d-flex mt-2 justify-content-center">
+      <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+        <Text
+          name="name"
+          label="Contact"
+          value={p.name}
+          disabled={true}
+          readonly={true}
+        />
+        <Text
+          name="profile_name"
+          label="Name"
+          value={p.profile_name || ""}
+          disabled={true}
+          readonly={true}
+        />
+        <Email
+          name="profile_email"
+          label="Email"
+          value={p.profile_email || ""}
+          disabled={true}
+          readonly={true}
+        />
+        <Select
+          id="domain_id"
+          label="Jitsi Domain"
+          bind:value={domain_id}
+          options={domains}
+        />
 
-      {#if warning}
-        <Warning>The call request is not accepted.</Warning>
-      {/if}
+        {#if warning}
+          <Warning>The call request is not accepted.</Warning>
+        {/if}
 
-      <div class="d-flex gap-5 mt-5 justify-content-center">
-        <Cancel on:click={cancel} />
-        <SubmitBlocker />
-        <Submit label="Call" />
-      </div>
-    </form>
-  </div>
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <Cancel on:click={cancel} />
+          <SubmitBlocker />
+          <Submit label="Call" />
+        </div>
+      </form>
+    </div>
+  {:catch}
+    <Warning>Something went wrong</Warning>
+  {/await}
 </section>
