d04dd38bc480cd28044e32c8c4256c6d4692030f
emrah <emrah.eryilmaz@nordeck.net>
2024-09-27 13:46:55 +0300

    feat(eb-app-ui): add the call button

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-call.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import LinkButton from "$lib/components/common/template-link.svelte";
+
+  export let href: string;
+  export let title = "Call";
+
+  const icon = "bi-telephone";
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<LinkButton {href} {icon} {title} />
