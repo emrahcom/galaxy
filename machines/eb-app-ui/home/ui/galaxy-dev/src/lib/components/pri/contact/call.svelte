fb2d5765d7b0b341a42d471e5097c3847d20efe9
emrah <emrah.eryilmaz@nordeck.net>
2024-09-27 13:57:39 +0300

    feat(eb-app-ui): add the call form

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/call.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { Contact } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Email from "$lib/components/common/form-email.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let p: Contact;
+
+  let warning = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    window.location.href = "/pri/contact";
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      await actionById("/api/pri/contact/call", p.id);
+    } catch {
+      warning = true;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="call">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="name"
+        label="Contact"
+        value={p.name}
+        disabled={true}
+        readonly={true}
+      />
+      <Text
+        name="profile_name"
+        label="Name"
+        value={p.profile_name || ""}
+        disabled={true}
+        readonly={true}
+      />
+      <Email
+        name="profile_email"
+        label="Email"
+        value={p.profile_email || ""}
+        disabled={true}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The call request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Call" />
+      </div>
+    </form>
+  </div>
+</section>
---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/contact/call/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Call from "$lib/components/pri/contact/call.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/contact/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Call this contact" />
+
+{#await pr then contact}
+  <Call p={contact} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
