ac16e765d282c38f6ce65a5883a047c4ff9b2068
emrah <emrah.eryilmaz@nordeck.net>
2024-10-02 21:46:34 +0300

    refactor(eb-app-api): improve migration.ts

--- machines/eb-app-api/home/api/galaxy/lib/adm/migration.ts
+++ machines/eb-app-api/home/api/galaxy/lib/adm/migration.ts
@@ -4,7 +4,7 @@ import type { QueryObject } from "../database/common.ts";
 // -----------------------------------------------------------------------------
 // Migrate template.
 // -----------------------------------------------------------------------------
-async function migrateTo(upgradeTo: string, sqls: QueryObject[]) {
+async function migrateTo(upgradeTo: string, sqls: (string | QueryObject)[]) {
   const version = await getVersion();
 
   if (version < upgradeTo) {
@@ -15,7 +15,11 @@ async function migrateTo(upgradeTo: string, sqls: QueryObject[]) {
     await trans.begin();
 
     for (const sql of sqls) {
-      await trans.queryObject(sql);
+      if (typeof sql === "string") {
+        await trans.queryObject(sql as string);
+      } else {
+        await trans.queryObject(sql as QueryObject);
+      }
     }
 
     await trans.commit();
@@ -27,18 +31,13 @@ async function migrateTo(upgradeTo: string, sqls: QueryObject[]) {
 async function migrateTo2024092201() {
   const upgradeTo = "20240922.01";
   const sqls = [
-    {
-      text: `
-        ALTER TABLE identity
-          ADD COLUMN IF NOT EXISTS
-            "seen_at" timestamp with time zone NOT NULL DEFAULT now()`,
-    },
-    {
-      text: `
-        UPDATE metadata
-        SET mvalue='${upgradeTo}'
-        WHERE mkey = 'database_version'`,
-    },
+    `ALTER TABLE identity
+       ADD COLUMN IF NOT EXISTS
+         "seen_at" timestamp with time zone NOT NULL DEFAULT now()`,
+
+    `UPDATE metadata
+       SET mvalue='${upgradeTo}'
+       WHERE mkey = 'database_version'`,
   ];
 
   await migrateTo(upgradeTo, sqls);
@@ -48,55 +47,42 @@ async function migrateTo2024092201() {
 async function migrateTo2024092801() {
   const upgradeTo = "20240928.01";
   const sqls = [
-    {
-      text: `
-        CREATE TYPE intercom_status_type AS ENUM (
-          'none',
-          'seen',
-          'accepted',
-          'rejected'
-        )`,
-    },
-    {
-      text: `
-        CREATE TYPE intercom_message_type AS ENUM (
-          'call',
-          'alarm_for_meeting',
-          'invite_for_domain',
-          'invite_for_room',
-          'invite_for_meeting',
-          'request_for_meeting_membership'
-        )`,
-    },
-    {
-      text: `
-        CREATE TABLE intercom (
-          "id" uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-          "identity_id" uuid NOT NULL REFERENCES identity(id) ON DELETE CASCADE,
-          "remote_id" uuid NOT NULL REFERENCES identity(id) ON DELETE CASCADE,
-          "status" intercom_status_type NOT NULL DEFAULT 'none',
-          "message_type" intercom_message_type NOT NULL DEFAULT 'call',
-          "intercom_attr" jsonb NOT NULL DEFAULT '{}'::jsonb,
-          "created_at" timestamp with time zone NOT NULL DEFAULT now(),
-          "updated_at" timestamp with time zone NOT NULL DEFAULT now(),
-          "expired_at" timestamp with time zone NOT NULL
-              DEFAULT now() + interval '10 seconds'
-        )`,
-    },
-    {
-      text: `
-        CREATE INDEX ON intercom("remote_id", "expired_at")`,
-    },
-    {
-      text: `
-        CREATE INDEX ON intercom("expired_at")`,
-    },
-    {
-      text: `
-        UPDATE metadata
-        SET mvalue='${upgradeTo}'
-        WHERE mkey = 'database_version'`,
-    },
+    `CREATE TYPE intercom_status_type AS ENUM (
+       'none',
+       'seen',
+       'accepted',
+       'rejected'
+     )`,
+
+    `CREATE TYPE intercom_message_type AS ENUM (
+       'call',
+       'alarm_for_meeting',
+       'invite_for_domain',
+       'invite_for_room',
+       'invite_for_meeting',
+       'request_for_meeting_membership'
+     )`,
+
+    `CREATE TABLE intercom (
+       "id" uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+       "identity_id" uuid NOT NULL REFERENCES identity(id) ON DELETE CASCADE,
+       "remote_id" uuid NOT NULL REFERENCES identity(id) ON DELETE CASCADE,
+       "status" intercom_status_type NOT NULL DEFAULT 'none',
+       "message_type" intercom_message_type NOT NULL DEFAULT 'call',
+       "intercom_attr" jsonb NOT NULL DEFAULT '{}'::jsonb,
+       "created_at" timestamp with time zone NOT NULL DEFAULT now(),
+       "updated_at" timestamp with time zone NOT NULL DEFAULT now(),
+       "expired_at" timestamp with time zone NOT NULL
+           DEFAULT now() + interval '10 seconds'
+     )`,
+
+    `CREATE INDEX ON intercom("remote_id", "expired_at")`,
+
+    `CREATE INDEX ON intercom("expired_at")`,
+
+    `UPDATE metadata
+     SET mvalue='${upgradeTo}'
+     WHERE mkey = 'database_version'`,
   ];
 
   await migrateTo(upgradeTo, sqls);
