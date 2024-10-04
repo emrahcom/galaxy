1b90d49142a89bce1588015a429326c7a1497e71
emrah <emrah.eryilmaz@nordeck.net>
2024-10-04 13:22:18 +0300

    feat(eb-app-ui): add disabled as attribute to the cancel button

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-cancel.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-cancel.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   export let label = "Cancel";
+  export let disabled = false;
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<button class="btn btn-outline-secondary" type="button" on:click>
+<button class="btn btn-outline-secondary" type="button" {disabled} on:click>
   {label}
 </button>
