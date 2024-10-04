e7db6e8e255953eb46cf568ec43900b79c21f8ce
emrah <emrah.eryilmaz@nordeck.net>
2024-10-04 12:44:05 +0300

    feat(eb-app-ui): add disabled as attribute to the submit button

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-submit.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-submit.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   export let label = "Submit";
+  export let disabled = false;
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<button class="btn btn-outline-secondary" type="submit">{label}</button>
+<button class="btn btn-outline-secondary" type="submit" {disabled}>
+  {label}
+</button>
