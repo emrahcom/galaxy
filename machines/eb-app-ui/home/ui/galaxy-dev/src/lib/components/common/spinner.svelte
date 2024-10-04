10d6c68e6037be097962095706edffc3abc8bcb4
emrah <emrah.eryilmaz@nordeck.net>
2024-10-04 15:37:05 +0300

    feat(eb-app-ui): add effect to spinner

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/spinner.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/spinner.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+  export let effect = "border";
+</script>
+
+<!-- -------------------------------------------------------------------------->
 <div class="d-flex justify-content-center my-5">
-  <div class="spinner-border text-secondary" role="status">
+  <div class="spinner-{effect} text-secondary" role="status">
     <span class="visually-hidden">
       <slot />
     </span>
