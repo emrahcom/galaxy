5bd2f2a4efeb9711f200b14baf63fd1264bf1fa5
emrah <emrah.eryilmaz@nordeck.net>
2024-10-04 14:46:38 +0300

    fix(eb-app-ui): disable buttons in domain-partner/enable while fetching

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partner/enable.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partner/enable.svelte
@@ -11,6 +11,7 @@
   export let p: DomainPartner;
 
   let warning = false;
+  let disabled = false;
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -21,10 +22,13 @@
   async function onSubmit() {
     try {
       warning = false;
+      disabled = true;
+
       await actionById("/api/pri/domain/partner/enable", p.id);
       window.location.href = `/pri/domain/partner/${p.domain_id}`;
     } catch {
       warning = true;
+      disabled = false;
     }
   }
 </script>
@@ -53,9 +57,9 @@
       {/if}
 
       <div class="d-flex gap-5 mt-5 justify-content-center">
-        <Cancel on:click={cancel} />
+        <Cancel bind:disabled on:click={cancel} />
         <SubmitBlocker />
-        <Submit label="Enable" />
+        <Submit label="Enable" bind:disabled />
       </div>
     </form>
   </div>
