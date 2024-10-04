30133f3f71f9f1b853046fadd8b14631d045f6b8
emrah <emrah.eryilmaz@nordeck.net>
2024-10-04 14:48:44 +0300

    fix(eb-app-ui): disable buttons in domain-partnerhip/del while fetching

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partnership/del.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partnership/del.svelte
@@ -11,6 +11,7 @@
   export let p: DomainPartnership;
 
   let warning = false;
+  let disabled = false;
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -21,10 +22,13 @@
   async function onSubmit() {
     try {
       warning = false;
+      disabled = true;
+
       await actionById("/api/pri/domain/partnership/del", p.id);
       window.location.replace(`/pri/domain`);
     } catch {
       warning = true;
+      disabled = false;
     }
   }
 </script>
@@ -57,9 +61,9 @@
       {/if}
 
       <div class="d-flex gap-5 mt-5 justify-content-center">
-        <Cancel on:click={cancel} />
+        <Cancel bind:disabled on:click={cancel} />
         <SubmitBlocker />
-        <Submit label="Delete" />
+        <Submit label="Delete" bind:disabled />
       </div>
     </form>
   </div>
