858230efd2496d00878afd35e97083e054e345fe
emrah <emrah.eryilmaz@nordeck.net>
2024-10-04 14:43:07 +0300

    fix(eb-app-ui): disable buttons in room-partner-candidacy/accept while fetching

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-partner-candidacy/accept.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-partner-candidacy/accept.svelte
@@ -11,6 +11,7 @@
   export let p: RoomPartnerCandidacy;
 
   let warning = false;
+  let disabled = false;
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -21,10 +22,13 @@
   async function onSubmit() {
     try {
       warning = false;
+      disabled = true;
+
       await actionById("/api/pri/room/partner/candidacy/accept", p.id);
       window.location.href = "/pri/room";
     } catch {
       warning = true;
+      disabled = false;
     }
   }
 </script>
@@ -60,9 +64,9 @@
       {/if}
 
       <div class="d-flex gap-5 mt-5 justify-content-center">
-        <Cancel on:click={cancel} />
+        <Cancel bind:disabled on:click={cancel} />
         <SubmitBlocker />
-        <Submit label="Accept" />
+        <Submit label="Accept" bind:disabled />
       </div>
     </form>
   </div>
