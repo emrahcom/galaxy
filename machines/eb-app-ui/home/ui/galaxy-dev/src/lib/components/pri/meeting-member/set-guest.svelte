6d74e0fc350b39510dd063e457bf861b1fd653f9
emrah <emrah.eryilmaz@nordeck.net>
2024-10-04 14:51:40 +0300

    fix(eb-app-ui): disable buttons in meeting-member/set-guest while fetching

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member/set-guest.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-member/set-guest.svelte
@@ -11,6 +11,7 @@
   export let p: MeetingMember;
 
   let warning = false;
+  let disabled = false;
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -21,10 +22,13 @@
   async function onSubmit() {
     try {
       warning = false;
+      disabled = true;
+
       await actionById("/api/pri/meeting/member/set/guest", p.id);
       window.location.href = `/pri/meeting/member/${p.meeting_id}`;
     } catch {
       warning = true;
+      disabled = false;
     }
   }
 </script>
@@ -60,9 +64,9 @@
       {/if}
 
       <div class="d-flex gap-5 mt-5 justify-content-center">
-        <Cancel on:click={cancel} />
+        <Cancel bind:disabled on:click={cancel} />
         <SubmitBlocker />
-        <Submit label="Set as Guest" />
+        <Submit label="Set as Guest" bind:disabled />
       </div>
     </form>
   </div>
