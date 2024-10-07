2ef6ca17d8f0455c1a3f5b48f260917717bcaf0e
emrah <emrah.eryilmaz@nordeck.net>
2024-10-07 19:23:22 +0300

    feat(eb-app-ui): initialize call accept/reject buttons

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/intercom.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/intercom.ts
@@ -29,7 +29,15 @@ function createCallToast(msg: IntercomMessage) {
           onclick="document.getElementById('msg-${msg.id}')?.remove()"
         ></button>
       </div>
-      <div>
+      <div class="d-flex justify-content-center">
+        <button
+          class="btn btn-sm m-2 mb-0 btn-danger"
+          type="button"
+        >Reject</button>
+        <button
+          class="btn btn-sm m-2 mb-0 btn-success"
+          type="button"
+        >Accept</button>
         <audio id="ring-${msg.id}" src="/ringing.mp3" loop></audio>
       </div>
     </div>
