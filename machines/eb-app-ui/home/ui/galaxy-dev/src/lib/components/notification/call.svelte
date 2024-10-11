58590283255a5b1b1001afc7d1a1a3263997f248
emrah <emrah.eryilmaz@nordeck.net>
2024-10-11 21:48:30 +0300

    feat(eb-app-ui): initialize Call component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/notification/call.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { IntercomMessage } from "$lib/types";
+
+  export let notification: IntercomMessage;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<p>{notification.id}</p>
--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/notification/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/notification/list.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import type { IntercomMessage } from "$lib/types";
+  import Call from "$lib/components/notification/call.svelte";
 
   export let notifications: IntercomMessage[];
-
-  console.error(notifications);
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<div id="notifications" class="toast-container bottom-0 end-0 p-3"></div>
+<div id="notifications" class="toast-container bottom-0 end-0 p-3">
+  {#each notifications as notification}
+    {#if notification.message_type === "call"}
+      <Call bind:notification />
+    {/if}
+  {/each}
+</div>
