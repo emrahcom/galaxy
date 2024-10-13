263a107e236e80bf25cd29bfcb88aa067521ea64
emrah <emrah.eryilmaz@nordeck.net>
2024-10-13 18:50:44 +0300

    feat(eb-app-ui): add join into incoming call page

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/call/join/[uuid]/+page.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/call/join/[uuid]/+page.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import { actionById } from "$lib/api";
+  import type { IntercomMessage } from "$lib/types";
+  import Spinner from "$lib/components/common/spinner.svelte";
   import Subheader from "$lib/components/common/subheader-center.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
-  const msgId = $page.params.uuid;
-  const pr = actionById("/api/pri/intercom/set/accepted", msgId);
+  // ---------------------------------------------------------------------------
+  async function join(msgId: string) {
+    const json = globalThis.localStorage.getItem(`msg-${msgId}`);
+    if (!json) throw "storage item is not found";
+
+    const msg = JSON.parse(json) as IntercomMessage;
+    const url = msg.intercom_attr.url;
+
+    await actionById("/api/pri/intercom/set/accepted", msgId);
+    globalThis.localStorage.removeItem(`msg-${msgId}`);
+
+    globalThis.location.replace(url);
+  }
+
+  const pr = join($page.params.uuid);
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<Subheader subheader="Joining the call" />
+<Subheader subheader="Join the call" />
 
 {#await pr}
-  redirecting...
+  <Spinner>joining...</Spinner>
+{:then}
+  <Spinner>joining...</Spinner>
 {:catch}
   <Warning>Something went wrong</Warning>
 {/await}
