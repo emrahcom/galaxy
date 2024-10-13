928f7f241d4a5cb3711f9298e238baf5e1670b80
emrah <emrah.eryilmaz@nordeck.net>
2024-10-13 14:51:08 +0300

    refactor(eb-app-ui): initialize aceept and recject call functions

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/message/call.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/message/call.svelte
@@ -10,16 +10,6 @@
   watchCall(msg.id);
 
   // ---------------------------------------------------------------------------
-  function close() {
-    try {
-      // set message status to seen
-      actionById("/api/pri/intercom/set/seen", msg.id);
-    } catch {
-      // do nothing
-    }
-  }
-
-  // ---------------------------------------------------------------------------
   onMount(() => {
     try {
       const toast = document.getElementById(`msg-${msg.id}`);
@@ -33,6 +23,32 @@
       // do nothing
     }
   });
+
+  // ---------------------------------------------------------------------------
+  function close() {
+    try {
+      // set message status to seen
+      actionById("/api/pri/intercom/set/seen", msg.id);
+
+      // stop ringing without waiting for the status update
+      const ring = document.getElementById(
+        `ring-${msg.id}`,
+      ) as HTMLAudioElement;
+      if (ring) ring.pause();
+    } catch {
+      // do nothing
+    }
+  }
+
+  // ---------------------------------------------------------------------------
+  function accept() {
+    console.error("accepted");
+  }
+
+  // ---------------------------------------------------------------------------
+  function reject() {
+    console.error("rejected");
+  }
 </script>
 
 <!-- -------------------------------------------------------------------------->
@@ -58,12 +74,20 @@
       ></button>
     </div>
     <div class="d-flex justify-content-center">
-      <button class="btn btn-sm m-2 mb-0 btn-danger" type="button"
-        >Reject</button
+      <button
+        class="btn btn-sm m-2 mb-0 btn-danger"
+        type="button"
+        on:click={reject}
       >
-      <button class="btn btn-sm m-2 mb-0 btn-success" type="button"
-        >Accept</button
+        Reject
+      </button>
+      <button
+        class="btn btn-sm m-2 mb-0 btn-success"
+        type="button"
+        on:click={accept}
       >
+        Accept
+      </button>
       <audio id="ring-{msg.id}" src="/ringing.mp3" loop></audio>
     </div>
   </div>
