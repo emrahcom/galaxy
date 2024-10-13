730688e1f0626e226c9e6d06531645821c4d13e1
emrah <emrah.eryilmaz@nordeck.net>
2024-10-13 18:23:01 +0300

    feat(eb-app-ui): add router for call/join

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/call/join/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { actionById } from "$lib/api";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const msgId = $page.params.uuid;
+  const pr = actionById("/api/pri/intercom/set/accepted", msgId);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Joining the call" />
+
+{#await pr}
+  redirecting...
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
