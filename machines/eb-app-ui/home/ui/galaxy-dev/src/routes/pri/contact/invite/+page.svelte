bc98140d1ccc7209a8c88dbf3297c3aedd4a554b
emrah <emrah.eryilmaz@nordeck.net>
2024-10-17 21:23:52 +0300

    feat(eb-app-ui): add the router for contact/invite

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/contact/invite/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { list } from "$lib/api";
+  import List from "$lib/components/pri/contact-invite/list.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = list("/api/pri/contact/invite/list", 100);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader
+  subheader="Friendship keys"
+  hrefContact="/pri/contact"
+  hrefContactTitle="Show contacts"
+/>
+
+{#await pr then invites}
+  <List {invites} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
