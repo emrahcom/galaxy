f8d5feef807d65181b725d594f8ea15d82dd04f8
emrah <emrah.eryilmaz@nordeck.net>
2024-10-18 14:41:17 +0300

    feat(eb-app-ui): add the contact adding page

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-friendship/add.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { action } from "$lib/api";
+  import type { ContactInvite111 } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  export let invite: ContactInvite111;
+  export let isExist: boolean;
+
+  let warning = false;
+  let disabled = false;
+  let p = {
+    code: invite.code,
+  };
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    globalThis.location.href = `/pri/contact`;
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      disabled = true;
+
+      await action("/api/pri/contact/friendship/add/bycode", p);
+      globalThis.location.href = `/pri/contact`;
+    } catch {
+      warning = true;
+      disabled = false;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="add">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="name"
+        label="Name"
+        value={invite.profile_name}
+        disabled={true}
+        readonly={true}
+      />
+      <Text
+        name="email"
+        label="Email"
+        value={invite.profile_email}
+        disabled={true}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The add request is not accepted.</Warning>
+      {/if}
+
+      {#if isExist}
+        <Warning>
+          This contact is already in your list.<br />
+          Nothing to do.
+        </Warning>
+
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <Cancel label="Abort" on:click={cancel} />
+        </div>
+      {:else}
+        <div class="d-flex gap-5 mt-5 justify-content-center">
+          <Cancel bind:disabled on:click={cancel} />
+          <SubmitBlocker />
+          <Submit label="Add" bind:disabled />
+        </div>
+      {/if}
+    </form>
+  </div>
+</section>
