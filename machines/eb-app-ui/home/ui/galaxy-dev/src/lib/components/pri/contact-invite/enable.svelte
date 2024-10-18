e93035219a28483d7bb0b38574d80fc4f550c66a
emrah <emrah.eryilmaz@nordeck.net>
2024-10-18 13:15:14 +0300

    feat(eb-app-ui): add the enable page for contact-invite

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-invite/enable.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Switch from "$lib/components/common/form-switch.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  let warning = false;
+  let disabled = false;
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    globalThis.location.href = "/pri/contact/invite";
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onSubmit() {
+    try {
+      warning = false;
+      disabled = true;
+
+      await actionById("/api/pri/contact/invite/enable", p.id);
+      globalThis.location.replace("/pri/contact/invite");
+    } catch {
+      warning = true;
+      disabled = false;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="enable">
+  <div class="d-flex mt-2 justify-content-center">
+    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="name"
+        label="Name"
+        value={p.name}
+        disabled={true}
+        readonly={true}
+      />
+
+      <p class="text-muted me-3 mt-3 mb-1" />
+      <Switch
+        name="disposable"
+        label="Disposable"
+        desc="(can only be used once)"
+        bind:value={p.disposable}
+        disabled={true}
+      />
+
+      {#if warning}
+        <Warning>The enable request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel bind:disabled on:click={cancel} />
+        <SubmitBlocker />
+        <Submit label="Enable" bind:disabled />
+      </div>
+    </form>
+  </div>
+</section>
