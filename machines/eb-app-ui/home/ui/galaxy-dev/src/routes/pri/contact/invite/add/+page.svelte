4f5ba03960324cb45e7dae00345c77f026e08f60
emrah <emrah.eryilmaz@nordeck.net>
2024-10-18 12:47:28 +0300

    feat(eb-app-ui): add the contact-invite router

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/contact/invite/add/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Add from "$lib/components/pri/contact-invite/add.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Create a contact key" />
+<Add />
