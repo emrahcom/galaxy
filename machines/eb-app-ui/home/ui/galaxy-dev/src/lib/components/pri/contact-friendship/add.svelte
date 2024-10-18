5978af7c8b716c6f64ee0cad200651bca05bf05e
emrah <emrah.eryilmaz@nordeck.net>
2024-10-18 16:16:33 +0300

    fix(eb-app-ui): fields in contact-friendship

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-friendship/add.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-friendship/add.svelte
@@ -3,6 +3,7 @@
   import { action } from "$lib/api";
   import type { ContactInvite111 } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Email from "$lib/components/common/form-email.svelte";
   import Submit from "$lib/components/common/button-submit.svelte";
   import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
   import Text from "$lib/components/common/form-text.svelte";
@@ -15,6 +16,7 @@
   let disabled = false;
   let p = {
     code: invite.code,
+    name: invite.profile_name,
   };
 
   // ---------------------------------------------------------------------------
@@ -41,17 +43,18 @@
 <section id="add">
   <div class="d-flex mt-2 justify-content-center">
     <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+      <Text name="name" label="Contact" bind:value={p.name} required={true} />
       <Text
-        name="name"
+        name="profile_name"
         label="Name"
-        value={invite.profile_name}
+        value={invite.profile_name || ""}
         disabled={true}
         readonly={true}
       />
-      <Text
-        name="email"
+      <Email
+        name="profile_email"
         label="Email"
-        value={invite.profile_email}
+        value={invite.profile_email || ""}
         disabled={true}
         readonly={true}
       />
