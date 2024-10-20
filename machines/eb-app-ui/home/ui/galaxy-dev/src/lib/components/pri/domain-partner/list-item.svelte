a0092c945bf02aee48d98e643d340ec05ea1c1e9
emrah <emrah.eryilmaz@nordeck.net>
2024-10-20 18:13:21 +0300

    refactor(eb-app-ui): use a seperate component for the items of domain-partner

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partner/list-item.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { DomainPartner } from "$lib/types";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Disable from "$lib/components/common/link-disable.svelte";
+  import Enable from "$lib/components/common/link-enable.svelte";
+
+  export let p: DomainPartner;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="col-md-6 col-xl-4">
+  <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
+    <div class="card-body text-center">
+      <h5 class="card-title text-muted">
+        {p.contact_name || p.profile_name || ""}
+      </h5>
+
+      <p class="card-text text-muted">
+        {p.profile_email || ""}
+      </p>
+    </div>
+
+    <div class="card-footer bg-body border-0 text-center">
+      <Del href="/pri/domain/partner/del/{p.id}" />
+
+      {#if p.enabled}
+        <Disable href="/pri/domain/partner/disable/{p.id}" />
+      {:else}
+        <Enable href="/pri/domain/partner/enable/{p.id}" />
+      {/if}
+    </div>
+  </div>
+</div>
