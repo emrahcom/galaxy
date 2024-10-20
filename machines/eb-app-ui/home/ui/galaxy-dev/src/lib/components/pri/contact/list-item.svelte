b90c34d50d1021cea0c2f747f7dc86e1cb701d45
emrah <emrah.eryilmaz@nordeck.net>
2024-10-20 15:14:00 +0300

    refactor(eb-app-ui): use a seperate component for the items of contact

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/list-item.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import type { Contact } from "$lib/types";
+  import Call from "$lib/components/common/link-call.svelte";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Update from "$lib/components/common/link-update.svelte";
+
+  export let p: Contact;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="col-md-6 col-xl-4">
+  <div class="card h-100">
+    <div class="card-body text-center">
+      <h5 class="card-title text-muted">{p.name}</h5>
+      <p class="card-text text-muted"></p>
+      <p class="card-text text-muted">
+        {p.profile_name || ""}<br />
+        {p.profile_email || ""}
+      </p>
+    </div>
+
+    <div class="card-footer bg-body border-0 text-center">
+      <Del href="/pri/contact/del/{p.id}" />
+      <Update href="/pri/contact/update/{p.id}" />
+      <Call href="/pri/contact/call/{p.id}" />
+    </div>
+  </div>
+</div>
--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/list.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import type { Contact } from "$lib/types";
-  import Call from "$lib/components/common/link-call.svelte";
-  import Del from "$lib/components/common/link-del.svelte";
-  import Update from "$lib/components/common/link-update.svelte";
+  import Item from "$lib/components/pri/contact/list-item.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   export let contacts: Contact[];
@@ -12,24 +10,7 @@
 <section id="list">
   <div class="row mx-auto mt-2 g-3">
     {#each contacts as p}
-      <div class="col-md-6 col-xl-4">
-        <div class="card h-100">
-          <div class="card-body text-center">
-            <h5 class="card-title text-muted">{p.name}</h5>
-            <p class="card-text text-muted"></p>
-            <p class="card-text text-muted">
-              {p.profile_name || ""}<br />
-              {p.profile_email || ""}
-            </p>
-          </div>
-
-          <div class="card-footer bg-body border-0 text-center">
-            <Del href="/pri/contact/del/{p.id}" />
-            <Update href="/pri/contact/update/{p.id}" />
-            <Call href="/pri/contact/call/{p.id}" />
-          </div>
-        </div>
-      </div>
+      <Item {p} />
     {:else}
       <Warning>There is no contact in the list.</Warning>
     {/each}
