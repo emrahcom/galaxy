a2fd23865ac7279945adad69ff51bd079a182c1e
emrah <emrah.eryilmaz@nordeck.net>
2024-10-20 15:43:54 +0300

    refactor(eb-app-ui): use a seperate component for the items of contact-invite

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-invite/list-item.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { showLocaleDatetime } from "$lib/common";
+  import type { ContactInvite } from "$lib/types";
+  import Copy from "$lib/components/common/button-copy.svelte";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Disable from "$lib/components/common/link-disable.svelte";
+  import Enable from "$lib/components/common/link-enable.svelte";
+
+  export let p: ContactInvite;
+
+  // ---------------------------------------------------------------------------
+  function copy(code: string) {
+    const text = `${$page.url.origin}/pri/contact/friendship/add/${code}`;
+
+    navigator.clipboard.writeText(text);
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="col-md-6 col-xl-4">
+  <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
+    <div class="card-body text-center">
+      <h5 class="card-title text-muted">{p.name}</h5>
+
+      <p class="card-text text-muted small">
+        {showLocaleDatetime(p.expired_at)}
+      </p>
+
+      <p class="card-text text-muted">
+        {$page.url.origin}/pri/contact/friendship/add/{p.code}
+      </p>
+
+      {#if p.enabled}
+        <Copy label="copy" on:click={() => copy(p.code)} />
+      {/if}
+    </div>
+
+    <div class="card-footer bg-body border-0 text-center">
+      <Del href="/pri/contact/invite/del/{p.id}" />
+
+      {#if p.enabled}
+        <Disable href="/pri/contact/invite/disable/{p.id}" />
+      {:else}
+        <Enable href="/pri/contact/invite/enable/{p.id}" />
+      {/if}
+    </div>
+  </div>
+</div>
--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-invite/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-invite/list.svelte
@@ -1,57 +1,17 @@
 <script lang="ts">
-  import { page } from "$app/stores";
-  import { showLocaleDatetime } from "$lib/common";
   import type { ContactInvite } from "$lib/types";
   import Add from "$lib/components/common/link-add.svelte";
-  import Copy from "$lib/components/common/button-copy.svelte";
-  import Del from "$lib/components/common/link-del.svelte";
-  import Disable from "$lib/components/common/link-disable.svelte";
-  import Enable from "$lib/components/common/link-enable.svelte";
+  import Item from "$lib/components/pri/contact-invite/list-item.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   export let invites: ContactInvite[];
-
-  // ---------------------------------------------------------------------------
-  function copy(code: string) {
-    const text = `${$page.url.origin}/pri/contact/friendship/add/${code}`;
-
-    navigator.clipboard.writeText(text);
-  }
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <section id="list">
   <div class="row mx-auto mt-2 g-3">
     {#each invites as p}
-      <div class="col-md-6 col-xl-4">
-        <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
-          <div class="card-body text-center">
-            <h5 class="card-title text-muted">{p.name}</h5>
-
-            <p class="card-text text-muted small">
-              {showLocaleDatetime(p.expired_at)}
-            </p>
-
-            <p class="card-text text-muted">
-              {$page.url.origin}/pri/contact/friendship/add/{p.code}
-            </p>
-
-            {#if p.enabled}
-              <Copy label="copy" on:click={() => copy(p.code)} />
-            {/if}
-          </div>
-
-          <div class="card-footer bg-body border-0 text-center">
-            <Del href="/pri/contact/invite/del/{p.id}" />
-
-            {#if p.enabled}
-              <Disable href="/pri/contact/invite/disable/{p.id}" />
-            {:else}
-              <Enable href="/pri/contact/invite/enable/{p.id}" />
-            {/if}
-          </div>
-        </div>
-      </div>
+      <Item {p} />
     {:else}
       <Warning>
         You have not a contact key. Click
