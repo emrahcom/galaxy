f2f5eef8218940dbdfe593dbb5f463a8c14a5004
emrah <emrah.eryilmaz@nordeck.net>
2024-10-20 16:29:30 +0300

    refactor(eb-app-ui): use a seperate component for the items of profile

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/list-item.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Profile } from "$lib/types";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Set from "$lib/components/common/link-set-default-profile.svelte";
+  import Update from "$lib/components/common/link-update.svelte";
+
+  export let p: Profile;
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="col-md-6 col-xl-4">
+  <div class="card h-100 {p.is_default ? 'border-primary' : ''}">
+    <div class="card-body text-center">
+      <h5 class="card-title text-muted">{p.name}</h5>
+      <p class="card-text text-muted">{p.email}</p>
+    </div>
+
+    <div class="card-footer bg-body border-0 text-center">
+      {#if !p.is_default}
+        <Del href="/pri/profile/del/{p.id}" />
+        <Set href="/pri/profile/set/default/{p.id}" />
+      {/if}
+
+      <Update href="/pri/profile/update/{p.id}" />
+    </div>
+  </div>
+</div>
--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/list.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import type { Profile } from "$lib/types";
   import Add from "$lib/components/common/link-add.svelte";
-  import Del from "$lib/components/common/link-del.svelte";
-  import Set from "$lib/components/common/link-set-default-profile.svelte";
-  import Update from "$lib/components/common/link-update.svelte";
+  import Item from "$lib/components/pri/profile/list-item.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   export let profiles: Profile[];
@@ -13,23 +11,7 @@
 <section id="list">
   <div class="row mx-auto mt-2 g-3">
     {#each profiles as p}
-      <div class="col-md-6 col-xl-4">
-        <div class="card h-100 {p.is_default ? 'border-primary' : ''}">
-          <div class="card-body text-center">
-            <h5 class="card-title text-muted">{p.name}</h5>
-            <p class="card-text text-muted">{p.email}</p>
-          </div>
-
-          <div class="card-footer bg-body border-0 text-center">
-            {#if !p.is_default}
-              <Del href="/pri/profile/del/{p.id}" />
-              <Set href="/pri/profile/set/default/{p.id}" />
-            {/if}
-
-            <Update href="/pri/profile/update/{p.id}" />
-          </div>
-        </div>
-      </div>
+      <Item {p} />
     {:else}
       <Warning>
         There is no profile in the list. Click <Add href="/pri/profile/add" />
