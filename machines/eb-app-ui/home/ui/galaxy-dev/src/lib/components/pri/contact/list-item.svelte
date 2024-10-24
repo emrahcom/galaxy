6331e1f203c7457ba8c800712d723e67a7fd406c
emrah <emrah.eryilmaz@nordeck.net>
2024-10-24 19:50:05 +0300

    feat(eb-app-ui): add the status marker of the contact

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/list-item.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact/list-item.svelte
@@ -14,8 +14,13 @@
 
       const contactStatusList = JSON.parse(statusData) as ContactStatus[];
       const contactStatus = contactStatusList.find((item) => item.id === p.id);
-      if (contactStatus) status = contactStatus.seen_second_ago;
-      console.error(status);
+      if (!contactStatus) return;
+
+      const seen = contactStatus.seen_second_ago;
+      if (!seen) return;
+
+      if (seen < 100) status = 1;
+      else if (seen < 3600) status = 2;
     } finally {
       setTimeout(updateStatus, 20000);
     }
@@ -30,6 +35,15 @@
 <div class="col-md-6 col-xl-4">
   <div class="card h-100">
     <div class="card-body text-center">
+      <div class="position-absolute top-0 start-0">
+        {#if status === 1}
+          <i class="bi bi-circle-fill text-success ms-1"></i>
+        {:else if status === 2}
+          <i class="bi bi-circle-fill text-warning ms-1"></i>
+        {:else}
+          <i class="bi bi-circle-fill text-secondary ms-1"></i>
+        {/if}
+      </div>
       <h5 class="card-title text-muted">{p.name}</h5>
       <p class="card-text text-muted"></p>
       <p class="card-text text-muted">
