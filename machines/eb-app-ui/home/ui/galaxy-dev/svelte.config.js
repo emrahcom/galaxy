d569656929f38d01d94c9e4a27c5474fbb946727
emrah <emrah.eryilmaz@nordeck.net>
2024-10-25 16:20:50 +0300

    feat(eb-app-ui): update config files for Svelte 5

--- machines/eb-app-ui/home/ui/galaxy-dev/svelte.config.js
+++ machines/eb-app-ui/home/ui/galaxy-dev/svelte.config.js
@@ -4,7 +4,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
 
