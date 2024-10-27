ee8dd54f5c2fe7591cabbb9e9406529b39c4af30
emrah <emrah.eryilmaz@nordeck.net>
2024-10-27 18:24:12 +0300

    fix(eb-app-ui): remove spaces

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/nav/button-account.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/nav/button-account.svelte
@@ -60,7 +60,7 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/logout" onclick={closeMenu}> Log Out </a>
+        <a class="nav-link" href="/logout" onclick={closeMenu}>Log Out</a>
       </li>
     </ul>
   </div>
