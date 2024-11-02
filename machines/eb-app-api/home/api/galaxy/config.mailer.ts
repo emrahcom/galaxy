079572d43aef7945d4b01934aaff5732baa35f5a
emrah <emrah.eryilmaz@nordeck.net>
2024-11-02 20:48:29 +0300

    feat(eb-ap-api): mailer config

---
+++ machines/eb-app-api/home/api/galaxy/config.mailer.ts
@@ -0,0 +1,11 @@
+export const transportOptions = {
+  host: "___MAILER_HOST___",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "___MAILER_USER___",
+    pass: "___MAILER_PASS___",
+  },
+};
+
+export const from = "___MAILER_FROM___";
