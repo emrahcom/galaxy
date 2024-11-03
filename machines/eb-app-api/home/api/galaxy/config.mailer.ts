edd6d1fd6f6bdfc5157e185c6ee7dc6c24bca44a
emrah <emrah.eryilmaz@nordeck.net>
2024-11-03 14:19:57 +0300

    refactor(eb-app-api): comments in config.mailer.ts

--- machines/eb-app-api/home/api/galaxy/config.mailer.ts
+++ machines/eb-app-api/home/api/galaxy/config.mailer.ts
@@ -1,3 +1,12 @@
+// -----------------------------------------------------------------------------
+// Mailer config is not part of the main config.ts to make the system more
+// customizable. The structure of "transportOptions" depends on the mail system.
+//
+// This config must provide "transportOptions" and "from" (the sender address)
+// See https://nodemailer.com/smtp for details.
+// -----------------------------------------------------------------------------
+
+// transporter settings
 export const transportOptions = {
   host: "___MAILER_HOST___",
   port: 465,
@@ -8,4 +17,5 @@ export const transportOptions = {
   },
 };
 
+// the sender address
 export const from = "___MAILER_FROM___";
