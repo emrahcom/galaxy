9544f82fd03bf87fdd302dbfeeb3aa497822d22b
emrah <emrah.eryilmaz@nordeck.net>
2024-11-08 16:58:53 +0300

    feat(eb-app-api): initialize the mail library

---
+++ machines/eb-app-api/home/api/galaxy/lib/common/mail.ts
@@ -0,0 +1,52 @@
+import { mailFrom, transportOptions } from "../../config.mailer.ts";
+import { getIdentity } from "../database/identity.ts";
+import { getDefaultProfile } from "../database/profile.ts";
+import { createTransport } from "npm:nodemailer";
+
+// -----------------------------------------------------------------------------
+export async function sendMail(
+  mailTo: string,
+  mailSubject: string,
+  mailText: string,
+) {
+  try {
+    const transporter = createTransport(transportOptions);
+    const mailOptions = {
+      from: mailFrom,
+      to: mailTo,
+      subject: mailSubject,
+      text: mailText,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+// -----------------------------------------------------------------------------
+export async function mailMissingCall(caller: string, callee: string) {
+  try {
+    const calleeIdentities = await getIdentity(callee);
+    const calleeIdentity = calleeIdentities[0];
+    if (!calleeIdentity) throw "callee not found";
+
+    const mailTo = calleeIdentity.identity_attr.email;
+    if (!mailTo) throw "email not found";
+
+    const callerProfiles = await getDefaultProfile(caller);
+    const callerProfile = callerProfiles[0];
+    if (!callerProfile) throw "caller not found";
+    const callerName = callerProfile.name;
+
+    const mailSubject = `${callerName} called you`;
+    const mailText = `${callerName} called you`;
+
+    const res = await sendMail(mailTo, mailSubject, mailText);
+    if (!res) throw "sendMail failed";
+  } catch {
+    // do nothing
+  }
+}
