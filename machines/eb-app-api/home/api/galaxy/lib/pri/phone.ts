46ab6971d351ca8632f69969dd89ce97739c7852
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 14:40:02 +0300

    feat(eb-app-api): add phone endpoints

---
+++ machines/eb-app-api/home/api/galaxy/lib/pri/phone.ts
@@ -0,0 +1,96 @@
+import { notFound } from "../http/response.ts";
+import { pri as wrapper } from "../http/wrapper.ts";
+import { getLimit, getOffset } from "../database/common.ts";
+import {
+  addPhone,
+  delPhone,
+  getPhone,
+  listPhone,
+  updatePhone,
+  updatePhoneEnabled,
+} from "../database/phone.ts";
+
+const PRE = "/api/pri/phone";
+
+// -----------------------------------------------------------------------------
+async function get(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const phoneId = pl.id;
+
+  return await getPhone(identityId, phoneId);
+}
+
+// -----------------------------------------------------------------------------
+async function list(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const limit = getLimit(pl.limit);
+  const offset = getOffset(pl.offset);
+
+  return await listPhone(identityId, limit, offset);
+}
+
+// -----------------------------------------------------------------------------
+async function add(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const name = pl.name;
+
+  return await addPhone(identityId, name);
+}
+
+// -----------------------------------------------------------------------------
+async function del(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const phoneId = pl.id;
+
+  return await delPhone(identityId, phoneId);
+}
+
+// -----------------------------------------------------------------------------
+async function enable(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const phoneId = pl.id;
+
+  return await updatePhoneEnabled(identityId, phoneId, true);
+}
+
+// -----------------------------------------------------------------------------
+async function disable(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const phoneId = pl.id;
+
+  return await updatePhoneEnabled(identityId, phoneId, false);
+}
+
+// -----------------------------------------------------------------------------
+async function update(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const phoneId = pl.id;
+  const name = pl.name;
+
+  return await updatePhone(identityId, phoneId, name);
+}
+
+// -----------------------------------------------------------------------------
+export default async function (
+  req: Request,
+  path: string,
+  identityId: string,
+): Promise<Response> {
+  if (path === `${PRE}/get`) {
+    return await wrapper(get, req, identityId);
+  } else if (path === `${PRE}/list`) {
+    return await wrapper(list, req, identityId);
+  } else if (path === `${PRE}/add`) {
+    return await wrapper(add, req, identityId);
+  } else if (path === `${PRE}/del`) {
+    return await wrapper(del, req, identityId);
+  } else if (path === `${PRE}/enable`) {
+    return await wrapper(enable, req, identityId);
+  } else if (path === `${PRE}/disable`) {
+    return await wrapper(disable, req, identityId);
+  } else if (path === `${PRE}/update`) {
+    return await wrapper(update, req, identityId);
+  } else {
+    return notFound();
+  }
+}
