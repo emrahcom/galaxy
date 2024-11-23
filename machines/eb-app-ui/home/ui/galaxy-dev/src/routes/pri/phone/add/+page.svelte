184703ab1be4bd03959cad38f405a3c7d152259b
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 17:23:08 +0300

    feat(eb-app-ui): add the router for adding phone

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/phone/add/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Add from "$lib/components/pri/phone/add.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Add a virtual phone" />
+<Add />
