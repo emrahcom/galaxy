7cbc400d46f3bd31120eb0222eaae243bc2b00be
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 17:23:54 +0300

    feat(eb-app-ui): add the router for updating phone

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/phone/update/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Update from "$lib/components/pri/phone/update.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/phone/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Update this virtual phone" />
+
+{#await pr then phone}
+  <Update p={phone} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
