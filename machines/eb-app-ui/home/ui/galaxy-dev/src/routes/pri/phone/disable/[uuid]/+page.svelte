821345142350080ce778cf13fc60c088c5c59cd4
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 17:23:37 +0300

    feat(eb-app-ui): add the router for disabling phone

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/phone/disable/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Disable from "$lib/components/pri/phone/disable.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/phone/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Disable this virtual phone" />
+
+{#await pr then phone}
+  <Disable p={phone} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
