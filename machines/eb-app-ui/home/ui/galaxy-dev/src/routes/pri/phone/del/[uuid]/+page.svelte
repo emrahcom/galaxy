48dfb0c63110d956e93d3340a22d2c889c4eb717
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 17:23:27 +0300

    feat(eb-app-ui): add the router for deleting phone

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/phone/del/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Del from "$lib/components/pri/phone/del.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/phone/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Delete this virtual phone" />
+
+{#await pr then phone}
+  <Del p={phone} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
