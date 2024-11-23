15e4a05a270f4eda4a7e31e1d7d94def17e1baad
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 16:07:25 +0300

    feat(eb-app-ui): add phone list

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/phone/list-item.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import type { Phone } from "$lib/types";
+  import Copy from "$lib/components/common/button-copy.svelte";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Disable from "$lib/components/common/link-disable.svelte";
+  import Enable from "$lib/components/common/link-enable.svelte";
+  import Update from "$lib/components/common/link-update.svelte";
+
+  interface Props {
+    p: Phone;
+  }
+
+  let { p }: Props = $props();
+
+  // ---------------------------------------------------------------------------
+  function copy(code: string) {
+    const text = `${$page.url.origin}/aud/phone/${code}`;
+
+    navigator.clipboard.writeText(text);
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="col-md-6 col-xl-4">
+  <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
+    <div class="card-body text-center">
+      <h5 class="card-title text-muted">{p.name}</h5>
+
+      <p class="card-text text-muted">
+        {$page.url.origin}/aud/phone/{p.code}
+      </p>
+
+      {#if p.enabled}
+        <Copy label="copy" onclick={() => copy(p.code)} />
+      {/if}
+    </div>
+
+    <div class="card-footer bg-body border-0 text-center">
+      <Del href="/pri/phone/del/{p.id}" />
+
+      {#if p.enabled}
+        <Disable href="/pri/phone/disable/{p.id}" />
+      {:else}
+        <Enable href="/pri/phone/enable/{p.id}" />
+      {/if}
+
+      <Update href="/pri/phone/update/{p.id}" />
+    </div>
+  </div>
+</div>
---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/phone/list.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { Phone } from "$lib/types";
+  import Add from "$lib/components/common/link-add.svelte";
+  import Item from "$lib/components/pri/phone/list-item.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  interface Props {
+    phones: Phone[];
+  }
+
+  let { phones }: Props = $props();
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="list">
+  <div class="row mx-auto mt-2 g-3">
+    {#each phones as p}
+      <Item {p} />
+    {:else}
+      <Warning>
+        There is no phone in the list. Click <Add href="/pri/phone/add" />
+        to add a new virtual phone.
+      </Warning>
+    {/each}
+  </div>
+</section>
