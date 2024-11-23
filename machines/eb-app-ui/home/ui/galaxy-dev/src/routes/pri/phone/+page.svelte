07ec6504c5ebfc0cf285524225df9f19f8be0134
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 16:08:05 +0300

    feat(eb-app-ui): add the main route for phone

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/phone/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { list } from "$lib/api";
+  import List from "$lib/components/pri/phone/list.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = list("/api/pri/phone/list", 100);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader
+  subheader="My virtual phones"
+  hrefAdd="/pri/phone/add"
+  hrefAddTitle="Add a new virtual phone"
+/>
+
+{#await pr then phones}
+  <List {phones} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
