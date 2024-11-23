2eccc9338886795b4aaad0a72e8d0777ddc4eabc
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 20:11:32 +0300

    fix(eb-app-ui): disable the phone if domain is disabled

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/phone/list-item.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/phone/list-item.svelte
@@ -23,7 +23,9 @@
 
 <!-- -------------------------------------------------------------------------->
 <div class="col-md-6 col-xl-4">
-  <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
+  <div
+    class="card h-100 {p.enabled && p.domain_enabled ? '' : 'border-danger'}"
+  >
     <div class="card-body text-center">
       <h5 class="card-title text-muted">{p.name}</h5>
 
