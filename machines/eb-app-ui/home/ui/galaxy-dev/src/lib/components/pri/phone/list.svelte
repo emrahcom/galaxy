45c8cdf338d1a17acc8f66b56bc8022237fd2525
emrah <emrah.eryilmaz@nordeck.net>
2024-11-23 20:53:15 +0300

    refactor(eb-app-ui): update the comment of the empty phone list

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/phone/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/phone/list.svelte
@@ -19,7 +19,7 @@
     {:else}
       <Warning>
         There is no phone in the list. Click <Add href="/pri/phone/add" />
-        to add a new virtual phone.
+        to add a new virtual phone and share its link with your friends.
       </Warning>
     {/each}
   </div>
