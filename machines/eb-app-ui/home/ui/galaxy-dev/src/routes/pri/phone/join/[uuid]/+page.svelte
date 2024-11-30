ea90a2cb43f55e3206a90e03b246f3728d7629c1
emrah <emrah.eryilmaz@nordeck.net>
2024-11-30 20:17:34 +0300

    feat(eb-app-ui): add join page for the call

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/phone/join/[uuid]/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { actionById } from "$lib/api";
+  import type { IntercomMessage222 } from "$lib/types";
+  import Spinner from "$lib/components/common/spinner.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  // ---------------------------------------------------------------------------
+  async function join(msgId: string) {
+    const json = globalThis.localStorage.getItem(`msg-${msgId}`);
+    if (!json) throw "storage item is not found";
+
+    const msg = JSON.parse(json) as IntercomMessage222;
+    const url = msg.intercom_attr.owner_url;
+
+    await actionById("/api/pri/intercom/set/accepted", msgId);
+    globalThis.localStorage.removeItem(`msg-${msgId}`);
+
+    globalThis.location.replace(url);
+  }
+
+  const pr = join($page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Join the phone call" />
+
+{#await pr}
+  <Spinner>joining...</Spinner>
+{:then}
+  <Spinner>joining...</Spinner>
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
