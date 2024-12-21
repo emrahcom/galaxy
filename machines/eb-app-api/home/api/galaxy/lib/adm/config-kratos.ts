c047a79e49fe00075904e9829bc5d77d51682653
emrah <emrah.eryilmaz@nordeck.net>
2024-12-21 15:23:20 +0300

    feat(eb-app-api): the contact email into the config endpoint

--- machines/eb-app-api/home/api/galaxy/lib/adm/config-kratos.ts
+++ machines/eb-app-api/home/api/galaxy/lib/adm/config-kratos.ts
@@ -1,9 +1,11 @@
 import { ok } from "../http/response.ts";
 import { KRATOS_FQDN } from "../../config.ts";
+import { CONTACT_EMAIL } from "../../config.ts";
 
 // -----------------------------------------------------------------------------
 export default function (): Response {
   const config = [{
+    contact_email: CONTACT_EMAIL,
     kratos_fqdn: KRATOS_FQDN,
   }];
 
