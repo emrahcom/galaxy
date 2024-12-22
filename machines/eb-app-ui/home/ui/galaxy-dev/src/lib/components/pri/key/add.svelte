c58ff8810a9aedcc26e65e5326db1682f9e67756
emrah <emrah.eryilmaz@nordeck.net>
2024-12-22 14:10:53 +0300

    feat(eb-app-ui): add the key

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/key/add.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { action } from "$lib/api";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  let warning = $state(false);
+  let disabled = $state(false);
+  let p = $state({
+    name: "",
+  });
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    globalThis.location.href = "/pri/key";
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onsubmit() {
+    try {
+      warning = false;
+      disabled = true;
+
+      await action("/api/pri/key/add", p);
+      globalThis.location.href = "/pri/key";
+    } catch {
+      warning = true;
+      disabled = false;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="add">
+  <div class="d-flex mt-2 justify-content-center">
+    <form {onsubmit} style="width:{FORM_WIDTH};">
+      <Text name="name" label="Name" bind:value={p.name} required={true} />
+
+      {#if warning}
+        <Warning>
+          The add request is not accepted. Please check your inputs.
+        </Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel {disabled} onclick={cancel} />
+        <SubmitBlocker />
+        <Submit {disabled} label="Add" />
+      </div>
+    </form>
+  </div>
+</section>
