0df1e3e87416fcb8b3b0b24f29302fd293aba7e1
emrah <emrah.eryilmaz@nordeck.net>
2024-12-22 13:34:36 +0300

    feat(eb-app-ui): add /route/pri/key/enable

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/key/enable/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Enable from "$lib/components/pri/key/enable.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/key/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Enable this private key" />
+
+{#await pr then key}
+  <Disable p={key} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
