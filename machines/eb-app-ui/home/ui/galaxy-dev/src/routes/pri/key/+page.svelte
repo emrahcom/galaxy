948f32920deb3947956f8ef287bb0a4da2c88e57
emrah <emrah.eryilmaz@nordeck.net>
2024-12-22 13:33:59 +0300

    feat(eb-app-ui): add /route/pri/key

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/key/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { list } from "$lib/api";
+  import List from "$lib/components/pri/key/list.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = list("/api/pri/key/list", 100);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader
+  subheader="My private keys"
+  hrefAdd="/pri/key/add"
+  hrefAddTitle="Add a new private key"
+/>
+
+{#await pr then keys}
+  <List {keys} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
