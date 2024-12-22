17dde14ed44d72db06b0c11947aceff1a41c7278
emrah <emrah.eryilmaz@nordeck.net>
2024-12-22 14:10:39 +0300

    feat(eb-app-ui): list keys

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/key/list-item.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type { IdentityKey } from "$lib/types";
+  import Copy from "$lib/components/common/button-copy.svelte";
+  import Del from "$lib/components/common/link-del.svelte";
+  import Disable from "$lib/components/common/link-disable.svelte";
+  import Enable from "$lib/components/common/link-enable.svelte";
+
+  interface Props {
+    p: IdentityKey;
+  }
+
+  let { p }: Props = $props();
+
+  // ---------------------------------------------------------------------------
+  function copy(code: string) {
+    navigator.clipboard.writeText(code);
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="col-md-6 col-xl-4">
+  <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
+    <div class="card-body text-center">
+      <h5 class="card-title text-muted">{p.name}</h5>
+
+      <p class="card-text text-muted">{p.code}</p>
+
+      {#if p.enabled}
+        <Copy label="copy" onclick={() => copy(p.code)} />
+      {/if}
+    </div>
+
+    <div class="card-footer bg-body border-0 text-center">
+      <Del href="/pri/key/del/{p.id}" />
+
+      {#if p.enabled}
+        <Disable href="/pri/key/disable/{p.id}" />
+      {:else}
+        <Enable href="/pri/key/enable/{p.id}" />
+      {/if}
+    </div>
+  </div>
+</div>
---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/key/list.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { IdentityKey } from "$lib/types";
+  import Add from "$lib/components/common/link-add.svelte";
+  import Item from "$lib/components/pri/key/list-item.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  interface Props {
+    keys: IdentityKey[];
+  }
+
+  let { keys }: Props = $props();
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="list">
+  <div class="row mx-auto mt-2 g-3">
+    {#each keys as p}
+      <Item {p} />
+    {:else}
+      <Warning>
+        There is no key in the list. Click <Add href="/pri/key/add" />
+        to add a new private key.
+      </Warning>
+    {/each}
+  </div>
+</section>
