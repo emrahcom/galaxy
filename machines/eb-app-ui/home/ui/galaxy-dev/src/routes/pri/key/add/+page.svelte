843b9e05cf6f57fa2c173b2ddc5b2dd690e6c40c
emrah <emrah.eryilmaz@nordeck.net>
2024-12-22 13:34:13 +0300

    feat(eb-app-ui): add /route/pri/key/add

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/key/add/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Add from "$lib/components/pri/key/add.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Add a private key" />
+<Add />
