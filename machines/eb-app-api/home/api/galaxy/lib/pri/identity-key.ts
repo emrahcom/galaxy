83e35d3430a7165a12aa32bcf8708e5eb9eaf3a3
emrah <emrah.eryilmaz@nordeck.net>
2024-12-22 14:53:44 +0300

    feat(eb-app-api): add the identity key endpoints

---
+++ machines/eb-app-api/home/api/galaxy/lib/pri/identity-key.ts
@@ -0,0 +1,85 @@
+import { notFound } from "../http/response.ts";
+import { pri as wrapper } from "../http/wrapper.ts";
+import { isValidUrl } from "../common/validate.ts";
+import { getLimit, getOffset } from "../database/common.ts";
+import {
+  addIdentityKey,
+  delIdentityKey,
+  getIdentityKey,
+  listIdentityKey,
+  updateIdentityKeyEnabled,
+} from "../database/identity-key.ts";
+
+const PRE = "/api/pri/key";
+
+// -----------------------------------------------------------------------------
+async function get(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const keyId = pl.id;
+
+  return await getIdentityKey(identityId, keyId);
+}
+
+// -----------------------------------------------------------------------------
+async function list(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const limit = getLimit(pl.limit);
+  const offset = getOffset(pl.offset);
+
+  return await listIdentityKey(identityId, limit, offset);
+}
+
+// -----------------------------------------------------------------------------
+async function add(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const name = pl.name;
+
+  return await addIdentityKey(identityId, name);
+}
+
+// -----------------------------------------------------------------------------
+async function del(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const keyId = pl.id;
+
+  return await delIdentityKey(identityId, keyId);
+}
+
+// -----------------------------------------------------------------------------
+async function enable(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const keyId = pl.id;
+
+  return await updateIdentityKeyEnabled(identityId, keyId, true);
+}
+
+// -----------------------------------------------------------------------------
+async function disable(req: Request, identityId: string): Promise<unknown> {
+  const pl = await req.json();
+  const keyId = pl.id;
+
+  return await updateIdentityKeyEnabled(identityId, keyId, false);
+}
+
+// -----------------------------------------------------------------------------
+export default async function (
+  req: Request,
+  path: string,
+  identityId: string,
+): Promise<Response> {
+  if (path === `${PRE}/get`) {
+    return await wrapper(get, req, identityId);
+  } else if (path === `${PRE}/list`) {
+    return await wrapper(list, req, identityId);
+  } else if (path === `${PRE}/add`) {
+    return await wrapper(add, req, identityId);
+  } else if (path === `${PRE}/del`) {
+    return await wrapper(del, req, identityId);
+  } else if (path === `${PRE}/enable`) {
+    return await wrapper(enable, req, identityId);
+  } else if (path === `${PRE}/disable`) {
+    return await wrapper(disable, req, identityId);
+  } else {
+    return notFound();
+  }
+}
