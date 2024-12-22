a25ff2a26e4626f91b7684b7fe3d90dd9d6d8eea
emrah <emrah.eryilmaz@nordeck.net>
2024-12-22 13:34:27 +0300

    feat(eb-app-ui): add /route/pri/key/disable

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/key/disable/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Disable from "$lib/components/pri/key/disable.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/key/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Disable this private key" />
+
+{#await pr then key}
+  <Disable p={key} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
