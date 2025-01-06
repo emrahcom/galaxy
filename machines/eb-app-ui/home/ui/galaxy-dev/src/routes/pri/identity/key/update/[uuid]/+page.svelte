e5cf7cb15af41e2b0ff7cf8e0df73754cea0c98c
emrah <emrah.eryilmaz@nordeck.net>
2025-01-06 18:25:07 +0300

    feat(eb-app-ui): add the update identity-key router

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/identity/key/update/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Update from "$lib/components/pri/identity-key/update.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/identity/key/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Update this private key" />
+
+{#await pr then key}
+  <Update p={key} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
