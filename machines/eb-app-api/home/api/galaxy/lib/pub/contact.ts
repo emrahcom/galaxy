6d4f083724251d494eb9b38d4ea826d604d84a0c
emrah <emrah.eryilmaz@nordeck.net>
2025-01-07 22:11:03 +0300

    refactor(eb-app-api): renamed as keyValue in contact.ts

--- machines/eb-app-api/home/api/galaxy/lib/pub/contact.ts
+++ machines/eb-app-api/home/api/galaxy/lib/pub/contact.ts
@@ -1,27 +1,27 @@
 import { getLimit, getOffset } from "../database/common.ts";
 import { notFound } from "../http/response.ts";
 import { pub as wrapper } from "../http/wrapper.ts";
-import { callContactByCode, listContactByCode } from "../database/contact.ts";
+import { callContactByKey, listContactByKey } from "../database/contact.ts";
 
 const PRE = "/api/pub/contact";
 
 // -----------------------------------------------------------------------------
 async function list(req: Request): Promise<unknown> {
   const pl = await req.json();
-  const code = pl.code;
+  const keyValue = pl.key_value;
   const limit = getLimit(pl.limit);
   const offset = getOffset(pl.offset);
 
-  return await listContactByCode(code, limit, offset);
+  return await listContactByKey(keyValue, limit, offset);
 }
 
 // -----------------------------------------------------------------------------
 async function call(req: Request): Promise<unknown> {
   const pl = await req.json();
-  const code = pl.code;
+  const keyValue = pl.key_value;
   const contactId = pl.contact_id;
 
-  return await callContactByCode(code, contactId);
+  return await callContactByKey(keyValue, contactId);
 }
 
 // -----------------------------------------------------------------------------
