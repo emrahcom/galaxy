3a55ad564fb39f38169d181791f3d9cc5a24ad3e
emrah <emrah.eryilmaz@nordeck.net>
2025-03-02 12:49:07 +0300

    refactor(eb-app-api): list only visible contacts for extension

--- machines/eb-app-api/home/api/galaxy/lib/database/contact.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/contact.ts
@@ -159,6 +159,7 @@ export async function listContactByKey(
                               WHERE value = $1
                                 AND enabled
                              )
+        AND co.visible
       ORDER BY name, profile_name, profile_email
       LIMIT $2 OFFSET $3`,
     args: [
