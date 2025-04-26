420aceb2f3b6a70fe4ec80c28fa10125b2848bb3
emrah <emrah.eryilmaz@conceptboard.com>
2025-04-26 16:14:40 +0300

    fix(eb-app-ui): svelte/require-each-key in common/form-select.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-select.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-select.svelte
@@ -30,7 +30,7 @@
   >
     <option class="bg-body"></option>
 
-    {#each options as opt}
+    {#each options as opt (opt[0])}
       <option value={opt[0]}>{opt[1]}</option>
     {/each}
   </select>
