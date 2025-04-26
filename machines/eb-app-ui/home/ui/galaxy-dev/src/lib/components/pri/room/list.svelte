e28704ad846ef99283134ed6e83b1820bd042959
emrah <emrah.eryilmaz@conceptboard.com>
2025-04-26 15:22:09 +0300

    fix(eb-app-ui): svelte/require-each-key in room/list.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room/list.svelte
@@ -18,11 +18,11 @@
 <!-- -------------------------------------------------------------------------->
 <section id="list">
   <div class="row mx-auto mt-2 g-3">
-    {#each rooms as p}
+    {#each rooms as p (p.id)}
       <Item {p} />
     {/each}
 
-    {#each candidacies as p}
+    {#each candidacies as p (p.id)}
       <Candidacy {p} />
     {/each}
 
