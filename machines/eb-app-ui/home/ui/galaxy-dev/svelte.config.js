a0b0ff949d61788eab0454e39d095c601af238ac
emrah <emrah.eryilmaz@conceptboard.com>
2025-04-26 14:59:38 +0300

    refactor(eb-app-ui): latest svelte.config.js

--- machines/eb-app-ui/home/ui/galaxy-dev/svelte.config.js
+++ machines/eb-app-ui/home/ui/galaxy-dev/svelte.config.js
@@ -1,13 +1,8 @@
-//import adapter from "@sveltejs/adapter-auto";
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter({
       fallback: "index.html",
