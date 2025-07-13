ef40aad055b4e2c8fc47532141a9727d14ac3c92
emrah <emrah.eryilmaz@conceptboard.com>
2025-07-13 15:09:53 +0300

    refactor(eb-app-ui): update eslint

--- machines/eb-app-ui/home/ui/galaxy-dev/eslint.config.js
+++ machines/eb-app-ui/home/ui/galaxy-dev/eslint.config.js
@@ -1,6 +1,6 @@
 import prettier from "eslint-config-prettier";
-import js from "@eslint/js";
 import { includeIgnoreFile } from "@eslint/compat";
+import js from "@eslint/js";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
 import { fileURLToPath } from "node:url";
@@ -20,7 +20,11 @@ export default ts.config(
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
     },
-    rules: { "no-undef": "off" },
+    rules: {
+      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      "no-undef": "off",
+    },
   },
   {
     files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
