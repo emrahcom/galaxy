4fe364534a01e3bd86f659507d7a09ed6b10990e
emrah <emrah.eryilmaz@conceptboard.com>
2025-08-02 13:36:25 +0300

    feat(eb-app-api): add the text message functions in pub

--- machines/eb-app-api/home/api/galaxy/lib/pub/contact.ts
+++ machines/eb-app-api/home/api/galaxy/lib/pub/contact.ts
@@ -1,7 +1,11 @@
 import { getLimit, getOffset } from "../database/common.ts";
 import { notFound } from "../http/response.ts";
 import { pub as wrapper } from "../http/wrapper.ts";
-import { callContactByKey, listContactByKey } from "../database/contact.ts";
+import {
+  callContactByKey,
+  listContactByKey,
+  textContactByKey,
+} from "../database/contact.ts";
 
 const PRE = "/api/pub/contact";
 
@@ -25,11 +29,23 @@ async function callByKey(req: Request): Promise<unknown> {
 }
 
 // -----------------------------------------------------------------------------
+async function textByKey(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const keyValue = pl.key_value;
+  const contactId = pl.contact_id;
+  const message = pl.message;
+
+  return await textContactByKey(keyValue, contactId, message);
+}
+
+// -----------------------------------------------------------------------------
 export default async function (req: Request, path: string): Promise<Response> {
   if (path === `${PRE}/list/bykey`) {
     return await wrapper(listByKey, req);
   } else if (path === `${PRE}/call/bykey`) {
     return await wrapper(callByKey, req);
+  } else if (path === `${PRE}/text/bykey`) {
+    return await wrapper(textByKey, req);
   } else {
     return notFound();
   }
