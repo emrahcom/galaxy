992d12cfa1c1cf52bb31b230b6d40c29df9aaf67
emrah <emrah.eryilmaz@conceptboard.com>
2025-08-02 20:52:01 +0300

    feat(eb-app-ui): add the route for chat

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/contact/text/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Text from "$lib/components/pri/contact/text.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/contact/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Send message to this contact" />
+
+{#await pr then contact}
+  <Text p={contact} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
