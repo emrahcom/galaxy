52c25182929d488fada6aa570f72df52215e6785
emrah <emrah.eryilmaz@conceptboard.com>
2025-08-03 18:43:30 +0300

    refactor(eb-app-ui): transparent bg for the message text

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/message/text.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/message/text.svelte
@@ -65,7 +65,7 @@
     <div class="d-flex justify-content-left">
       <p
         class="d-inline-block card-text text-muted text-start
-        text-truncate bg-light w-auto mt-3"
+        text-truncate bg-transparent w-auto mt-3"
         style="max-width: 90%; white-space: pre"
       >
         {msg.intercom_attr.message || ""}
