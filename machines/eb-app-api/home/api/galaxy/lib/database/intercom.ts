5b4c033578719a2b8a97a64cfd69928da0f00c53
emrah <emrah.eryilmaz@conceptboard.com>
2025-08-16 17:19:06 +0300

    feat(eb-app-api): add getIntercomByKey sql

--- machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
@@ -73,6 +73,54 @@ export async function getIntercom(identityId: string, intercomId: string) {
 }
 
 // -----------------------------------------------------------------------------
+// Consumer is the callee with an identity key.
+//
+// Epoch time in query output and on the client-side is microsecond (as integer)
+// but second (as float) in Postgres...
+//
+// message in jsonb is base64 encoded. Not because of the security reason, to
+// get clear output in database backups.
+// -----------------------------------------------------------------------------
+export async function getIntercomByKey(keyValue: string, intercomId: string) {
+  // There will be no contact if this call is from a public phone.
+  // ic.identity_id is the system user in this case.
+  const sql = {
+    text: `
+      SELECT ic.id, co.name as contact_name, ic.status, ic.message_type,
+        CASE
+          WHEN ic.message_type = 'text' THEN
+            jsonb_build_object(
+              'message', convert_from(
+                           decode(ic.intercom_attr->>'message', 'base64'),
+                           'UTF8'
+                         )
+            )
+          ELSE ic.intercom_attr
+        END AS intercom_attr,
+        ic.created_at,
+        TRUNC(
+          EXTRACT(EPOCH FROM ic.created_at) * 1000000
+        ) AS microsec_created_at,
+        ic.expired_at
+      FROM intercom ic
+        LEFT JOIN contact co ON co.identity_id = ic.remote_id
+                                AND co.remote_id = ic.identity_id
+      WHERE ic.id = $2
+        AND ic.remote_id = (SELECT identity_id
+                            FROM identity_key
+                            WHERE value = $1
+                              AND enabled
+                           )`,
+    args: [
+      keyValue,
+      intercomId,
+    ],
+  };
+
+  return await fetch(sql) as IntercomMessage222[];
+}
+
+// -----------------------------------------------------------------------------
 // Consumer is a public user by using a public phone code.
 // -----------------------------------------------------------------------------
 export async function getIntercomAttrByCode(code: string, intercomId: string) {
