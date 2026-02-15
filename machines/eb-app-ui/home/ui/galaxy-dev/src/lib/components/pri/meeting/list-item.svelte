82c9ed0930f90214dfab22e2f47bf64d5a362a5b
emrah <emrah.eryilmaz@conceptboard.com>
2026-02-15 14:06:11 +0300

    fix(eb-app-ui): session_list in meeting/list-item

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting/list-item.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting/list-item.svelte
@@ -15,6 +15,8 @@
   }
 
   const { p }: Props = $props();
+
+  const sessionList = $derived([...new Set(p.session_list)].slice(0, 3));
 </script>
 
 <!-- -------------------------------------------------------------------------->
@@ -47,7 +49,7 @@
       <div class="card-text fw-bold">
         {#if p.schedule_type === "scheduled"}
           {#if p.session_at}
-            {#each p.session_list.slice(0, 3) as at (at)}
+            {#each sessionList as at (at)}
               {#if isOnline(at)}
                 <p class="text-primary my-0">{showLocaleDatetime(at)}</p>
               {:else if isToday(at)}
