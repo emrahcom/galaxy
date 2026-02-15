c197d3c2e075c53ce7743dc0e56a6044bca40cf7
emrah <emrah.eryilmaz@conceptboard.com>
2026-02-15 13:20:26 +0300

    fix(eb-app-ui): duplicated options in form-select

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-select.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-select.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { SvelteMap } from "svelte/reactivity";
+
   interface Props {
     disabled?: boolean;
     id: string;
@@ -12,10 +14,21 @@
     disabled = false,
     id,
     label,
-    options,
+    options: _options,
     required = true,
     value = $bindable(),
   }: Props = $props();
+
+  // Filter out duplicated options
+  const options = $derived.by(() => {
+    const map = new SvelteMap<string, string[]>();
+
+    for (const opt of _options) {
+      map.set(opt[0], opt);
+    }
+
+    return [...map.values()];
+  });
 </script>
 
 <!-- -------------------------------------------------------------------------->
@@ -31,7 +44,7 @@
     <option class="bg-body"></option>
 
     {#each options as opt (opt[0])}
-      <option value={opt[0]}>{opt[1]}</option>
+      <option value={opt[0]}>{opt[1] || ""}</option>
     {/each}
   </select>
   <label for={id}>{label}</label>
