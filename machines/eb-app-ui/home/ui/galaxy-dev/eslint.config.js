63b910e48febe8962cf4b3f228e8dd741169f895
emrah <emrah.eryilmaz@conceptboard.com>
2026-03-08 12:17:59 +0300

    refactor(eb-app-ui): update packages

--- machines/eb-app-ui/home/ui/galaxy-dev/eslint.config.js
+++ machines/eb-app-ui/home/ui/galaxy-dev/eslint.config.js
@@ -23,6 +23,7 @@ export default defineConfig(
       // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
       // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
       "no-undef": "off",
+      // galaxy: disable this checking
       "svelte/no-navigation-without-resolve": "off",
     },
   },
